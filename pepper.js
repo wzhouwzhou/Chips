c64aeedc188e95b21c5dd618bf8247bbb0580136
William <wzhouwzhou@gmail.com>
2017-03-28 19:42:28 -0400

    Ping User Display Name
    Ping returns user’s display name

--- pepper.js
+++ pepper.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const prefix = "-";
+
 client.login("Mjk2NDA4NDIwMDg3NjI3Nzc2.C7xzXQ.GAtbiTb2jmdPzm3IkUnlrBwvWtY");
 
 client.on('ready', () => {
@@ -8,7 +10,10 @@ client.on('ready', () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.sendMessage("pong!");
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix+ "ping")) {
+    message.channel.sendMessage("pong! " + message.channel.guild.member(message.author).displayName);
+    console.log("ping pong!" + message.author.username)
   }
 });
