0e1dcd542c73f014618b8635061fc38bfbafa197
Pg Biel <pgbiel@hotmail.com.br>
2017-04-02 18:34:24 -0300

    Switch for base

--- routes/base.js
+++ routes/base.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // If the route is being used on /something/somestuff, then "/" here would be /something/somestuff
 
 // ".get" for the GET method
-router.get("/", (res, req) => {
+router.get("/", (req, res) => {
   // Do stuff
   res.render("views-file-name-here" /* It auto-adds .html at the end for you, DO NOT put .html */, { something: "something Else" } /* <--- context, variables accessible from the renderer */);
 });
@@ -15,7 +15,7 @@ router.get("/", (res, req) => {
 // like POST
 // But you're not forced to
 
-router.post("/", (res, req) => {
+router.post("/", (req, res) => {
   // Do stuff
   res.send("You have sent a POST!");
 });
