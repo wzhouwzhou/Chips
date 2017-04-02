c0ee9d1e0c045bf93c8ae68085cbb4a43171389f
William <wzhouwzhou@gmail.com>
2017-04-01 23:36:29 -0400

    Adding Heroku files

--- chips.js
+++ chips.js
@@ -1,3 +1,20 @@
+var express = require('express');
+
+var request = require('request');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('bot is alive!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 const Discord = require("discord.js");
 
 const client = new Discord.Client();
---
+++ edit.bat
@@ -0,0 +1,4 @@
+git commit -a -m "asdf"
+git push heroku master
+heroku open
+heroku logs --tail
