34318d4f1eac1320158a5cdb23aa16da69aaa14b
Pg Biel <pgbiel@hotmail.com.br>
2017-04-01 22:37:11 -0300

    g

--- chips.js
+++ chips.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const _ = require("lodash");
-const chalk = require("chalk");
+global._ = require("lodash");
+global.chalk = require("chalk");
 const readline = require('readline');
 
 const prefix = "-";
