4d7611b1443b89bd555f9dd7cee034dd354de7e2
Pg Biel <pgbiel@hotmail.com.br>
2017-04-02 18:33:46 -0300

    eh

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (res, req) => {
+router.get("/", (req, res) => {
   res.render("index", { timestamp: new Date().toString() });
 });
 
