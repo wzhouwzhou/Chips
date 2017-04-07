6dd54c83d4a3238c18d3f6f13f5757b8b3718ac0
Pg Biel <pgbiel@hotmail.com.br>
2017-04-07 14:24:41 -0300

    the ultimate fix

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
     "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js",
     "/vendor/magnific-popup/jquery.magnific-popup.min.js",
     "/vendor/scrollreveal/scrollreveal.js",
-  ], stylesheets: [
+  ], sheets: [
     "/vendor/font-awesome/css/font-awesome.min.css", 
     "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
     "https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic",
