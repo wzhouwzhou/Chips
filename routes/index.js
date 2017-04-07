8f5f145497d358ad7cdf3b9e5dfa13f4e8dca7b0
William <wzhouwzhou@gmail.com>
2017-04-06 23:03:59 -0400

    syntax

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // index
 const express = require('express');
 const router = express.Router();
-const stylesheets: ["/vendor/font-awesome/css/font-awesome.min.css"]
+const stylesheets= ["/vendor/font-awesome/css/font-awesome.min.css"];
 
 router.get("/", (req, res) => {
   res.render("index", { timestamp: new Date().toString(),stylesheets:stylesheets });
