afd379686d7c494be2df04e35c6a6189aee39d66
William <wzhouwzhou@gmail.com>
2017-04-06 23:08:21 -0400

    more vss

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 // index
 const express = require('express');
 const router = express.Router();
-const stylesheets= ["/vendor/font-awesome/css/font-awesome.min.css"];
+const stylesheets= ["/vendor/font-awesome/css/font-awesome.min.css",
+  "/vendor/magnific-popup/magnific-popup.css",
+  "css/creative.min.css"
+];
 
 router.get("/", (req, res) => {
   res.render("index", { timestamp: new Date().toString(),stylesheets:stylesheets });
