2a21b172b5472ae36a946f1c37d11f151135e846
William <wzhouwzhou@gmail.com>
2017-04-07 00:19:37 -0400

    ?

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const stylesheets = [
+  "/vendor/bootstrap/css/bootstrap.css",
   "/vendor/font-awesome/css/font-awesome.min.css",
   "/vendor/magnific-popup/magnific-popup.css",
   "/css/creative.min.css",
-  "/vendor/bootstrap/css/bootstrap.css",
   "/vendor/font-awesome/css/font-awesome.min.css",
   "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
   "https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic"
