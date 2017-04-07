e6bf2f72483eb9951923dd7ebf24a8a2fd8a6803
Pg Biel <pgbiel@hotmail.com.br>
2017-04-07 13:34:34 -0300

    bang bang

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("index", { timestamp: new Date().toString(), scripts: [
-    "js/creative.min.js",
-    "vendor/magnific-popup/jquery.magnific-popup.min.js",
-    "vendor/scrollreveal/scrollreveal.js",
+    "/js/creative.min.js",
+    "/vendor/magnific-popup/jquery.magnific-popup.min.js",
+    "/vendor/scrollreveal/scrollreveal.js",
     "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js",
   ], stylesheets: [
     "/vendor/font-awesome/css/font-awesome.min.css", 
     "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
     "https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic",
-    "vendor/magnific-popup/magnific-popup.css",
-    "css/creative.min.css"
+    "/vendor/magnific-popup/magnific-popup.css",
+    "/css/creative.min.css"
   ], bodyid: "page-top"});
 });
 
