99c3c79d9a8121fc4b349c1d5c2d87a95cbe0442
William <wzhouwzhou@gmail.com>
2017-04-07 00:34:33 -0400

    reordering?

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const stylesheets = [
   "/vendor/bootstrap/css/bootstrap.css",
   "/vendor/font-awesome/css/font-awesome.min.css",
-  "/vendor/magnific-popup/magnific-popup.css",
-  "/css/creative.min.css",
   "/vendor/font-awesome/css/font-awesome.min.css",
   "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
-  "https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic"
+  "https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic",
+  "/vendor/magnific-popup/magnific-popup.css",
+  "/css/creative.min.css"
 ];
 
 const plugins = [
