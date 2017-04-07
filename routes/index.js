80be16c6870e14edb1498f2482e80f703f364b33
Pg Biel <pgbiel@hotmail.com.br>
2017-04-07 13:36:46 -0300

    heh

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("index", { timestamp: new Date().toString(), scripts: [
-    "/js/creative.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js",
     "/vendor/magnific-popup/jquery.magnific-popup.min.js",
     "/vendor/scrollreveal/scrollreveal.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js",
+    "/js/creative.min.js",
   ], stylesheets: [
     "/vendor/font-awesome/css/font-awesome.min.css", 
     "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800",
