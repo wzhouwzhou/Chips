23e902ddefa4a2db7f8bc8f9202a496b2cd48bee
William <wzhouwzhou@gmail.com>
2017-04-06 23:01:47 -0400

    Attempt to add stylesheet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 // index
 const express = require('express');
 const router = express.Router();
+const stylesheets: ["/vendor/font-awesome/css/font-awesome.min.css"]
 
 router.get("/", (req, res) => {
-  res.render("index", { timestamp: new Date().toString() });
+  res.render("index", { timestamp: new Date().toString(),stylesheets:stylesheets });
 });
 
 module.exports = router;
