d404429919a1a43c9958a9450e4fa3349954ac58
William <wzhouwzhou@gmail.com>
2017-04-08 02:06:36 -0400

    fixes

--- commands/s.js
+++ commands/s.js
@@ -1,9 +1,10 @@
 module.exports = {
   name: "s",
   async func(msg, { send, author }) {
+    if(author.id==259209114268336129) return;
     let content=msg.content;
     msg.delete();
-    if(author.id==259209114268336129)
-      send(content);
+    content=content.substr(msg.content.indexOf(" ") + 1);
+    send(content);
   }
 };
