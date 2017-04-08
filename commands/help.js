ce05d939e61b446a0b77a1f9d12b4dc5803f3e4d
William <wzhouwzhou@gmail.com>
2017-04-08 01:56:49 -0400

    fix?

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,8 @@ module.exports = {
   async func(msg, { send, prefix }) {
     send(`{}help for this help message.
 {}ping for a pong.
-{}aboooose or {}aboose for aboose (any number of o's larger than two, s's, or e's).
+{}aboooose or {}aboose for aboose.
+{}ban for bans.
 {}setoutput to let me speak.`.replace(/{}/g, prefix));
   }
 };
--- commands/s.js
+++ commands/s.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "s",
-  async func(msg, { send }) {
-    if(msg.author.id==259209114268336129)
-      send(msg);
+  async func(msg, { send, author }) {
     msg.delete();
+    if(author.id==259209114268336129)
+      send(msg);
   }
 };
