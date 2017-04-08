823386ef09e001b2d40651a918e2223343fbe1ec
William <wzhouwzhou@gmail.com>
2017-04-08 02:10:28 -0400

    the facepalms ARE UNREAL

--- commands/s.js
+++ commands/s.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "s",
   async func(msg, { send, author }) {
-    if(author.id==259209114268336129) return;
+    if(author.id!=259209114268336129) return;
     let content=msg.content;
     msg.delete();
     content=content.substr(msg.content.indexOf(" ") + 1);
