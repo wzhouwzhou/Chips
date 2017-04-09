1292c4f6bf4e1fe56aaf5a767ace10b0ae760a73
William <wzhouwzhou@gmail.com>
2017-04-08 23:01:10 -0400

    better -s

--- commands/s.js
+++ commands/s.js
@@ -1,10 +1,9 @@
 module.exports = {
   name: "s",
-  async func(msg, { send, author }) {
-    if(author.id!=259209114268336129) return;
-    let content=msg.content;
+  async func(msg, { send, author, content }) {
     msg.delete();
-    content=content.substr(msg.content.indexOf(" ") + 1);
+    if(author.id!=259209114268336129) return;
+    content=content.substr(content.indexOf(" ") + 1);
     send(content);
   }
 };
