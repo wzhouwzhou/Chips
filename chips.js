4c9c487aa0fd14ae53937aedded2df6ce1f4e6ca
William <wzhouwzhou@gmail.com>
2017-04-09 01:39:48 -0400

    send instead of sendMessage?

--- chips.js
+++ chips.js
@@ -200,13 +200,13 @@ async function dmHandle (message) {
       .setThumbnail(message.author.avatarURL)
       .setTitle(moment(message.timestamp).format('ddd, Do of MMM @ HH:mm:ss'));
     mEmbeds=mEmbeds.push(main);
-    /*
+    
     let msgembeds=message.embeds;
     msgembeds.forEach(function (item,index,array){
       mEmbeds=mEmbeds.push(item);
-    });*/
+    });
 
-    hook.sendMessage("**DM Received**",{
+    hook.send("**DM Received**",{
       embeds: mEmbeds
     })
     .then(hook.delete())
