831df88f0f7d3424a45a9428c68456a32d26a071
William <wzhouwzhou@gmail.com>
2017-04-09 12:06:26 -0400

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,15 @@
 module.exports = {
   name: "ping",
   async func(msg, { send, member }) {
-    send(`pong! ${member.displayName}`);
-    console.log("ping pong!" + member.user.username);
+    let sentmessage;
+    try {
+      sentmsg = await send("Pong! ");
+    } catch (err) { console.error(`Error at sending message of SetOutput: ${err}`); }
+
+    return sentmsg.edit(`Pong! ${member.displayName}`).then(m=> {
+      const diff = m.editedAt - m.createdAt;
+      console.log("ping pong!" + member.user.username + "\t" + diff);
+      return m.edit(`🏓\u2000Pong! (${diff} ms)`);
+    });
   }
 };
