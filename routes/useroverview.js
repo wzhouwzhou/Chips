b2cb74c7325b4e3f0acbfecf0127eac773fac6e0
William <wzhouwzhou@gmail.com>
2017-04-10 19:25:14 -0400

    null catching

--- routes/useroverview.js
+++ routes/useroverview.js
@@ -3,12 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("useroverview",
-    { timestamp: new Date().toString(),
-      isAuthenticated: req.isAuthenticated(),
-      user:req.user,
-      ujson: res.json(req.user)
-    });
+  if(req.user!=null)
+    res.render("useroverview",
+      { timestamp: new Date().toString(),
+        isAuthenticated: req.isAuthenticated(),
+        user: req.user,
+        ujson: res.json(req.user)
+      }
+    );
+  else
+    res.render("useroverview",
+      { timestamp: new Date().toString(),
+        isAuthenticated: req.isAuthenticated()
+      }
+    );
 });
 
 module.exports = router;
