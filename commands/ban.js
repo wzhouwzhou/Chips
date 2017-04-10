e360c34f9c8c56e8338db7fb7499eba39479d706
William <wzhouwzhou@gmail.com>
2017-04-10 01:03:31 -0400

    Update

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "ban",
-  async func(msg, { send, member, author, content, channel, guild, args, gMember }) {
+  async func(msg, { send, member, author, content, channel, guild, args, gMember}) {
     const used = member || author;
     switch (used.id) {
       case Constants.users.WILLYZ:
@@ -14,14 +14,27 @@ module.exports = {
     const target = args[0].match(Constants.patterns.MENTION)[1];
     // console.log("Target: "+target);
 
-    const user = gMember(target).user.username;
+    const split = content.replace(/\s+/g,' ').trim().split(" ");
+    const reason = split.slice(2,split.length).join(" ");
 
+    const user = gMember(target).user;
+
+    let emb = new Discord.RichEmbed()
+      .setTitle("Ban Notice!")
+      .setAuthor(`You were banned from the server: ${guild.name}!`)
+      .setColor(9109504)
+      .setThumbnail("https://i.ppy.sh/2dabc46c70a032cdeac21093ac8c4b9204f04e75/687474703a2f2f692e696d6775722e636f6d2f5a4e4f7445494e2e706e67")
+      .addField("Ban reason: ", `(${reason})`,true);
+
+    user.sendEmbed(emb);
+
+    const usernm=user.username;
 
     send(`${member.displayName}, user banned succesfully!`);
 
     channel.createWebhook("Mee6", Constants.avatars.MEE6)
       .then (whook => whook.edit('Mee6', Constants.avatars.MEE6))
-      .then(hook => hook.sendMessage(`**${user}** left`)
+      .then(hook => hook.sendMessage(`**${usernm}** left`)
         .then(m => {hook.delete();})
         .catch(console.error)
       );
