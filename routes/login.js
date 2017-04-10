2d1c5d8c9bb81dd872c300c52be664f88c02f43a
William <wzhouwzhou@gmail.com>
2017-04-10 18:12:58 -0400

    files

---
+++ routes/login.js
@@ -0,0 +1,9 @@
+// user
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("login", { timestamp: new Date().toString() });
+});
+
+module.exports = router;
