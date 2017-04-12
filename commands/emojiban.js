dd97edca674b0913a708a5edb2a460b3ac394c2b
William <wzhouwzhou@gmail.com>
2017-04-12 18:16:44 -0400

    emojiban

--- commands/emojiban.js
+++ commands/emojiban.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: "ban",
+  name: "emojiban",
   async func(msg, { send, member, author, content, channel, guild, args, gMember, Discord }) {
     const used = member || author;
     switch (used.id) {
@@ -12,34 +12,31 @@ module.exports = {
       default:
         return send(`No bans for you! ${member.displayName}`);
     }
+    let ebanRole;
+    if (!message.guild.roles.has("name", "Emoji Banned"))
+      guild.createRole({ name: 'Emoji Banned' })
+        .then(role => ebanRole=role)
+        .catch(console.error);
+    let serverChannels=guild.channels.array;
+    serverChannels.forEach(function(channel) {
+      channel.overwritePermissions(ebanRole, {
+        EXTERNAL_EMOJIS: false
+      });
+    });
+
     if (!args[0]) return send("No user given :(");
     const target = args[0].match(Constants.patterns.MENTION)[1];
     // console.log("Target: "+target);
-
     const split = content.replace(/\s+/g, ' ').trim().split(" ");
     let reason = split.slice(2, split.length).join(" ");
     if (reason == "") reason = "None";
+
     const user = gMember(target).user;
 
-    let emb = new Discord.RichEmbed()
-      .setAuthor("Ban Notice!")
-      .setTitle(`You were banned from the server: ${guild.name}!`)
-      .setColor(9109504)
-      .setThumbnail(Constants.images.WARNING)
-      .addField("Ban reason: ", `${reason}`, true);
-    try{
-      await user.sendEmbed(emb);
-    } catch (err) { console.error(`Error of dming User: ${err}`); }
+    ebanRole.addMember(msg.author);
 
     const usernm = user.username;
 
-    send(`${member.displayName}, user banned successfully!`);
-
-    channel.createWebhook("Mee6 (!help)", Constants.avatars.MEE6)
-      .then (whook => whook.edit('Mee6 (!help)', Constants.avatars.MEE6))
-      .then(hook => hook.sendMessage(`**${usernm}** left`)
-        .then(m => { hook.delete(); })
-        .catch(console.error)
-      );
+    send(`${member.displayName}, user ${usernm} emoji banned successfully!`);
   }
 };
