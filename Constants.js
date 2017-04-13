fa2b43862733214cb0f6160a4bdeb8ad5ec456c3
xzlq <explosivepie123@gmail.com>
2017-04-12 23:14:18 -0700

    asdf

--- Constants.js
+++ Constants.js
@@ -1,6 +1,6 @@
-const exports = {};
+const ex = {};
 
-exports.users = {
+ex.users = {
   WILLY: "259209114268336129",
   WILLYZ: "259209114268336129",
   WILLY_Z: "259209114268336129",
@@ -17,26 +17,26 @@ exports.users = {
   ARX: "237270037528969218"
 };
 
-exports.avatars = {
+ex.avatars = {
   MEE6: "https://cdn.discordapp.com/avatars/159985870458322944/675866cce22f49524a5d25e61859d23e.jpg"
 };
 
-exports.images = {
+ex.images = {
   WARNING: "https://i.ppy.sh/2dabc46c70a032cdeac21093ac8c4b9204f04e75/687474703a2f2f692e696d6775722e636f6d2f5a4e4f7445494e2e706e67"
 };
 
-exports.channels = {
+ex.channels = {
   TEST: "296420294678020107",
   DMS: "300466114243067906",
   SLOGS: "252533870312816642"
 };
 
-exports.patterns = {
+ex.patterns = {
   MENTION: /^[^]*<@!?(\d+)>[^]*$/
 };
 
-exports.express = {
+ex.express = {
   ENGINE: "html"
 };
 
-module.exports = exports;
+module.exports = ex;
