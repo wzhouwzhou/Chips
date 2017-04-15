5a53a2418106c3dd02758cf03248cce1cc3ddc45
William <wzhouwzhou@gmail.com>
2017-04-14 20:56:29 -0400

    oops

--- commands/botpanic.js
+++ commands/botpanic.js
@@ -12,12 +12,7 @@ module.exports = {
       default:
         return send(`No I am not having a panic attack, ${member.displayName}!`);
     }
-    if (!args[0]) return send("No user given :(");
-    const target = args[0].match(Constants.patterns.MENTION)[1];
-    // console.log("Target: "+target);
-
-    const usernm = user.username;
-
+    
     send(`${member.displayName}, panic mode activated! Chips shutting down...`);
     database.sheets[`botlog`].addRow({time: `${moment().format('ddd, Do of MMM @ HH:mm:ss')}`, action: "PANIC", mainvalue: -100, label: " (code)"},(err) => {console.log(`Error : ${err}`);});
     process.exit(-100);
