2596e3cbba7b569db78a42e62954ac203f01f2b4
William <wzhouwzhou@gmail.com>
2017-04-15 01:05:23 -0400

    updated helptext

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,15 @@ module.exports = {
   name: "help",
   async func(msg, { send, prefix }) {
     send(`{}help for this help message.
-{}ping for a pong.
-{}aboose for aboose.
-{}ban for bans.
-{}listen for terminal input (Deprecated)
-{}setdm to set dmC
-{}setoutput to let me speak.`.replace(/{}/g, prefix));
+      {}ping for ping pong.
+      {}aboose for aboose.
+      {}exposed for exposed.
+      {}listen for terminal input (Deprecated)
+      {}setdm to set dmC\nRestricted/Whitelisted commands:
+      {}emojiban to external-emoji ban someone.
+      {}ban to ban people.
+      {}setoutput to define testC.
+      {}setDM to define dmC.
+      {}botpanic to force shutdown`.replace(/{}/g, prefix));
   }
 };
