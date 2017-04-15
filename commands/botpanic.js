12a7d77d7b4e4b4285cedb89495f884b46342f38
William <wzhouwzhou@gmail.com>
2017-04-14 21:05:08 -0400

    .

--- commands/botpanic.js
+++ commands/botpanic.js
@@ -12,9 +12,9 @@ module.exports = {
       default:
         return send(`No I am not having a panic attack, ${member.displayName}!`);
     }
-    
+
     await send(`${member.displayName}, panic mode activated! Chips shutting down...`);
-    database.sheets[`botlog`].addRow({time: `${moment().format('ddd, Do of MMM @ HH:mm:ss')}`, action: "PANIC", mainvalue: -100, label: " (code)"},(err) => {console.log(`Error : ${err}`);});
-    proc.exit(-100);
+    database.sheets[`botlog`].addRow({time: `${moment().format('ddd, Do of MMM @ HH:mm:ss')}`, action: "PANIC", mainvalue: "SIGTERM"},(err) => {console.log(`Error : ${err}`);});
+    proc.kill('SIGTERM');
   }
 };
