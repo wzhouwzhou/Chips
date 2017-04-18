bd2a95a2a2fb157b6c875aa457275ba82f065596
William <wzhouwzhou@gmail.com>
2017-04-18 01:28:08 -0400

    oops v2

--- commands/slogstatus.js
+++ commands/slogstatus.js
@@ -5,7 +5,7 @@ module.exports = {
     let mainContent = new Discord.RichEmbed()
       .setColor(205)
       .addField("Half-Hourly spy update:", "Message count: ",true)
-      .setTitle(moment(message.timestamp).format('ddd, Do of MMM @ HH:mm:ss'))
+      .setTitle(moment(msg.timestamp).format('ddd, Do of MMM @ HH:mm:ss'))
       .addField("Num msgs in sk: ", `${sMsgs} msgs`)
       .addField("Num msgs in nebula: ", `${nMsgs} msgs`)
       .addField("Num msgs in sinx: ", `${sxMsgs} msgs`);
