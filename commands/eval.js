f7957013ee9de2e97930fd7d70219c46d7532f69
xzlq <explosivepie123@gmail.com>
2017-04-22 14:46:24 -0700

    oops

--- commands/eval.js
+++ commands/eval.js
@@ -10,12 +10,12 @@ module.exports = {
 	async func(msg, {send, member, author, content, channel}) {
 		if (!whitelist.indexOf(author.id)>=0) return;
 		try {
-			var evaled = eval(m.content.slice(';eval '.length));
+			let evaled = eval(content.slice('-eval '.length));
 			if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-			m.channel.sendCode("xl", evaled).catch(console.error);
+			channel.sendCode("xl", evaled).catch(console.error);
 		}
 		catch(err) {
-			m.channel.sendCode("xl", err.toString()).catch(console.error);
+			channel.sendCode("xl", err.toString()).catch(console.error);
 		}
 	}
 }
