a8f225ef5e2a550567c4d4bc6ea59c1ab6884e3b
William <wzhouwzhou@gmail.com>
2017-04-24 00:22:19 -0400

    Putting scripts and sheets in route

--- routes/useroverview.js
+++ routes/useroverview.js
@@ -8,7 +8,14 @@ router.get("/", (req, res) => {
       { timestamp: new Date().toString(),
         isAuthenticated: req.isAuthenticated(),
         user: req.user,
-        avatarURL: req.user.avatarURL
+        avatarURL: req.user.avatarURL,
+        scripts: [
+          "https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min.js",
+          "/activity/github-activity.js"
+        ], sheets: [
+          "https://cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.min.css",
+          "/activity/github-activity.css"
+        ]//,
         //ujson: res.json(req.user)
       }
     );
