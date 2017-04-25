aa57eb568b97a4553cd940d2f487df121bfb7386
William <wzhouwzhou@gmail.com>
2017-04-25 18:11:49 -0400

    nR.js

---
+++ commands/nR.js
@@ -0,0 +1,39 @@
+module.exports = {
+  name: "nR",
+  async func(msg, { send, member, author, content, channel, guild, args, gMember, Discord }) {
+    const used = member || author;
+    switch (used.id) {
+      case Constants.users.WILLYZ:
+      case Constants.users.PGSUPER:
+      case Constants.users.ZALGO:
+      case Constants.users.XZLQ:
+      case Constants.users.KONEKO:
+      case Constants.users.NELYN:
+      case Constants.users.LOAF:
+      case Constants.users.ARX:
+        break;
+      default:
+        return;
+    }
+    if (!args[0]) return send("No user given :(");
+    const target = args[0];
+    // console.log("Target: "+target);
+
+    let us = target;
+    let serv = Constants.servers.NEB;
+    let num=0;
+    let i=0;
+    let bad =(new (require('discord.js')).RichEmbed());
+    c3.guilds.get(serv).members.get(us).roles.forEach(function(item){
+      bad.setColor(200).setTitle(`Role lookup for ${guild.members.get(us).username}: ${us}`);
+      bad.addField(`role match ${num}:`,item.name); i++;num++;
+      if(i==24) {
+        message.channel.sendEmbed(bad);
+        bad =(new (require('discord.js')).RichEmbed());
+        i=0;
+      }
+    });
+    message.channel.sendEmbed(bad);
+    message.channel.send(num+" roles");
+  }
+};
