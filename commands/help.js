71a86bbbf5b50c3d55f3304201b63df0d75c582a
William <wzhouwzhou@gmail.com>
2017-04-28 18:19:02 -0400

    Updated -help

--- commands/help.js
+++ commands/help.js
@@ -2,18 +2,16 @@ module.exports = {
   name: "help",
   async func(msg, { send, prefix }) {
     send(`**Main commands:**
-      {}help for this help message.
-      {}ping for ping pong.
-      {}aboose for aboose.
-      {}exposed for exposed.
-      {}happy (new command, credits to Tani)
-      {}listen for terminal input (Deprecated)
-      {}setdm to set dmC\n**Restricted/Whitelisted commands:**
+      **{}help** for this help message.
+      **{}support** for chips' support server.
+      **{}ping** for ping pong.
+      **{}aboose** for aboose.
+      **{}exposed** for exposed.
+      **{}happy** (new command, credits to Tani)
+      **{}listen** for terminal input (Deprecated)
+      **{}setdm** to set dmC\n**Restricted/Whitelisted commands:**
       {}emojiban to external-emoji ban someone.
-      {}-ban to ban people.
-      {}setoutput to define testC.
-      {}setDM to define dmC.
-      {}botpanic to force shutdown (Auto restart)
-      {}support for chips' support server.`.replace(/{}/g, prefix));
+      {}-ban to ban people (Disclaimer: This is a fake ban).
+      {}botpanic to force shutdown (Auto restart)`.replace(/{}/g, prefix));
   }
 };
