1a5ba1382337bd8bef8e0bf5df975caf76714ac6
William <wzhouwzhou@gmail.com>
2017-04-29 22:22:36 -0400

    Fixed Filter.js and updated aboose.js

--- handlers/Filter.js
+++ handlers/Filter.js
@@ -14,19 +14,29 @@ f.filter = (message) => {
     Constants.BLACKLIST[message.guild.id]=['ok'];
   }*/
   //console.log("[SUPER SPAMMYDEBUG] blacklist: " + Constants.BLACKLIST[message.guild.id]);
+
+  //detect blacklisted content
   if(mContent=="ok"||mContent=="ko"){//(Constants.BLACKLIST[message.guild.id]).indexOf(mContent)>-1){
     console.log("Blacklisted content found!");
+    console.log("message content: " + mContent);
+
+    //increment blacklist occurence
+    okSpamLogs[id]=okSpamLogs[id]+1;
+    //reset interval
+    currentOkInterval[id] = 0;
+    console.log("ok num increase in channel: " + id +  " new: " + okSpamLogs[id]);
+
     if(okFilter){
-      console.log("ok received: " + mContent);
-      okSpamLogs[id]=okSpamLogs[id]+1;currentOkInterval[id] = 0; console.log("ok num increase in channel: " + id +  " new: " + okSpamLogs[id]);
       if(okSpamLogs[id]>=maxOk){
         message.delete();
         console.log("ok deleted in channel "+ id);
       }
-    }else if(!okFilter && currentOkInterval[id]>5){
+    }else if(!okFilter && okSpamLogs[id]>5){
+      //reenable filter
       okFilter=true;
       console.log("censoring reenabled due to over 5 black listed words in a row for channel: " + id);
     }
+
   }else{
     currentOkInterval[id]=currentOkInterval[id]+1; console.log("currentOkInterval incr: "+ currentOkInterval[id] + " for channel "+ id);
     if(okFilter && currentOkInterval[id]>=okInterval){
