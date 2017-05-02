cfa34c2cb264e339b50785f273245d5ef1281fdf
William <wzhouwzhou@gmail.com>
2017-05-02 19:14:56 -0400

    Helptext

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,7 @@ module.exports = {
       **{}aboose** for aboose.
       **{}exposed** for exposed.
       **{}happy** (new command, credits to Tani)
+      **{}eat** for your own bag of chips.
       **{}listen** for terminal input (Deprecated)\n**Restricted/Whitelisted commands:**
       {}emojiban to external-emoji ban someone.
       {}-ban to ban people (Disclaimer: This is a fake ban).
