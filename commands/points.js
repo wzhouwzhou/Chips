02da970e36c7a6be070956aa59b01ee59a96ebb9
William <wzhouwzhou@gmail.com>
2017-05-04 19:31:52 -0400

    Fix

--- commands/points.js
+++ commands/points.js
@@ -3,6 +3,10 @@ module.exports = {
   async func(msg, {send, member, author, content, channel, guild, args, gMember, Discord, reply, bot}) {
     if(!guild||guild.id!="257889450850254848") return;
     const used = member || author;
-    return reply(`You have: ${database.sinxUsers.get(used.id).username||0} points`);
+    let dbUser = database.sinxUsers.get(used.id);
+    if(dbUser!=null)
+      return reply(`You have: ${dbUser.points} points`);
+    else
+      return reply(`You have no points`);
   }
 };
