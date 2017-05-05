07fce8ce2a6efdffc215e1967e2f47f7e7a1a4dc
William <wzhouwzhou@gmail.com>
2017-05-04 23:18:10 -0400

    Updates to points
    Get points of other users through mentions

--- commands/points.js
+++ commands/points.js
@@ -4,9 +4,24 @@ module.exports = {
     if(!guild||guild.id!="257889450850254848") return;
     const used = member || author;
     let dbUser = database.sinxUsers.get(used.id);
-    if(dbUser!=null)
-      return reply(`You have: ${dbUser.points} points`);
-    else
-      return reply(`You have no points`);
+    if(args[0]==null){
+      if(dbUser!=null)
+        return reply(`You have: ${dbUser.points} points`);
+      else
+        return reply(`You have no points`);
+    }else{
+      const target = args[0].match(Constants.patterns.MENTION)[1];
+      // console.log("Target: "+target);
+      const mem = gMember(target);
+      if(mem==null)
+        return reply(`Target user is not in Sinbad Knights!`);
+      else {
+        upts = database.sinxUsers.get(mem.id);
+        if(upts!=null)
+          return reply(`[${mem.nickname}] has: ${upts} points`);
+        else
+          return reply(`[${mem.nickname}] has no points`);
+      }
+    }
   }
 };
