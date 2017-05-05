a6501c1091c366196a041c713360f47676e02124
William <wzhouwzhou@gmail.com>
2017-05-05 09:24:44 -0400

    gave edp -s access

--- commands/s.js
+++ commands/s.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "s",
   async func(msg, { send, author, content }) {
     msg.delete();
-    if (author.id != Constants.users.WILLYZ) return;
+    if (author.id != Constants.users.WILLYZ && author.id!=Constants.users.EVILDEATHPRO) return;
     content = content.substr(content.indexOf(" ") + 1);
     send(content);
   }
