ddddfdfe5742fff6f8328634fd92870e7bb5f5e2
xzlq <explosivepie123@gmail.com>
2017-05-06 20:42:04 -0700

    bah

--- commands/roll.js
+++ commands/roll.js
@@ -1,12 +1,11 @@
 
 module.exports = {
 	name: "roll",
-	async func(msg, { reply }) {
-		let m = msg;
-		let par = m.contents.split(' ');
-		let atx = m.content.slice(par[0].length+1);
+	async func(msg, { reply, contents, author}) {
+		let par = contents.split(' ');
+		let atx = content.slice(par[0].length+1);
 		let cind = '#'; // comment indicator
-		let ns = m.author.toString();
+		let ns = author.toString();
 		let sdx = atx.indexOf(cind);
 		if (sdx>=0) {
 			expr = atx.substring(0,sdx);
@@ -16,7 +15,7 @@ module.exports = {
 		expr = expr.split(' ').join('');
 		for (let i = 0; i<expr.length; i++) {
 			if (!'0123456789+-*/()d'.includes(expr[i])) {
-				m.reply('error: invalid expression!');
+				reply('error: invalid expression!');
 				return;
 			}
 		}
@@ -25,7 +24,7 @@ module.exports = {
 		while (expr.includes('d')) {
 			ncycles += 1;
 			if (ncycles>50) {
-				m.reply('error: too many separate dice rolls!');
+				reply('error: too many separate dice rolls!');
 				return;
 			}
 			let dex = expr.indexOf('d');
@@ -34,22 +33,22 @@ module.exports = {
 			let dnum = 1;
 			if (pre<dex-1) dnum = parseInt(expr.substring(pre+1,dex));
 			if (dnum == 0) {
-				m.reply('error: invalid expression!');
+				reply('error: invalid expression!');
 				return;
 			}
 			dnumt += dnum;
 			if (dnumt > 99) {
-				m.reply('error: too many dice rolls!');
+				reply('error: too many dice rolls!');
 				return;
 			}
 			let suf = dex+1;
 			for (;suf<expr.length && '0123456789'.includes(expr[suf]); suf++);
 			if (suf==dex+1) {
-				m.reply('error: invalid expression!');
+				reply('error: invalid expression!');
 				return;
 			}
 			if (suf-dex>6) {
-				m.reply('error: dice has too many faces!');
+				reply('error: dice has too many faces!');
 				return;
 			}
 			let dfaces = parseInt(expr.substring(dex+1,suf));
@@ -67,14 +66,14 @@ module.exports = {
 			ns += expr + ' = ' + evaled;
 			ns = ns.split('*').join('\\*');
 			if (ns.length>=2000) {
-				m.reply('error: result exceeds message size limit!');
+				reply('error: result exceeds message size limit!');
 				return;
 			}
-			m.channel.send(ns);
+			channel.send(ns);
 			return;
 		}
 		catch (e) {
-			m.reply('evaluation error...');
+			reply('evaluation error...');
 			return;
 		}
 	}
