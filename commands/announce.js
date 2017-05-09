520c5cde6d0f2fa4055f9acdbdb61fc82922eba0
William <wzhouwzhou@gmail.com>
2017-05-08 23:45:35 -0400

    else text

--- commands/announce.js
+++ commands/announce.js
@@ -6,5 +6,7 @@ module.exports = {
   if(guild.ownerID==author.id)
     await guild.members.forEach(u=>u.send(`Announcement from <@${author.id}> in server [${guild.name}]: ${content.substring((prefix+'announce ').length)}`));
     return reply(`Announcement sent!`);
+  }else{
+    return reply(`You must be server owner to use this cmd...`);
   }
 };
