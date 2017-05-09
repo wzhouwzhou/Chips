64f958341def994704024691eb45901074391f7b
William <wzhouwzhou@gmail.com>
2017-05-09 00:19:46 -0400

    open admin access

--- commands/announce.js
+++ commands/announce.js
@@ -2,10 +2,10 @@ const nEmpireID="297800479356878849";
 
 module.exports = {
   name: "announce",
-  async func(msg, { content, guild, author, reply, args }) {
+  async func(msg, { content, guild, author, member, reply, args }) {
     if(args[0]==null||args[0]=="")return reply(`I can't send an empty announcement.`);
-    
-    if(guild.ownerID==author.id){
+
+    if(guild.ownerID==author.id||member.hasPermission("ADMINISTRATOR")){
       guild.members.forEach(u=>u.send(`Announcement from <@${author.id}> in server [${guild.name}]: ${content.substring((prefix+'announce ').length)}`));
       return reply(`Announcement sent!`);
     }else{
