d2bdee8c81877dbed56bf6f3208214eb24e9e234
William <wzhouwzhou@gmail.com>
2017-05-09 00:28:47 -0400

    Rearrangement

--- commands/announce.js
+++ commands/announce.js
@@ -3,9 +3,8 @@ const nEmpireID="297800479356878849";
 module.exports = {
   name: "announce",
   async func(msg, { content, guild, author, member, reply, args }) {
-    if(args[0]==null||args[0]=="")return reply(`I can't send an empty announcement.`);
-
     if(guild.ownerID==author.id||member.hasPermission("ADMINISTRATOR")){
+      if(args[0]==null||args[0]=="")return reply(`I can't send an empty announcement.`);
       console.log(`[Announcement]: Announcement from <@${author.id}> in server [${guild.name}]: ${content.substring((prefix+'announce ').length)}`);
       guild.members.forEach(u=>{
         try{
