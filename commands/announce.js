a2c015d587c27afe2265cb7d04b12cd670f88be0
William <wzhouwzhou@gmail.com>
2017-05-08 23:48:08 -0400

    fix

--- commands/announce.js
+++ commands/announce.js
@@ -3,10 +3,11 @@ const nEmpireID="297800479356878849";
 module.exports = {
   name: "announce",
   async func(msg, { content, guild, author, reply }) {
-  if(guild.ownerID==author.id)
-    await guild.members.forEach(u=>u.send(`Announcement from <@${author.id}> in server [${guild.name}]: ${content.substring((prefix+'announce ').length)}`));
-    return reply(`Announcement sent!`);
-  }else{
-    return reply(`You must be server owner to use this cmd...`);
+    if(guild.ownerID==author.id){
+      guild.members.forEach(u=>u.send(`Announcement from <@${author.id}> in server [${guild.name}]: ${content.substring((prefix+'announce ').length)}`));
+      return reply(`Announcement sent!`);
+    }else{
+      return reply(`You must be server owner to use this cmd...`);
+    }
   }
 };
