cfcd2ec9e9eaea320b641442a1f4f1fdb2dda9f5
William <wzhouwzhou@gmail.com>
2017-05-08 20:44:46 -0400

    dog.js fix

--- commands/dog.js
+++ commands/dog.js
@@ -7,7 +7,7 @@ const got = require('got');
 getDog = (callback) => {
   got('https://random.dog/woof.json').then(res => {
     try {
-      callback(undefined, JSON.parse(res.body).file);
+      callback(undefined, JSON.parse(res.body).url);
     } catch (err) {
       callback(err);
     }
