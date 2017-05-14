801084903b2adc16dd4b3c6d5ff379c358674b81
William <wzhouwzhou@gmail.com>
2017-05-13 23:06:45 -0400

    Grab nR with mention

--- commands/nR.js
+++ commands/nR.js
@@ -1,8 +1,9 @@
 module.exports = {
   name: "nR",
+  perm:["custom.nr"],
   async func(msg, { send, member, author, content, channel, guild, args, reply, Discord }) {
     const used = member || author;
-    if(!used.hasPermission("ADMINISTRATOR")){
+    if(!used.hasPermission("BAN_MEMBERS")){
       switch (used.id) {
         case Constants.users.WILLYZ:
         case Constants.users.PGSUPER:
@@ -19,25 +20,34 @@ module.exports = {
       }
     }
     if (!args[0]) return send("No user given :(");
-    const target = args[0];
-    // console.log("Target: "+target);
 
-    let us = target;
+    let target, us;
+    try{  //try find by mention
+      target=args[0].match(Constants.patterns.MENTION)[1];
+      // console.log("Target: "+target);
+      us = gMember(target);
+    }catch(err){
+      //an id was specified:
+      target = args[0];
+      // console.log("Target: "+target);
+      us = target;
+    }
     let serv = Constants.servers.NEB;
     let num=0;
     let i=0;
-    let bad =(new (require('discord.js')).RichEmbed());
-    if(c3.guilds.get(serv).members.get(us)==null)return reply(`No roles! (Not in target guild/server).`);
+    let bad =(new Discord.RichEmbed());
+    if(c3.guilds.get(serv).members.get(us)==null)
+      return reply(`No roles for: \`\`${args[0]}\`\`! (Probably not in target guild/server).`);
     c3.guilds.get(serv).members.get(us).roles.forEach(function(item){
       bad.setColor(200).setTitle(`Role lookup for ${c3.guilds.get(serv).members.get(us).user.username}: ${us}`);
       bad.addField(`role match ${num}:`,item.name); i++;num++;
       if(i==24) {
-        channel.sendEmbed(bad);
-        bad =(new (require('discord.js')).RichEmbed());
+        channel.send(' ', {embed: bad});
+        bad =(new Discord.RichEmbed());
         i=0;
       }
     });
-    channel.sendEmbed(bad);
+    channel.send(' ',{embed: bad});
     channel.send(`${--num} roles`);
   }
 };
