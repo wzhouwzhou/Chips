f739dfe34367d8fb50d1066fee32c0271f677e4b
William <wzhouwzhou@gmail.com>
2017-05-14 22:17:08 -0400

    mute update

--- commands/pmute.js
+++ commands/pmute.js
@@ -3,21 +3,12 @@ module.exports = {
   perm: ["server.mute"],
   async func(msg, { send, member, author, content, channel, guild, args, gMember, Discord, reply }) {
     const used = member || author;
-    /*switch (used.id) {
-      case Constants.users.WILLYZ:
-      case Constants.users.PGSUPER:
-      case Constants.users.ZALGO:
-      case Constants.users.XZLQ:
-      case Constants.users.KONEKO:
-      case Constants.users.NELYN:
-      case Constants.users.LOAF:
-      case Constants.users.ARX:
-        break;
-      default:
-        return send(`No mutes for you! ${member.displayName}`);
-    }*/
 
     if (!args[0]) return send("No user given :(");
+    console.log("[Info] Creating new searcher for guild " + guild.id);
+    let options = { guild: guild };
+    searchers[guild.id] = new Searcher( options.guild );
+
     const target = args[0].match(Constants.patterns.MENTION)[1];
     // console.log("Target: "+target);
     const split = content.replace(/\s+/g, ' ').trim().split(" ");
@@ -25,7 +16,11 @@ module.exports = {
     if (reason == "") reason = "None";
 
     const mem = gMember(target);
-    let muterole= guild.roles.get("295551520425115649");
+
+    let list = searchers[guild.id].searchRole("Muted");
+    if(list.length>1) console.log("Multiple mute roles found, using first one..");
+    else if(list.length<1) return await reply(`A mute role was not found in this server!`);
+    let muterole = list[0] || guild.roles.get("295551520425115649");
 
     mem.addRole(muterole);
 
