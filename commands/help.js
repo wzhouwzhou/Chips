307421af464c7327f6828035ccdf44e8dd703f54
Evildeathpro <elawrence152@gmail.com>
2017-05-22 19:10:27 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,35 +6,35 @@ module.exports = {
 We apologize for any inconveniences at this time, we are doing a permissions rewrite that may interfere with daily usage.
       ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 	  ***Informative commands***
-	  **{}help** for this help message.
-      **{}support** for chips' support server.
-      **{}ping** for ping pong.
-      **{}stats** to show my stats.
-	  **{}discordstatus** to get the current status of discord.
+	**{}discordstatus** to get the current status of discord.
+	**{}help** for this help message.
+	**{}ping** for ping pong.
+	**{}stats** to show my stats.
+	**{}support** for chips' support server.
 	  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 	  ***Moderation Commands***
-	  **{}kick** to kick someone.
 	  **{}ban** to ban someone.
-	  **{}pmute** to give someone a muted role! (Disclaimer: this does not create a Muted role)
 	  **{}clear** to clear some messages. (up to 99)
+	  **{}kick** to kick someone.
+	  **{}pmute** to give someone a muted role! (Disclaimer: this does not create a Muted role)
 	  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 	  ***Fun Commands***
-	  **{}aboose** for aboose.
-      **{}exposed** for exposed.
-      **{}happy** (new command, credits to Tani)
-      **{}eat** for your own bag of chips.
-	  **{}lenny** to make a lenny face.
-	  **{}cat** to create a cat.
-	  **{}dog** to create a dog.
-	  **{}rekt** to make some people get rekt.
-	  **{}-calc** to calculate some equations!
-	  **{}quote** to get a quote of a message.
-	  **{}coinflip** to flip a coin.
-	  **{}roll** to roll some dice.
-	  **{}-ban** to ban people (Disclaimer: This is a fake ban).
+	**{}aboose** for aboose.
+	**{}-ban** to ban people (Disclaimer: This is a fake ban).
+	**{}-calc** to calculate some equations!
+	**{}cat** to create a cat.
+	**{}coinflip** to flip a coin.
+	**{}dog** to create a dog.
+	**{}exposed** for exposed.
+	**{}eat** for your own bag of chips.
+	**{}happy** (new command, credits to Tani)
+	**{}lenny** to make a lenny face.
+	**{}-quote** to get a quote of a message.
+	**{}rekt** to make some people get rekt.
+	**{}roll** to roll some dice.
 	  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
       ***Restricted/Whitelisted commands***
-      {}emojiban to external-emoji ban someone.
-      {}botpanic to force shutdown (Auto restart)`.replace(/{}/g, prefix));
+	{}botpanic to force shutdown (Auto restart)
+	{}emojiban to external-emoji ban someone.`.replace(/{}/g, prefix));
   }
 };
