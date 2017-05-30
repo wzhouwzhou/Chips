49166f19aeeab478cdd6194f25e4c13a72737b8e
William <wzhouwzhou@gmail.com>
2017-05-29 20:13:50 -0400

    Fix json parsing

--- commands/boobs.js
+++ commands/boobs.js
@@ -3,7 +3,8 @@ const got = require('got');
 getBoobs = (callback) => {
   got('http://api.oboobs.ru/boobs/noise/10732').then(res => {
     try {
-      callback(undefined, JSON.parse(res.body).file);
+      let length =  JSON.parse(res.body).length;
+      callback(undefined, JSON.parse(res.body)[_.random(0,length)].preview);
     } catch (err) {
       callback(err);
     }
@@ -13,14 +14,16 @@ getBoobs = (callback) => {
 module.exports = {
   name: "boobs",
   perm: ["server.nsfw"],
-  async func(msg, { send, channel }) {
-    let emb = new Discord.RichEmbed();
-    if(channel.nsfw)
+  async func(msg, { member, send, channel }) {
+    let emb = new Discord.RichEmbed().setColor(member.displayColor);
+    if(channel.nsfw){
+      b='http://media.oboobs.ru/'+b;
       return getBoobs((a,b)=>{
         emb.setImage(b);
         send(' ', {embed: emb});
       });
-    else 
+    }
+    else
       return ("No NSFW commands are allowed in this channel!");
   }
 };
