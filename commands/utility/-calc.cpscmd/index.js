a707e0c544a21832bca75679339b93ce58fa667b
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:46:54 -0400

    Update index.js

--- commands/utility/-calc.cpscmd/index.js
+++ commands/utility/-calc.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let calc = require('./-calc');
+
 console.log('[CPSCMD][MODERATION][calc] Building objects...');
 calc.category = require('../').category;
 
 calc.description = 'This command simplifies expressions and solves equations!';
+
+calc.usage = '-calc <expression>';
+
+calc.example = '-calc 1+1';
 
 console.log('[CPSCMD][UTILITY][calc] Build objects complete!');
 module.exports = [
