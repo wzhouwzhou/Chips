2a4e807557f93d4e096b7ba551cff662dd284e7c
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:36:14 -0400

    Update index.js

--- commands/nsfw/nsfw.cpscmd/index.js
+++ commands/nsfw/nsfw.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let nsfw = require('./nsfw');
+
 console.log('[CPSCMD][NSFW][nsfw] Building objects...');
 nsfw.category = require('../').category;
 
 nsfw.description = 'This command lists Chips\' nsfw-related commands.';
+
+nsfw.usage = 'nsfw';
+
+nsfw.example = 'nsfw';
 
 console.log('[CPSCMD][NSFW][nsfw] Build objects complete!');
 module.exports = [
