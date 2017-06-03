8e53388078f25c77d3aa2276a415c494a0c58bad
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:10:31 -0400

    Update index.js

--- commands/info/discordstatus.cpscmd/index.js
+++ commands/info/discordstatus.cpscmd/index.js
@@ -3,6 +3,8 @@ let discordstatus = require('./discordstatus');
 console.log('[CPSCMD][INFO][discordstatus] Building objects...');
 discordstatus.category = require('../').category;
 discordstatus.description = 'Checks Discord status, getting data straight from status.discordapp.com!';
+discordstatus.usage = 'discordstatus';
+discordstatus.example = 'discordstatus';
 console.log('[CPSCMD][INFO][discordstatus] Build objects complete!');
 module.exports = [
   [discordstatus.name,discordstatus]
