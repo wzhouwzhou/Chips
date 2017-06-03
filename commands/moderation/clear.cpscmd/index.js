21d431041821c1aa8bca345df3fce5560405c0ad
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:27:21 -0400

    Update index.js

--- commands/moderation/clear.cpscmd/index.js
+++ commands/moderation/clear.cpscmd/index.js
@@ -1,8 +1,15 @@
 
 let clear = require('./clear');
+
 console.log('[CPSCMD][MODERATION][clear] Building objects...');
 clear.category = require('../').category;
+
 clear.description = 'This command clears chat messages!';
+
+clear.usage = 'clear <number 0-100>';
+
+clear.description = 'clear 22';
+
 console.log('[CPSCMD][MODERATION][clear] Build objects complete!');
 module.exports = [
   [clear.name,clear]
