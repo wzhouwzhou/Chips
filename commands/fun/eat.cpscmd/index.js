466a656d4deb6533d324aa97d95fbdeee1ca0074
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:18:55 -0400

    Update index.js

--- commands/fun/eat.cpscmd/index.js
+++ commands/fun/eat.cpscmd/index.js
@@ -2,9 +2,9 @@
 let eat = require('./eat');
 console.log('[CPSCMD][FUN][eat] Building objects...');
 eat.category = require('../').category;
-
 eat.description = 'This command gives you your own bag of Chips!';
-
+eat.usage = 'eat';
+eat.example = 'eat';
 console.log('[CPSCMD][FUN][eat] Build objects complete!');
 module.exports = [
   [eat.name,eat],
