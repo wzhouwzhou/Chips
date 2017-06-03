079f5c9b3c598a8948f7d097e23b2cf7aad8690d
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:37:43 -0400

    Update index.js

--- commands/nsfw/ass.cpscmd/index.js
+++ commands/nsfw/ass.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let ass = require('./ass');
+
 console.log('[CPSCMD][NSFW][ass] Building objects...');
 ass.category = require('../').category;
 
 ass.description = 'The name of this command is self explanatory.';
+
+ass.usage = 'ass';
+
+ass.example = 'ass';
 
 console.log('[CPSCMD][NSFW][ass] Build objects complete!');
 module.exports = [
