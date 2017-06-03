c7e637c56e7628783c563f54a49063e4dc0b192b
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:25:53 -0400

    Update index.js

--- commands/moderation/chipsprefix.cpscmd/index.js
+++ commands/moderation/chipsprefix.cpscmd/index.js
@@ -1,8 +1,15 @@
 
 let chipsprefix = require('./chipsprefix');
+
 console.log('[CPSCMD][MODERATION][chipsprefix] Building objects...');
 chipsprefix.category = require('../').category;
+
 chipsprefix.description = 'This command lets you set Chips\' prefix!';
+
+chipsprefix.usage = 'chipsprefix set <new prefix>';
+
+chipsprefix.example = 'chipsprefix set c!';
+
 console.log('[CPSCMD][MODERATION][chipsprefix] Build objects complete!');
 module.exports = [
   [chipsprefix.name,chipsprefix]
