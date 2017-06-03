4a3a9a81e4560cc57a6d3fbb7c795817242d4750
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:14:18 -0400

    Update index.js

--- commands/info/ping.cpscmd/index.js
+++ commands/info/ping.cpscmd/index.js
@@ -1,10 +1,15 @@
 
 let ping = require('./ping');
+
 console.log('[CPSCMD][INFO][ping] Building objects...');
 ping.category = require('../').category;
+
 ping.description = 'Gives ping for various actions with chips, and also rates a weighted average!';
+
 ping.usage = 'ping <no args>';
+
 ping.example = 'ping';
+
 console.log('[CPSCMD][INFO][ping] Build objects complete!');
 module.exports = [
   [ping.name,ping]
