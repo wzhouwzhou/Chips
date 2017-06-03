89a2fbc602d83deaabfb978db6474e94c2a74bb8
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:17:05 -0400

    Update index.js

--- commands/fun/random.cpscmd/index.js
+++ commands/fun/random.cpscmd/index.js
@@ -1,15 +1,20 @@
 
 let dice = require('./roll');
 let coin = require('./coinflip');
+
 console.log('[CPSCMD][FUN][random] Building objects...');
 dice.category = require('../').category;
 coin.category = require('../').category;
+
 dice.description = 'This command rolls some dice!';
 coin.description = 'This command flips a coin!';
+
 dice.usage = 'dice <number1>d<number2>';
 coin.usage = 'coinflip';
+
 dice.example = 'dice 12d24';
 coin.example = 'coinflip';
+
 console.log('[CPSCMD][FUN][random] Build objects complete!');
 module.exports = [
   [dice.name,dice],
