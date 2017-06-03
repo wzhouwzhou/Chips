f660d91974012c0fb1f6bd03c2677731b2ff3f88
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:12:30 -0400

    Update index.js

--- commands/info/quote.cpscmd/index.js
+++ commands/info/quote.cpscmd/index.js
@@ -1,10 +1,15 @@
 
 let quote = require('./quote');
+
 console.log('[CPSCMD][INFO][quote] Building objects...');
 quote.category = require('../').category;
+
 quote.description = 'Quotes a message from someone else in the current channel.';
+
 quote.usage = ' quote <message id>';
+
 quote.example = 'quote 1234567890';
+
 console.log('[CPSCMD][INFO][quote] Build objects complete!');
 module.exports = [
   [quote.name,quote]
