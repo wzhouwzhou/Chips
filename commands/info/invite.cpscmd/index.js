997576e62237af798801358268648d3db9ec35ba
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:07:43 -0400

    Update index.js

--- commands/info/invite.cpscmd/index.js
+++ commands/info/invite.cpscmd/index.js
@@ -3,6 +3,8 @@ let invite = require('./invite');
 console.log('[CPSCMD][INFO][invite] Building objects...');
 invite.category = require('../').category;
 invite.description = 'Gives Chips\' invite code';
+quote.usage = 'invite';
+quote.example = 'invite';
 console.log('[CPSCMD][INFO][invite] Build objects complete!');
 module.exports = [
   [invite.name,invite]
