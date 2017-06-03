0ed56a8042743f3b24ddfb8032387aa8ee9b8ae2
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:36:59 -0400

    Update index.js

--- commands/nsfw/boobs.cpscmd/index.js
+++ commands/nsfw/boobs.cpscmd/index.js
@@ -5,6 +5,10 @@ boobs.category = require('../').category;
 
 boobs.description = 'The name of this command is self explanatory.';
 
+boobs.usage = 'boobs';
+
+boobs.example = 'boobs';
+
 console.log('[CPSCMD][NSFW][boobs] Build objects complete!');
 module.exports = [
   [boobs.name,boobs],
