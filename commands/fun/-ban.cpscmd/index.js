6a4e2fca132ea5734b0b0f52cbb3773baa0f1ee3
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:14:19 -0400

    Update index.js

--- commands/fun/-ban.cpscmd/index.js
+++ commands/fun/-ban.cpscmd/index.js
@@ -2,9 +2,9 @@
 let fakeban = require('./-ban');
 console.log('[CPSCMD][FUN][fakeban] Building objects...');
 fakeban.category = require('../').category;
-
 fakeban.description = 'This command fake bans someone!';
-
+fakeban.usage = '-ban <user> <reason>';
+fakeban.example = '-ban @周珺 • WillyZ#6686 Letting Chips be eaten';
 console.log('[CPSCMD][FUN][fakeban] Build objects complete!');
 module.exports = [
   [fakeban.name,fakeban],
