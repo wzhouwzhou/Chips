6d78cd1f4ce710005caeb902853a719da0781b2d
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:08:19 -0400

    Update index.js

--- commands/info/help.cpscmd/index.js
+++ commands/info/help.cpscmd/index.js
@@ -3,6 +3,8 @@ let help = require('./help');
 console.log('[CPSCMD][INFO][help] Building objects...');
 help.category = require('../').category;
 help.description = 'The main help command. This command lists information about commands for the bot!';
+help.usage = 'help';
+help.example = 'help';
 console.log('[CPSCMD][INFO][help] Build objects complete!');
 module.exports = [
   [help.name,help]
