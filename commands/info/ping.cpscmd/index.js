f1d7bc758f47ad50e37a9083e016b8232ce6d8c8
William <wzhouwzhou@gmail.com>
2017-06-03 14:19:45 -0400

    Things
    All over the place but
    • Cmd usage
    • Cmd example
    • Formatting

--- commands/info/ping.cpscmd/index.js
+++ commands/info/ping.cpscmd/index.js
@@ -3,6 +3,8 @@ let ping = require('./ping');
 console.log('[CPSCMD][INFO][ping] Building objects...');
 ping.category = require('../').category;
 ping.description = 'Gives ping for various actions with chips, and also rates a weighted average!';
+ping.usage = 'ping <no args>';
+ping.example = 'ping';
 console.log('[CPSCMD][INFO][ping] Build objects complete!');
 module.exports = [
   [ping.name,ping]
--- commands/info/stats.cpscmd/index.js
+++ commands/info/stats.cpscmd/index.js
@@ -3,6 +3,8 @@ let stats = require('./stats');
 console.log('[CPSCMD][INFO][stats] Building objects...');
 stats.category = require('../').category;
 stats.description = 'Gives stats for chips including guild/user count, cpu usage, and more!';
+stats.usage = 'stats <no args>';
+stats.example = 'stats';
 console.log('[CPSCMD][INFO][stats] Build objects complete!');
 module.exports = [
   [stats.name,stats]
