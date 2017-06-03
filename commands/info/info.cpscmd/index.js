328cc352d15fe3125a30b4ed78173e467d73a1e0
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:15:17 -0400

    Update index.js

--- commands/info/info.cpscmd/index.js
+++ commands/info/info.cpscmd/index.js
@@ -1,10 +1,15 @@
 
 let info = require('./info');
+
 console.log('[CPSCMD][INFO][info] Building objects...');
 info.category = require('../').category;
+
 info.description = 'Shows server, channel, role, and user info.';
+
 info.usage = 'info <object> <extra detail>';
+
 info.example = 'info channel #general';
+
 console.log('[CPSCMD][INFO][info] Build objects complete!');
 module.exports = [
   [info.name,info]
