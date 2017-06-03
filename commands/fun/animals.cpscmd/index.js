b7526bdf4abd7b8b4275ebbd7515a781ecb9ab4d
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:15:57 -0400

    Update index.js

--- commands/fun/animals.cpscmd/index.js
+++ commands/fun/animals.cpscmd/index.js
@@ -1,15 +1,20 @@
 
 let cat = require('./cat');
 let dog = require('./dog');
+
 console.log('[CPSCMD][FUN][animals] Building objects...');
 cat.category = require('../').category;
 dog.category = require('../').category;
+
 cat.description = 'This command sends cat images!';
 dog.description = 'This command sends dog images!';
+
 cat.usage = 'cat';
 dogn.usage = 'dog';
+
 cat.example = 'cat';
 dog.example = 'dog';
+
 console.log('[CPSCMD][FUN][animals] Build objects complete!');
 module.exports = [
   [cat.name,cat],
