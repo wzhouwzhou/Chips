aaafa6241f78ca6833ccaa3cc5d952b276d25b92
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:35:01 -0400

    Update index.js

--- commands/moderation/antispam.cpscmd/index.js
+++ commands/moderation/antispam.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let blacklist = require('./-blacklist');
+
 console.log('[CPSCMD][MODERATION][antispam] Building objects...');
 blacklist.category = require('../').category;
 
 blacklist.description = 'This command lets you "blacklist" certain keywords and manage the blacklist filter in your server!';
+
+blacklist.usage = '-blacklist <word>';
+
+blacklist.example = '-blacklist eat';
 
 console.log('[CPSCMD][MODERATION][antispam] Build objects complete!');
 module.exports = [
