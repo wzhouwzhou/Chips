bcb57f84827eb3c5e2c04233a359819cda519914
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:49:25 -0400

    Update index.js

--- commands/utility/applyforstaff.cpscmd/index.js
+++ commands/utility/applyforstaff.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let staffapp = require('./applyforstaff');
+
 console.log('[CPSCMD][UTILITY][staffapp] Building objects...');
 staffapp.category = require('../').category;
 
 staffapp.description = 'This command guides you through applying for a staff role in the support server!';
+
+staffapp.usage = 'applyforstaff';
+
+staffapp.example = 'applyforstaff';
 
 console.log('[CPSCMD][UTILITY][staffapp] Build objects complete!');
 module.exports = [
