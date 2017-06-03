5f4b50b221ea1e2c80b02478ff50023a9d8c3852
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:15:03 -0400

    Update index.js

--- commands/info/support.cpscmd/index.js
+++ commands/info/support.cpscmd/index.js
@@ -1,10 +1,15 @@
 
 let support = require('./support');
+
 console.log('[CPSCMD][INFO][support] Building objects...');
 support.category = require('../').category;
+
 support.description = 'Links Chips\' support server!';
+
 support.usage = 'support';
+
 support.example = 'support';
+
 console.log('[CPSCMD][INFO][support] Build objects complete!');
 module.exports = [
   [support.name,support]
