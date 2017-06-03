ac99af7b5ce52f00e6f70a5508f002838d889d74
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:56:35 -0400

    Update index.js

--- commands/utility/stoptyping.cpscmd/index.js
+++ commands/utility/stoptyping.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let stoptyping = require('./stoptyping');
+
 console.log('[CPSCMD][UTILITY][stoptyping] Building objects...');
 stoptyping.category = require('../').category;
 
 stoptyping.description = 'This command stops the bot from typing if it is stuck!';
+
+stoptyping.usage = 'stoptyping';
+
+stoptyping.example = 'stoptyping';
 
 console.log('[CPSCMD][UTILITY][stoptyping] Build objects complete!');
 module.exports = [
