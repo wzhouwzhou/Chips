65071a9b6472624c0545c09ac84d53053ed62f5f
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:24:07 -0400

    Update index.js

--- commands/fun/say.cpscmd/index.js
+++ commands/fun/say.cpscmd/index.js
@@ -2,9 +2,9 @@
 let s = require('./s');
 console.log('[CPSCMD][FUN][say] Building objects...');
 s.category = require('../').category;
-
 s.description = 'This command lets you say something through Chips!';
-
+s.usage = 's <message>';
+s.example = 's I will eat my creators one day!';
 console.log('[CPSCMD][FUN][say] Build objects complete!');
 module.exports = [
   [s.name,s],
