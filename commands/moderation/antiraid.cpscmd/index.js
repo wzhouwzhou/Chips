4b548cc5efd189c6e07c49cc1db26cee0ab9cfc3
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:52:55 -0400

    Update index.js

--- commands/moderation/antiraid.cpscmd/index.js
+++ commands/moderation/antiraid.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let vs = require('./-vs');
+
 console.log('[CPSCMD][MODERATION][mutes] Building objects...');
 vs.category = require('../').category;
 
 vs.description = 'This command lets you verify server members, however, a verification system must be setup beforehand!';
+
+vs.usage = '-vs ok';
+
+vs.description = '-vs ok';
 
 console.log('[CPSCMD][MODERATION][mutes] Build objects complete!');
 module.exports = [
