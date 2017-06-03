8454cdb3798baa8c85bff07f3256bc706e446dfb
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:19:17 -0400

    Update index.js

--- commands/custom/points.cpscmd/index.js
+++ commands/custom/points.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let points = require('./points');
+
 console.log('[CPSCMD][UTILITY][points] Building objects...');
 points.category = require('../').category;
 
 points.description = 'This custom command shows user points in sinbad knights!';
+
+points.usage = 'points <user>';
+
+points.example = 'points @周珺 • WillyZ#6686';
 
 console.log('[CPSCMD][UTILITY][points] Build objects complete!');
 module.exports = [
