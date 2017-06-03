d68906bc17a1123dcd86f11af33d52a02af088d0
Evildeathpro <elawrence152@gmail.com>
2017-06-03 17:20:01 -0400

    Update index.js

--- commands/fun/happy.cpscmd/index.js
+++ commands/fun/happy.cpscmd/index.js
@@ -2,9 +2,9 @@
 let happy = require('./happy');
 console.log('[CPSCMD][FUN][happy] Building objects...');
 happy.category = require('../').category;
-
 happy.description = 'Be happy!';
-
+happy.usage = 'happy';
+happy.example = 'happy';
 console.log('[CPSCMD][FUN][happy] Build objects complete!');
 module.exports = [
   [happy.name,happy],
