1d75617ba47977abd50aa033f99c584e2b77a8f2
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:50:08 -0400

    Update index.js

--- commands/utility/botpanic.cpscmd/index.js
+++ commands/utility/botpanic.cpscmd/index.js
@@ -1,9 +1,14 @@
 
 let panic = require('./botpanic');
+
 console.log('[CPSCMD][UTILITY][panic] Building objects...');
 panic.category = require('../').category;
 
 panic.description = 'This command forces a shard restart!';
+
+panic.usage = 'botpanic';
+
+panic.example = 'botpanic';
 
 console.log('[CPSCMD][UTILITY][panic] Build objects complete!');
 module.exports = [
