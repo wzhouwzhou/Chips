00bdceb9257c14a6ed6f1389d3ff43f977c0e70f
Evildeathpro <elawrence152@gmail.com>
2017-06-03 18:28:55 -0400

    Update index.js

--- commands/moderation/kicks.cpscmd/index.js
+++ commands/moderation/kicks.cpscmd/index.js
@@ -1,8 +1,15 @@
 
 let kick = require('./kick');
+
 console.log('[CPSCMD][MODERATION][kick] Building objects...');
 kick.category = require('../').category;
+
 kick.description = 'This command lets you kick members!';
+
+kick.usage = 'kick <user> <reason>';
+
+kick.example = 'kick Evildeathpro✔#3981 Being a dad.';
+
 console.log('[CPSCMD][MODERATION][kick] Build objects complete!');
 module.exports = [
   [kick.name,kick]
