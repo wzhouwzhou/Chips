752b9512983087108da95c971b63164701de6aad
William <wzhouwzhou@gmail.com>
2017-06-04 00:56:44 -0400

    Translation output improvements

--- commands/utility/translate.cpscmd/translate.js
+++ commands/utility/translate.cpscmd/translate.js
@@ -5,10 +5,10 @@ const ex = {
   name: "translate",
   perm: ["global.info","global.info.all","global.info.serv","global.info.channel","global.info.role","global.info.user","global.info.user.self"],
   customperm: ['SEND_MESSAGES'],
-  async func(msg, {reply, Discord, args }) {
+  async func(msg, {reply, Discord, args, member }) {
     let inputContent = args.join(' ');
 
-    let langMatcher = /(?:target:?\s*(?:lang(?:uage):?(\s)?)?)/i.exec(inputContent)[0];
+    let langMatcher = /(\s*?(?:target):?\s*(?:lang(?:uage):?(\s)?)?)/i.exec(inputContent)[0];
     console.log('[TRANSLATE] langMatcher: '+ langMatcher);
     let langquery = inputContent.substring(inputContent.indexOf(langMatcher)+langMatcher.length);
     console.log('[TRANSLATE] langquery: '+ langquery);
@@ -19,8 +19,9 @@ const ex = {
     translate(stuffToTranslate, {to: targetlang?targetlang:'en'}).then(res => {
       let bad = new Discord.RichEmbed();
       bad.setTitle("Translation Results")
-         .addField(`Input:\n${inputContent}`,`Translated (from ${res.from.language.iso}):\n${res.text}`)
-         .setFooter(new Date().toUTCString());
+         .addField(`Input:\n${stuffToTranslate}`,`Translated (from ${res.from.language.iso}, to ${targetlang}):\n${res.text}`)
+         .setFooter(new Date().toUTCString())
+         .setColor(member?member.displayColor:1);
       return reply('',{embed: bad});
     }).catch(err => {
       console.log(err);
