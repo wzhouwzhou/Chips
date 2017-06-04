52ff013575fc788c2e108669e89d7ec024fd4531
William <wzhouwzhou@gmail.com>
2017-06-04 00:32:40 -0400

    -translate target language support

--- commands/utility/translate.cpscmd/translate.js
+++ commands/utility/translate.cpscmd/translate.js
@@ -7,15 +7,15 @@ const ex = {
   customperm: ['SEND_MESSAGES'],
   async func(msg, {reply, Discord, args }) {
     let inputContent = args.join(' ');
-    translate(inputContent, {to: 'en'}).then(res => {
+
+    let langMatcher = /(?:target:?\s*(?:lang(?:uage):?(\s)?)?)/i.exec(inputContent)[0];
+    let targetlang = inputContent.substring(inputContent.indexOf(langMatcher)+langMatcher.length).split(/\s+/)[0];
+
+    translate(inputContent, {to: targetlang?targetlang:'en'}).then(res => {
       let bad = new Discord.RichEmbed();
       bad.setTitle("Translation Results")
-         .addField(`Input:\n${inputContent}`,`Translated:\n${res.text}`)
+         .addField(`Input:\n${inputContent}`,`Translated (from ${res.from.language.iso}):\n${res.text}`)
          .setFooter(new Date().toUTCString());
-      /*  console.log(res.text);
-        //=> I speak English
-        console.log(res.from.language.iso);
-        //=> nl*/
       return reply('',{embed: bad});
     }).catch(err => {
       console.log(err);
