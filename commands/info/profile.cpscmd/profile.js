7ae44a97b95190ebc2243ee55d50fef7029fda3e
William <wzhouwzhou@gmail.com>
2017-06-06 13:31:35 -0400

    Drawing practice

--- commands/info/profile.cpscmd/profile.js
+++ commands/info/profile.cpscmd/profile.js
@@ -4,18 +4,26 @@ const fs = require('fs');
 module.exports = {
   name: "profile",
   perm: ["server.help"],
-  async func(msg, { send }) {
+  async func(msg, { author, send }) {
     try{
       let timestamp = process.hrtime();
 
-      let image = (await Jimp.read(path.join(__dirname,'../../../public/img/chipssplash.png'))).clone();
-      let filepath= `profile.${timestamp}.${image.getExtension()}`;
-      await image.write(filepath,()=>{
-        send('',{files: [filepath]}).then(_=>{
-          fs.unlinkSync(filepath);
-        });
-      });
+      let image = (await Jimp.read(path.join(__dirname,'../../../public/img/bg.png'))).clone();
+      image.gaussian( 10 );
 
+      let font = Jimp.FONT_SANS_16_BLACK;
+
+      Jimp.loadFont( font ).then(function (font) {
+        image.print(font, 50, 50, author.tag, 100);
+
+        let filepath= `profile.${timestamp}.${image.getExtension()}`;
+        image.write(filepath,()=>{
+          send('User Profile',{files: [filepath]}).then(_=>{
+            fs.unlinkSync(filepath);
+          });
+        });
+
+      });
       /*
       image = await Jimp.read(path.join(__dirname,'../../../public/image/loading.gif'));
       await send('',{files: [image]});
