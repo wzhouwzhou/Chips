ad1cc172d2f994c883207ab2654732e426ad8e7e
William <wzhouwzhou@gmail.com>
2017-06-06 14:20:36 -0400

    testing Blit

--- commands/info/profile.cpscmd/profile.js
+++ commands/info/profile.cpscmd/profile.js
@@ -14,9 +14,11 @@ module.exports = {
 
       let font = Jimp.FONT_SANS_64_BLACK;
 
-      Jimp.loadFont( font ).then(function (font) {
-        image.print(font, 400-Math.floor((author.tag.length)/2), 50, author.tag.toString(),300);
-        image.print(font, 400-Math.floor((member.displayName).length)/2, 350, member.displayName.toString(),300);
+      Jimp.loadFont( font ).then(async function (font) {
+        image.print(font, 200-Math.floor((author.tag.length)/2), 50, author.tag.toString(),300);
+        image.print(font, 200-Math.floor((member.displayName).length)/2, 100, member.displayName.toString(),300);
+        let avatar = await Jimp.read(author.displayAvatarURL(256));
+        image.blit(avatar,200,200);
         let filepath= `profile.${timestamp}.${image.getExtension()}`;
         image.write(filepath,()=>{
           send('User Profile',{files: [filepath]}).then(_=>{
