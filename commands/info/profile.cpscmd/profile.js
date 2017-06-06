c6a9fc7c84a494283f42fc01204c9aae97f54ce9
William <wzhouwzhou@gmail.com>
2017-06-06 14:58:03 -0400

    Img positioning

--- commands/info/profile.cpscmd/profile.js
+++ commands/info/profile.cpscmd/profile.js
@@ -16,9 +16,9 @@ module.exports = {
 
       Jimp.loadFont( font ).then(async function (font) {
         image.print(font, 200-Math.floor((author.tag.length)/2), 50, author.tag.toString(),300);
-        image.print(font, 200-Math.floor((member.displayName).length)/2, 100, member.displayName.toString(),300);
-        let avatar = await Jimp.read(author.avatarURL(256).replace('.webp','.png'));
-        image.blit(avatar,200,200);
+        image.print(font, 200-Math.floor((member.displayName).length)/2, 200, member.displayName.toString(),300);
+        let avatar = await Jimp.read(author.avatarURL(512).replace('.webp','.png'));
+        image.blit(avatar,200,300);
         let filepath= `profile.${timestamp}.${image.getExtension()}`;
         image.write(filepath,()=>{
           send('User Profile',{files: [filepath]}).then(_=>{
