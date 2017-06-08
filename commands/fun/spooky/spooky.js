26e040897e00c536020010b90fb45629ff2bc2c0
William <wzhouwzhou@gmail.com>
2017-06-08 15:21:55 -0400

    spooky

---
+++ commands/fun/spooky/spooky.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+  name: "spooky",
+  perm: ["server.spooky"],
+  customperm: ['SEND_MESSAGES'],
+  async func(msg, { author, member, guild, content, prefix, Discord }) {
+    let truecontent = content.substring((prefix+`profile `).length);
+    let toMatch = /(\s*?(?:num(?:ber)?):?\s*(?:rep(?:eats)?\s*:?(\s*))?)/i.exec(truecontent);
+    let numSpaces = 1;
+    if(toMatch){
+      let numMatcher = toMatch[0];
+      console.log('[SPOOKY] numMatcher: '+ numMatcher);
+      let numquery = truecontent.substring(truecontent.indexOf(langMatcher)+langMatcher.length);
+      console.log('[SPOOKY] numquery: '+ numquery);
+      numSpaces = langquery.split(/\s+/)[0];
+      console.log('[SPOOKY] numspaces: '+ numSpaces);
+      spookymsg = truecontent.replace(langMatcher&&targetlang?langMatcher+targetlang:'','');
+      spookymsg = spookymsg.length<1?' ':spookymsg;
+      console.log('[SPOOKY] stuff to convert: '+ spookymsg);
+    }
+    let s='';
+    for(let i=0;i<numSpaces;i++) s+=' ';
+    let converted = spookymsg.split(/\s*/).join(s);
+    let bad = new Discord.RichEmbed();
+    bad.setTitle('Spooky!').setColor(guild?member.displayColor:15152469);
+    bad.setAuthor(author.tag, author.displayAvatarURL.replace(/\.webp/,`.png`));
+    bad.setDescription(converted);
+  }
+};
