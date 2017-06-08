3df3b9dd49b37505dbfe9ee69a28b5125065ef75
William <wzhouwzhou@gmail.com>
2017-06-08 14:02:26 -0400

    Profile stuff

--- commands/info/profile.cpscmd/profile.js
+++ commands/info/profile.cpscmd/profile.js
@@ -1,6 +1,8 @@
 const Jimp = require('jimp');
 const fs = require('fs');
 
+const { pfpW, pfpY, pfpX } = Constants.profileSettings;
+
 module.exports = {
   name: "profile",
   perm: ["server.help"],
@@ -9,16 +11,16 @@ module.exports = {
     try{
       let timestamp = process.hrtime();
 
-      let image = (await Jimp.read(path.join(__dirname,'./profilebase.png'))).clone();
+      let image = (await Jimp.read(path.join(__dirname,'./profilebaseblank.png'))).clone();
 
       let font = path.join(__dirname,'./genericafnt/font.fnt');
 
       Jimp.loadFont( font ).then(async function (font) {
         image.print(font, 200-Math.floor((author.tag.length)/2), 50, author.tag.toString());
         image.print(font, 200-Math.floor((member.displayName).length)/2, 200, member.displayName.toString());
-        let avatar = await Jimp.read(author.avatarURL(512).replace('.webp','.png'));
-        avatar.resize(300, 300);
-        image.blit(avatar,200,300);
+        let avatar = await Jimp.read(author.avatarURL(2048).replace('.webp','.png'));
+        await avatar.resize(pfpW, pfpW);
+        await image.blit(avatar,pfpY,pfpX);
         let filepath= `${author.tag}profile${author.id}.${timestamp}.${image.getExtension()}`;
         image.write(filepath,async ()=>{
           await send('User Profile',{files: [filepath]});
