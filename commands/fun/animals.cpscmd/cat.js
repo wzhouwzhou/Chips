367cbb3378d75a4f9749d416d2bf284df42fc275
William <wzhouwzhou@gmail.com>
2017-06-12 17:07:16 -0400

    promise based cat

--- commands/fun/animals.cpscmd/cat.js
+++ commands/fun/animals.cpscmd/cat.js
@@ -1,6 +1,33 @@
 
 const got = require('got');
-getCat = (callback) => {
+getCat = () => {
+  return new Promise( (res,rej)=>{
+    got('http://www.random.cat/meow').then(res => {
+      try{
+        const f = JSON.parse(res.body).file;
+        f?res(f):rej('File not found');
+      }catch(err){
+        rej(err);
+      }
+    }).catch(rej);
+  });
+};
+
+module.exports = {
+  name: "cat",
+  perm: ["server.cat"],
+  async func(msg, { send }) {
+    try{
+      const f = await getCat();
+      await send(f);
+    }catch(err){
+      await send('The cat photographer went missing!');
+    }
+  }
+};
+
+/*
+getCat = () => {
   got('http://www.random.cat/meow').then(res => {
     try {
       callback(undefined, JSON.parse(res.body).file);
@@ -9,11 +36,4 @@ getCat = (callback) => {
     }
   }).catch(callback);
 };
-
-module.exports = {
-  name: "cat",
-  perm: ["server.cat"],
-  async func(msg, { send }) {
-    return getCat((a,b)=>send(b));
-  }
-};
+*/
