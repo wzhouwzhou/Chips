2e6a908ab4a6f78d0b0004828c347ac70cb67b6a
William <wzhouwzhou@gmail.com>
2017-06-12 10:28:23 -0400

    Fix up stats

--- setup/Constants.js
+++ setup/Constants.js
@@ -81,4 +81,23 @@ ex.profileSettings = {
   pfpX: 0,
 };
 
+ex.BOTINVITE = "https://discordapp.com/oauth2/authorize?client_id=296855425255473154&scope=bot&permissions=2146958591";
+
+ex.SUPPORTINVITE = "https://discord.gg/jj5FzF7";
+
+ex.WEBSITE = "http://www.chipsbot.tk:8080/";
+
+ex.DJS = `Useful links for learning JavaScript and Node:
+codecademy online course:| https://www.codecademy.com/learn/javascript
+Eloquent Javascript, free book:| http://eloquentjavascript.net/
+Some Node:|
+http://nodeschool.io/
+https://www.codeschool.com/courses/real-time-web-with-node-js
+discord.js getting started guides:|
+https://yorkaargh.gitbooks.io/discord-js-bot-guide/content/
+https://www.youtube.com/channel/UCvQubaJPD0D-PSokbd5DAiw/videos
+https://www.youtube.com/channel/UCLun-hgcYUgNvCCj4sIa-jA/videos
+Javascript reference/docs:| https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+discord.js documentation:| https://discord.js.org/#!/docs/`;
+
 module.exports = ex;
