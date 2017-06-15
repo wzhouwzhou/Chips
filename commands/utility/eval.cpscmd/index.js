1469bf1b3557dc2df41cce00d529a8f05d53a0ea
William <wzhouwzhou@gmail.com>
2017-06-15 12:59:03 -0400

    temp

--- commands/utility/eval.cpscmd/index.js
+++ commands/utility/eval.cpscmd/index.js
@@ -3,10 +3,13 @@ let regeval = require('./eval');
 let asynceval = require('./async');
 
 console.log('[CPSCMD][UTILITY][eval] Building objects...');
-regeval.category = require('../').category;
+regeval.metadata = {
+  category: require('../').category,
+  description: 'This command evaluates some javascript!',
+  perm: ['OWNER.*'],
+};
 asynceval.category = require('../').category;
 
-regeval.description = 'This command evaluates some javascript!';
 asynceval.description = 'This command evaluates some javascript in an async function!';
 
 console.log('[CPSCMD][UTILITY][eval] Build objects complete!');
