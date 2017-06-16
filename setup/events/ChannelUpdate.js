924687d40c7f50e17823f1cd2189b26cc3a6b375
William <wzhouwzhou@gmail.com>
2017-06-15 22:07:09 -0400

    color league channel

--- setup/events/ChannelUpdate.js
+++ setup/events/ChannelUpdate.js
@@ -6,10 +6,15 @@ module.exports = function() {
 };
 
 const checkPosition = (oldC, newC) =>{
-  if((oldC.position && newC.position)&&(oldC.position != newC.position))
+  if((oldC.position && newC.position)&&(oldC.position != newC.position)){
     if(newC.guild.id==Constants.servers.SINX){
       let bold='';
       if(Math.abs(oldC.position-newC.position)>1) bold = '**';
       client.channels.get(Constants.channels.SBKCHIPSLOGS).send(`${bold}Channel Position Update: ${newC.name.replace(/@/,'(at)')} was moved from position ${oldC.position} to ${newC.position}.${bold}`);
+    }else if(newC.guild.id=='313738223690186752'){
+      let bold='';
+      if(Math.abs(oldC.position-newC.position)>1) bold = '**';
+      client.channels.get('313757929780150272').send(`${bold}Channel Position Update: ${newC.name.replace(/@/,'(at)')} was moved from position ${oldC.position} to ${newC.position}.${bold}`);
     }
+  }
 };
