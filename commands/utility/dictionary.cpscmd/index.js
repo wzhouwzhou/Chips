4f38893cb61cffa28bdc146a1ac9628a65292b5a
William <wzhouwzhou@gmail.com>
2017-06-17 21:31:12 -0400

    client?

--- commands/utility/dictionary.cpscmd/index.js
+++ commands/utility/dictionary.cpscmd/index.js
@@ -14,11 +14,11 @@ const dictionaries = [
 ];
 
 const swears = 'https://raw.githubusercontent.com/ChaseFlorell/jQuery.ProfanityFilter/master/swearWords.json';
-dictionaries.swearlist = [];
+client.swearlist = [];
 
 needle.get(swears, function(error, response) {
   if (!error && response.statusCode == 200)
-    dictionaries.swearlist = JSON.parse(response.body);
+    client.swearlist = JSON.parse(response.body);
 });
 
 module.exports = dictionaries;
