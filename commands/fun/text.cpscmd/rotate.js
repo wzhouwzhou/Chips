7a374721092c41f31f4d9b193ca444a2563ee955
William <wzhouwzhou@gmail.com>
2017-06-21 14:59:22 -0400

    other rotate

--- commands/fun/text.cpscmd/rotate.js
+++ commands/fun/text.cpscmd/rotate.js
@@ -10,7 +10,17 @@ const backslash = input => {
       break;
   } return pushed;
 };
-
+const forslash = input => {
+  let pushed = input+'\n';
+  let split = input.split('');
+  while(true){
+    split.push(split.shift());
+    let next = split.join('');
+    pushed += next+'\n';
+    if(input.replace(/\s+/,'')==next.replace(/\s+/,''))
+      break;
+  } return pushed;
+};
 const cb = '```';
 
 module.exports = {
@@ -18,5 +28,7 @@ module.exports = {
   async func(msg, { args, send }) {
     if(args[0]==('\\'))
       send(`${cb}${backslash(_.drop(args).join(' '))}${cb}`);
+    else if(args[0]==('/'))
+      send(`${cb}${forslash(_.drop(args).join(' '))}${cb}`);
   }
 };
