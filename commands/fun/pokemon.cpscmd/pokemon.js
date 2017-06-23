043ba1eb400f69d2b0ea74c361f0bc3db3565a71
William <wzhouwzhou@gmail.com>
2017-06-22 23:58:15 -0400

    unstable - pokemon stuff

--- commands/fun/pokemon.cpscmd/pokemon.js
+++ commands/fun/pokemon.cpscmd/pokemon.js
@@ -3,6 +3,8 @@ const STARTMSG = 'Type __{prefix}start__ to begin playing or __stop__ or __cance
 const STARTCONFIRM = '{prefix}start';
 const CANCEL = 'cancel';
 const STOP = 'stop';
+
+const INPUTEXPIRE = 15000;
 const STARTWAIT = 60000;
 
 const GAMECANCEL = 'Pokemon game cancelled! To start another one do {prefix}pokemon!';
@@ -13,17 +15,27 @@ const BOOTWAIT = 1500;
 
 const SYSREADY = 'System is ready!';
 
-const ACTIONCHOICES = [
+const ACTIONCHOICESRXN = [
   '{one}Fite',
   '{two}Bag',
   '{three}Pokement',
 ].join('\n').replace('{one}',Constants.CHOICES[1]).replace('{two}',Constants.CHOICES[2]).replace('{three}',Constants.CHOICES[3]);
 
-const ACTIONSEMB = new Discord.RichEmbed().setTitle('Choose an action!').setDescription(ACTIONCHOICES);
+const ACTIONCHOICESTEXT = [
+  '**__Fite__**',
+  '**__Bag__**',
+  '**__Pokement__**',
+].join('\n');
+
+const ACTIONSEMBRXN = new Discord.RichEmbed().setTitle('Choose an action!').setDescription(ACTIONCHOICESRXN);
+const ACTIONSEMBTEXT = new Discord.RichEmbed().setTitle('Choose an action!').setDescription(ACTIONCHOICESTEXT);
 
 module.exports = {
   name: "pokemon",
-  async func(msg, { reply, prefix, author, channel, delay }) {
+  async func(msg, ctx = { reply, prefix, author, channel, delay }) {
+
+    let rxn = await rxnInputPrompt(msg, ctx);
+    if(!rxn) return reply('Sorry, text based input is currently not available');
 
     let beginmsg = await reply (STARTMSG.replace('{prefix}',prefix));
     let startInit = false;
@@ -55,10 +67,48 @@ module.exports = {
 
     await initialisationMsg.edit(SYSREADY);
 
-    let choiceChooser = await initialisationMsg.reply('', { embed: ACTIONSEMB });
+    let choiceChooser = await initialisationMsg.reply('', { embed: ACTIONSEMBRXN });
 
     for(let i = 1; i <= 3; i++)
       await choiceChooser.react(Constants.CHOICES[i]);
 
   }
+};
+
+const rxnInputPrompt = (msg, { reply, author, channel }) => {
+  return new Promise ( res => {
+    reply('Would you like to play by using message reactions (fancy) or just typing (good for mobile)? React or type __y__es or __n__o').then(async sentmsg => {
+      let mCol, confirmed = false;
+
+      let rC = sentmsg.createReactionCollector(
+        (r, u) => {
+          if(u.id != author.id) return false;
+          if(r.emoji.toString() == Constants.emojis.CHECK&& (!confirmed)){
+            mCol.stop();
+            res(true);
+            return true;
+          }
+      },
+      { max: 1, time: INPUTEXPIRE, errors: ['time'] } );
+
+      mCol = channel.createMessageCollector(
+        query => { if(query.content) if(/^(?:y(?:es)?)|(?:no?)$/i.test(query.content))
+          if((!temp.confirmed&&!temp.agreed)&&query.author.id==author.id){
+            temp.confirmed = true;
+            temp.agreed = /^(?:y(?:es)?)$/i.test(query.content);
+            reply(`Accepted response, continuing: ${temp.agreed}`).then(m=>{
+              setTimeout(()=>m.delete(),1000);
+            });
+            temp.usedmsg = true;
+            msgCol.stop();
+            return true;
+          }
+        },
+        { max: 1, time: EXPIRE, errors: ['time'] }
+      );
+
+      channel.createMessageCollector();
+      res(true);
+    });
+  });
 };
