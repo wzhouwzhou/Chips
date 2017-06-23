da6ecd4326f50285e41e11d1b07aa63684ecd57f
William <wzhouwzhou@gmail.com>
2017-06-23 13:37:46 -0400

    move around things

--- commands/fun/pokemon.cpscmd/pokemon.js
+++ commands/fun/pokemon.cpscmd/pokemon.js
@@ -33,45 +33,11 @@ const ACTIONSEMBTEXT = new Discord.RichEmbed().setTitle('Choose an action!').set
 module.exports = {
   name: "pokemon",
   async func(msg, ctx) {
-    let { reply, prefix, author, channel, delay } = ctx;
+    let { reply } = ctx;
+
     let rxn = await rxnInputPrompt(msg, ctx);
     if(!rxn) return reply('Sorry, text based input is currently not available');
-
-    let beginmsg = await reply(STARTMSG.replace('{prefix}',prefix));
-    let startInit = false;
-
-    const startFilter = (m) => {
-      if(m.author.id != author.id) return false;
-      if(m.content.toLowerCase() == STARTCONFIRM.replace('{prefix}',prefix).toLowerCase()){
-        startInit = true;
-        return true;
-      }else if (m.content.toLowerCase() == CANCEL || m.content.toLowerCase() == STOP ) return true;
-      return false;
-    };
-    let startCol;
-    try{
-      startCol = await channel.awaitMessages(startFilter, { max: 1, time: STARTWAIT, errors: ['time'] });
-    }catch(startCCollected){
-      return reply(GAMECANCEL.replace('{prefix}',prefix));
-    }
-    let agreedmsg = startCol.first();
-    await beginmsg.delete();
-
-    if(!startInit) return reply(GAMECANCEL.replace('{prefix}',prefix));
-
-    let initialisationMsg = await agreedmsg.reply(BOOT);
-    for(let i = 0; i<BOOTLOOP; i++){
-      await initialisationMsg.edit(initialisationMsg.content+'.');
-      await delay(BOOTWAIT);
-    }
-
-    await initialisationMsg.edit(SYSREADY);
-
-    let choiceChooser = await initialisationMsg.reply('', { embed: ACTIONSEMBRXN });
-
-    for(let i = 1; i <= 3; i++)
-      await choiceChooser.react(Constants.CHOICES[i]);
-
+    else await playGameRXN(msg, ctx);
   }
 };
 
@@ -125,4 +91,41 @@ const rxnInputPrompt = (msg, { reply, author, channel }) => {
 
     });
   });
+};
+
+const playGameRXN = async (msg, { reply, prefix, author, channel, delay }) => {
+  let beginmsg = await reply(STARTMSG.replace('{prefix}',prefix));
+  let startInit = false;
+
+  const startFilter = (m) => {
+    if(m.author.id != author.id) return false;
+    if(m.content.toLowerCase() == STARTCONFIRM.replace('{prefix}',prefix).toLowerCase()){
+      startInit = true;
+      return true;
+    }else if (m.content.toLowerCase() == CANCEL || m.content.toLowerCase() == STOP ) return true;
+    return false;
+  };
+  let startCol;
+  try{
+    startCol = await channel.awaitMessages(startFilter, { max: 1, time: STARTWAIT, errors: ['time'] });
+  }catch(startCCollected){
+    return reply(GAMECANCEL.replace('{prefix}',prefix));
+  }
+  let agreedmsg = startCol.first();
+  await beginmsg.delete();
+
+  if(!startInit) return reply(GAMECANCEL.replace('{prefix}',prefix));
+
+  let initialisationMsg = await agreedmsg.reply(BOOT);
+  for(let i = 0; i<BOOTLOOP; i++){
+    await initialisationMsg.edit(initialisationMsg.content+'.');
+    await delay(BOOTWAIT);
+  }
+
+  await initialisationMsg.edit(SYSREADY);
+
+  let choiceChooser = await msg.reply('', { embed: ACTIONSEMBRXN });
+
+  for(let i = 1; i <= 3; i++)
+    await choiceChooser.react(Constants.CHOICES[i]);
 };
