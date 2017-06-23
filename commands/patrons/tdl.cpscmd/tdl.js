034961987be316166f53cd1fdfb82e2f942c077c
Evildeathpro <elawrence152@gmail.com>
2017-06-23 07:12:53 -0400

    Update tdl.js
    Made the Title into and Author field
    Gave the Author field an image
    Changed up the Description

--- commands/patrons/tdl.cpscmd/tdl.js
+++ commands/patrons/tdl.cpscmd/tdl.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "tdl",
   async func(msg, { send, Discord }) {
-    const embed = new Discord.RichEmbed().setTitle('The_dark_lurch#1306').setDescription('The first Donator of Chips, <3 for The Dark Lurch! :3').setColor(16776960);
+    const embed = new Discord.RichEmbed().setAuthor('The_dark_lurch#1306', "https://lh3.googleusercontent.com/-8x1vUlUclhg/WUz3PAXGQRI/AAAAAAAAB6U/A6lQvkuWPR0vmcPyF09tnpBpk_u9Y4r0QCJoC/s128-p-rw/b152ae7660dd8c0a69f4fc6877e04aef.png") .setTitle('').setDescription('The first Donator of Chips, :heart: The Dark Lurch! :3').setColor(16776960);
     send('', { embed });
   }
 };
