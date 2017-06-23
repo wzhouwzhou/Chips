4abd3e1ed26c5767e34e69123fea0438378b2241
William <wzhouwzhou@gmail.com>
2017-06-22 23:55:51 -0400

    fmt

--- commands/moderation/chipsprefix.cpscmd/chipsprefix.js
+++ commands/moderation/chipsprefix.cpscmd/chipsprefix.js
@@ -11,7 +11,7 @@ ex = {
     console.log("[Custom prefix] Action: "+action);
 
     if(action=="status"){
-      return reply(`The prefix for this server is: ${customprefix[guild.id]?customprefix[guild.id]:prefix}`);
+      return reply(`The prefix for this server is: \`\`${customprefix[guild.id]?customprefix[guild.id]:prefix}\`\``);
     }else if(action=="set"){
       if (!args[1]) return reply("No custom prefix given to set!");
       else{
@@ -20,18 +20,18 @@ ex = {
         filter=require(path.join(__dirname, '../../../handlers', 'Filter'))();
         console.log(`${member.user.tag} changed prefix for guild ${guild.id}: ${newprefix}`);
         database.sheets['prefixes'].addRow({guildid: guild.id, prefix: newprefix});
-        return reply(`Prefix ${newprefix} set as my prefix successfully!`);
+        return reply(`Prefix \`\`${newprefix}\`\` set as my prefix successfully!`);
       }
     }else if(action=="reset"||action=="off"){
       if(!customprefix[guild.id]||customprefix[guild.id]==prefix) return reply(`Custom prefix is not enabled! Set a custom prefix for me with \`\`${prefix}chipsprefix set\`\``);
       customprefix[guild.id]=prefix;
       database.sheets['prefixes'].addRow({guildid: guild.id, prefix: prefix});
-      return reply(`Custom prefix reset! My prefix is now ${customprefix[guild.id]}`);
+      return reply(`Custom prefix reset! My prefix is now \`\`${customprefix[guild.id]}\`\``);
     }else if(action=="on"){
       if((!customprefix[guild.id])||customprefix[guild.id]==prefix)
         return reply(`Set a custom prefix for me with \`\`${prefix}chipsprefix set\`\``);
       else
-        return reply(`A prefix has already been set for this server! Check it with \`\`${customprefix[guild.id]}chipsprefix (status)\`\``);
+        return reply(`A prefix has already been set for this server! Check it with \`\`${customprefix[guild.id]}chipsprefix status\`\``);
     }
   }
 };
