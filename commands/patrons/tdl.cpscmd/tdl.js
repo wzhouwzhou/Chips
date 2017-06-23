5eaa333509269c27fa772280f904f7b33bf7ac86
Evildeathpro <elawrence152@gmail.com>
2017-06-23 01:37:08 -0400

    Update tdl.js

--- commands/patrons/tdl.cpscmd/tdl.js
+++ commands/patrons/tdl.cpscmd/tdl.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "tdl",
   async func(msg, { send, Discord }) {
-    const embed = new Discord.RichEmbed().setTitle('The_dark_lurch#1306').setDescription('The first Donator of Chips, thank you for being the first ever supporter The Dark Lurch! :3').setColor(16776960);
+    const embed = new Discord.RichEmbed().setTitle('The_dark_lurch#1306').setDescription('The first Donator of Chips, <3 for The Dark Lurch! :3').setColor(16776960);
     send('', { embed });
   }
 };
