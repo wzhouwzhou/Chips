03a1ab21adb932724ee21759c00fae8abb1692a5
Evildeathpro <elawrence152@gmail.com>
2017-06-25 19:06:16 -0400

    Update ChannelUpdate.js

--- setup/events/ChannelUpdate.js
+++ setup/events/ChannelUpdate.js
@@ -7,7 +7,7 @@ module.exports = function() {
 
 const checkPosition = (oldC, newC) =>{
   if((oldC.position && newC.position)&&(oldC.position != newC.position)){
-    if(newC.guild.id==Constants.servers.SINX){
+    if(newC.guild.id==Constants.servers.SURSKIT){
       let bold='';
       if(Math.abs(oldC.position-newC.position)>1) bold = '**';
       client.channels.get(Constants.channels.SBKCHIPSLOGS).send(`${bold}Channel Position Update: ${newC.name.replace(/@/,'(at)')} was moved from position ${oldC.position} to ${newC.position}.${bold}`);
