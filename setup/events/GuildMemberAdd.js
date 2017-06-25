dc019a80bcf0530f242c6550660f4237fce7509f
Evildeathpro <elawrence152@gmail.com>
2017-06-25 14:28:23 -0400

    Update GuildMemberAdd.js

--- setup/events/GuildMemberAdd.js
+++ setup/events/GuildMemberAdd.js
@@ -22,16 +22,16 @@ module.exports = function() {
       try {
         if(memberguild.id=="257889450850254848"){
           setTimeout(() =>{
-            console.log("[SINX] adding role...");
+            console.log("[SURSKIT] adding role...");
             member.addRole(memberguild.roles.get("305302877641900052")||memberguild.roles.find('name',"Unverified"));
-            console.log("[SINX] sending welcome msg...");
+            console.log("[SURSKIT] sending welcome msg...");
             let welcomeC=memberguild.channels.get("314407824568614913")||memberguild.channels.find('name','unverified');
             welcomeC.send(`<@${userid}>, Welcome to Sinbadx Knights! **If you would like to get verified and be able to speak in the other channels, please answer the following questions!**
               1. How did you hear about this server?
               2. Why did you join this server?
               3. Do you promise to read <#297263352252727296>?
               4. What is your favorite diep.io tank?
-  (you can answer these with just a sentence or two, no need to write an essay!)`).then(console.log("[SINX] Welcome msg sent")).catch(err=>console.log('welcome msg err:[sinx] '+ err));
+  (you can answer these with just a sentence or two, no need to write an essay!)`).then(console.log("[SURSKIT] Welcome msg sent")).catch(err=>console.log('welcome msg err:[sinx] '+ err));
           }, 500);
         /*}else if(memberguild.id=="252525368865456130"){
           setTimeout(() => {
@@ -43,16 +43,16 @@ module.exports = function() {
           }, 700);*/
         }else if(memberguild.id=="315891125825044482"){
           setTimeout(() =>{
-            console.log("[SK2] adding role...");
+            console.log("[SUCKX2] adding role...");
             member.addRole(memberguild.roles.get("316017088160595970")||memberguild.roles.find('name',"unverified"));
-            console.log("[SK2] sending welcome msg...");
+            console.log("[SUCKX2] sending welcome msg...");
             let welcomeC=client.channels.get("307342989783728131")||memberguild.channels.find('name','unverified');
             welcomeC.send(`<@${userid}>, Welcome to Sunk Nights! **If you would like to get verified and be able to speak in the other channels, please answer the following questions!**
               1. How did you hear about this server?
               2. Why did you join this server?
               3. Do you promise to read <#316019707276820483>?
               4. What is your favorite diep.io tank?
-              (you can answer these with just a sentence or two, no need to write an essay!)`).then(console.log("[SK2] Welcome msg sent"));
+              (you can answer these with just a sentence or two, no need to write an essay!)`).then(console.log("[SUCKX2] Welcome msg sent"));
           }, 500);
         }else if(memberguild.id=="315502587111669772"){
           setTimeout(()=>{
