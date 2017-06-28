92af0fce6a2ecc60807dd89bdffef30eef6d0079
William <wzhouwzhou@gmail.com>
2017-06-28 14:38:59 -0400

    a bit of logic

--- commands/fun/random.cpscmd/randombuild.js
+++ commands/fun/random.cpscmd/randombuild.js
@@ -1,14 +1,19 @@
 const UPGRADEPTS = 33;
 const PATHS = 8;
 let smasher = [0,1,2,7];
-
+let MAX1=8;
+let MAX2=10;
 module.exports = {
   name: "randombuild",
   async func(msg, { reply, content }) {
     let build = new Array(PATHS).fill(0);
-
-    for(let i = 0; i < UPGRADEPTS; i++)
-      (~content.toLowerCase().indexOf('smasher')&&(!(~content.toLowerCase().indexOf('auto'))))?build[_.sample(smasher)]++:build[_.random(0,build.length-1)]++;
+    let smasher = content.toLowerCase().indexOf('smasher')>-1;
+    let auto = content.toLowerCase().indexOf('auto')>-1;
+    for(let i = 0; i < UPGRADEPTS; i++){
+      let ind = (smasher&&!auto)?_.sample(smasher):_.random(0,build.length-1);
+      if(smasher&&build[ind]>=MAX2||!smasher&&build[ind]>=MAX1) --i;
+      else ++build[ind];
+    }
 
     return reply(build.join('/'));
   }
