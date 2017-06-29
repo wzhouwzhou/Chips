13cffc5d4adf46eea0c41c54bdf26ed8188c67cf
William <wzhouwzhou@gmail.com>
2017-06-29 16:04:07 -0400

    :/

--- commands/utility/eval.cpscmd/python.js
+++ commands/utility/eval.cpscmd/python.js
@@ -3,9 +3,9 @@ const cb = '```';
 module.exports = {
   name: "python",
   async func(msg, { reply, args }) {
-    let output = await new Promise( (res, rej) =>
+    let output = await new Promise( res =>
       childProcess.exec(`python3 -c "${args.join(' ').replace(/\\n/, '\\\\n').replace(/\n/g, '\\n').replace(/"/g, '\\"').replace(/'/g, '\\\'')}"`, (err, sout, serr) => {
-        return (err || serr)? rej(err || serr):res(sout);
+        return (err || serr)? res('Error:\n'+(err || serr)):res(sout);
       })
     );
     return reply('Python eval:\n'+cb+output+cb);
