c5d5de86d037b5e0b3f305e6c1c9e750b15cc2cd
William <wzhouwzhou@gmail.com>
2017-06-29 16:06:13 -0400

    fix

--- commands/utility/eval.cpscmd/python.js
+++ commands/utility/eval.cpscmd/python.js
@@ -2,12 +2,12 @@ const childProcess = require('child_process');
 const cb = '```';
 module.exports = {
   name: "python",
-  async func(msg, { reply, args }) {
+  async func(msg, { reply, content, prefix }) {
     let output = await new Promise( res =>
-      childProcess.exec(`python3 -c "${args.join(' ').replace(/\\n/, '\\\\n').replace(/\n/g, '\\n').replace(/"/g, '\\"').replace(/'/g, '\\\'')}"`, (err, sout, serr) => {
+      childProcess.exec(`python3 -c "${content.substring((prefix+'python ').length).replace(/\\n/, '\\\\n').replace(/\n/g, '\\n').replace(/"/g, '\\"').replace(/'/g, '\\\'')}"`, (err, sout, serr) => {
         return (err || serr)? res('Error:\n'+(err || serr)):res(sout);
       })
     );
-    return reply('Python eval:\n'+cb+output+cb);
+    return reply('Python eval:\n'+cb+'py'+output+cb);
   }
 };
