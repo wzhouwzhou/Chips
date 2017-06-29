05de1c9b7509a5bde2a277c99708a75deea19d9d
William <wzhouwzhou@gmail.com>
2017-06-29 15:58:00 -0400

    python

--- commands/utility/eval.cpscmd/index.js
+++ commands/utility/eval.cpscmd/index.js
@@ -1,6 +1,7 @@
 
 let regeval = require('./eval');
 let asynceval = require('./async');
+let pythoneval = require('./python');
 
 console.log('[CPSCMD][UTILITY][eval] Building objects...');
 regeval.metadata = {
@@ -14,9 +15,15 @@ asynceval.metadata = {
   description: 'This command evaluates some javascript in an async function!',
   perm: [['OWNER.owner.eval.async']],
 };
+pythoneval.metadata = {
+  category: require('../').category,
+  description: 'This command evaulates some python',
+  perm: [['OWNER.owner.eval.eval']],
+};
 
 console.log('[CPSCMD][UTILITY][eval] Build objects complete!');
 module.exports = [
   [regeval.name,regeval],
   [asynceval.name,asynceval],
+  [pythoneval.name,pythoneval],
 ];
---
+++ commands/utility/eval.cpscmd/python.js
@@ -0,0 +1,13 @@
+const childProcess = require('child_process');
+const cb = '```';
+module.exports = {
+  name: "python",
+  async func(msg, { reply }) {
+    let output = await new Promise( (res, rej) =>
+      childProcess.exec(`python -c ${args.join(' ')}`, (err, sout, serr) => {
+        return (err || serr)? rej(err || serr):res(sout);
+      })
+    );
+    return reply(cb+output+cb);
+  }
+};
