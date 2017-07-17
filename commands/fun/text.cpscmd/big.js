495e77a2d60069ef6030038ed9e6755ca234a46c
William <wzhouwzhou@gmail.com>
2017-07-17 01:48:38 -0400

    Check if no emoji is given for big

--- commands/fun/text.cpscmd/big.js
+++ commands/fun/text.cpscmd/big.js
@@ -7,32 +7,36 @@ module.exports = {
   async func( msg,
   {
     send,
-    reply
+    reply,
+    prefix,
   } ) {
     const potential = msg.content;
     let str = potential.match(/<:[\w0-9_]+:\d+>/g);
-    if(str&&str[0]) {
-      let id = str[0].substring(1+str[0].lastIndexOf(':'),str[0].length-1);
-      let fetched= await snekfetch.get(`https://cdn.discordapp.com/emojis/${id}.png`);
-      if(fetched&&fetched.body)
-        return send(' ',{files: [{attachment: fetched.body}]});
-      return reply('No emoji image found');
-    }else {
-      str = potential;
-      if(str&&str[0]){
-        let parsed = twe.parse(str).toString().match(/src="([\w|\d|:|\/|.]+")/);
-        if(parsed&&parsed[0]){
-          let url = parsed[0].substring('src="'.length, parsed[0].length-1);
-          let fetched;
-          if(url)
-            fetched= await snekfetch.get(url);
-          if(!fetched||!fetched.body)
-            return reply('No emoji image found');
+    if(str.replace(new RegExp(`${prefix}big\\s*`),'').length != 0) {
+      if(str[0]) {
+        let id = str[0].substring(1+str[0].lastIndexOf(':'),str[0].length-1);
+        let fetched= await snekfetch.get(`https://cdn.discordapp.com/emojis/${id}.png`);
+        if(fetched&&fetched.body)
           return send(' ',{files: [{attachment: fetched.body}]});
-        }
         return reply('No emoji image found');
+      } else {
+        str = potential;
+        if(str&&str[0]){
+          let parsed = twe.parse(str).toString().match(/src="([\w|\d|:|\/|.]+")/);
+          if(parsed&&parsed[0]){
+            let url = parsed[0].substring('src="'.length, parsed[0].length-1);
+            let fetched;
+            if(url)
+              fetched= await snekfetch.get(url);
+            if(!fetched||!fetched.body)
+              return reply('No emoji image found');
+            return send(' ',{files: [{attachment: fetched.body}]});
+          }
+          return reply('No emoji image found');
+        }
       }
-    }
     return reply('No emoji was given');
+    }
+  return reply('No emoji given');
   }
 };
