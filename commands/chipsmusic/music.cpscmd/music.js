9847566714ffd960f75eec445649e707530a5183
William Zhou <wzhouwzhou@gmail.com>
2017-07-24 11:41:29 +0800

    Check voice channel

--- commands/chipsmusic/music.cpscmd/music.js
+++ commands/chipsmusic/music.cpscmd/music.js
@@ -5,14 +5,16 @@ const _handlers = new Map();
 
 module.exports = {
   name: "music",
-  async func(msg, { args, guild, client }) {
+  async func(msg, { args, guild, client, member, reply }) {
     let h;
     if(_handlers.has(guild.id)) h = _handlers.get(guild.id);
     else{
       h = new Handler(guild.id, client);
       _handlers.set(guild.id, h);
     }
-    if(args&&args[0]==='demo')
+    if(args&&args[0]==='demo'){
+      if(!member.voiceChannel) return reply('Please join a voice channel first');
       h.startDemo(member.voiceChannel,channel);
+    }
   }
 };
