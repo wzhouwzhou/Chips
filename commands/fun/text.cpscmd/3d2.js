313053d660a01d2e375e72e80b0ed6ece926850d
William Zhou <wzhouwzhou@gmail.com>
2017-07-26 16:04:17 +0800

    Identify who called 3d

--- commands/fun/text.cpscmd/3d2.js
+++ commands/fun/text.cpscmd/3d2.js
@@ -6,18 +6,22 @@ const COOLDOWN = 60*1000;
 
 module.exports = {
   name:'3d2',
-  async func(msg, { send, prefix, content, guild }) {
+  async func(msg, { reply, send, prefix, content, guild }) {
     const split = content.substring(`${prefix}3d2 `.length).split(/\s+/);
     if(split.length <= 0||!split.every(w=>w.length>0&&!w.match(/\s+/g)))
-      return send ('You must provide at least one word to 3dtext!');
+      return send ('You must provide at least one word to 3dtext!').then(m=>m.delete(3000));
     else if(split.length<=5)
       if(!asciiCooldown.get(guild.id)){
         asciiCooldown.set(guild.id, true);
         setTimeout(()=>asciiCooldown.set(guild.id, false), COOLDOWN);
-        return split.forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: '3-d'}, (e,r)=>send(`${cb}${r&&r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) ));
+        return split.forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: '3-d'}, (e,r) => {
+          const str = `${cb}${r&&r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`;
+          if(split.indexOf(word)===0) return reply(str);
+          return send(str);
+        }));
       }else
-        return send('Woah there this command has a 1 minute cooldown please wait before trying that again!');
+        return send('Woah there this command has a 1 minute cooldown please wait before trying that again!').then(m=>m.delete(3000));
     else
-      return send('Too many words to 3dtext!');
+      return send('Too many words to 3dtext!').then(m=>m.delete(3000));
   }
 };
