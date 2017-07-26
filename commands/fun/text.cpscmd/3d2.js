8ffa4cb8b5e9d3a6f0ccdbd5d67886540c444524
William Zhou <wzhouwzhou@gmail.com>
2017-07-26 15:31:13 +0800

    3d2 and fix 3d

--- commands/fun/text.cpscmd/3d2.js
+++ commands/fun/text.cpscmd/3d2.js
@@ -5,7 +5,7 @@ const asciiCooldown = new Map();
 const COOLDOWN = 60*1000;
 
 module.exports = {
-  name:'3d',
+  name:'3d2',
   async func(msg, { send, prefix, content, guild }) {
     const split = content.substring(`${prefix}3d2 `.length).split(/\s+/);
     if(split.length <= 0||split.some(w=>w.length<=0||w.match(/\s+/g)))
@@ -14,7 +14,7 @@ module.exports = {
       if(!asciiCooldown.get(guild.id)){
         asciiCooldown.set(guild.id, true);
         setTimeout(()=>asciiCooldown.set(guild.id, false), COOLDOWN);
-        split.forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: 'larry3d'}, (e,r)=>send(`${cb}${r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) ));
+        split.forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: '3d'}, (e,r)=>send(`${cb}${r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) ));
       }else
         send('Woah there this command has a 1 minute cooldown please wait before trying that again!');
     else
