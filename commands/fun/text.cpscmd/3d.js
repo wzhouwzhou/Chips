be2305dc973e6e40fff24c6e2cd6225b93e95c91
William Zhou <wzhouwzhou@gmail.com>
2017-07-26 08:04:50 +0800

    limit to 5

--- commands/fun/text.cpscmd/3d.js
+++ commands/fun/text.cpscmd/3d.js
@@ -4,6 +4,6 @@ const asciify = require('asciify');
 module.exports = {
   name:'3d',
   async func(msg, { send, prefix, content }) {
-    return content.substring(`${prefix}3d `.length).split(/\s+/).forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: 'larry3d'}, (e,r)=>send(`${cb}${r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) ));
+    return content.substring(`${prefix}3d `.length).split(/\s+/).length<=5?content.substring(`${prefix}3d `.length).split(/\s+/).forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: 'larry3d'}, (e,r)=>send(`${cb}${r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) )):send('Too many words to 3dtext!');
   }
 };
