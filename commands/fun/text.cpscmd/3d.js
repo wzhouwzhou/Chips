5329bb35a53cf3e2a27802d7f056e61224a1103f
William Zhou <wzhouwzhou@gmail.com>
2017-07-26 08:09:55 +0800

    Cooldown

--- commands/fun/text.cpscmd/3d.js
+++ commands/fun/text.cpscmd/3d.js
@@ -1,9 +1,17 @@
 const cb= '```';
 const asciify = require('asciify');
 
+const asciiCooldown = new Map();
+const COOLDOWN = 60*1000;
+
 module.exports = {
   name:'3d',
-  async func(msg, { send, prefix, content }) {
-    return content.substring(`${prefix}3d `.length).split(/\s+/).length<=5?content.substring(`${prefix}3d `.length).split(/\s+/).forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: 'larry3d'}, (e,r)=>send(`${cb}${r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) )):send('Too many words to 3dtext!');
+  async func(msg, { send, prefix, content, guild }) {
+    if(!asciiCooldown.get(guild.id)){
+      asciiCooldown.set(guild.id, true);
+      setTimeout(()=>asciiCooldown.set(guild.id, false), COOLDOWN);
+      content.substring(`${prefix}3d `.length).split(/\s+/).length<=5?content.substring(`${prefix}3d `.length).split(/\s+/).forEach(word=> (word&&!word.match(/\s+/))&&asciify( word, {font: 'larry3d'}, (e,r)=>send(`${cb}${r.length<1900?r:word.length<1800?'too long to 3d text:\n'+word:'something too long'}${cb}`) )):send('Too many words to 3dtext!');
+    }else
+      send('Woah there this command has a 1 minute cooldown please wait before trying that again!');
   }
 };
