876135a3d4a3288617ae3697a3cfa44fbc516116
William Zhou <wzhouwzhou@gmail.com>
2017-07-27 09:41:33 +0800

    Adding nsfw

---
+++ commands/nsfw/hentai.cpscmd/hentai.js
@@ -0,0 +1,34 @@
+const needle = require('needle');
+
+const getHentai = () => {
+  const p = _.random(1,486);
+  const i = _.random(0,9);
+  return new Promise( (res, rej) => {
+    needle.get(`https://yande.re/post.json?tags=uncensored&limit=10&page=${p}`, (e,r) =>{
+      if(e) rej(e);
+      if(r.statusCode!=200) rej(`Status ${r.statusCode}`);
+      try{
+        res( (r.body)[i]['file_url'] );
+      }catch(err) {
+        rej(err);
+      }
+    });
+  });
+};
+
+module.exports = {
+  name:'hentai',
+  async func(msg, { send, member, Discord, channel }) {
+    if(!channel.nsfw) return;
+
+    try{
+      const embed = Discord.RichEmbed().setColor(member?member.displayColor:13512).setImage(await getHentai());
+      await send('', { embed });
+    }catch(err){
+      console.error(err);
+      if(err.startsWith('Status '))
+        return send('Hentai servers are down right now...');
+      send('Something went wrong..');
+    }
+  }
+};
---
+++ commands/nsfw/hentai.cpscmd/index.js
@@ -0,0 +1,18 @@
+
+let hentai = require('./hentai');
+let autohentai;
+
+console.log('[CPSCMD][NSFW][hentai] Building objects...');
+
+hentai.metadata = {
+  category: require('../').category,
+  description: 'The name of this command is self explanatory.',
+  usage: 'hentai',
+  example: 'hentai',
+  perm: [['global.nsfw.boobs.boobs']],
+};
+
+console.log('[CPSCMD][NSFW][hentai] Build objects complete!');
+module.exports = [
+  [hentai.name,hentai],
+];
