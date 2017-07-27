ef25ff5c4f2869df884d96851da625599f0afcc1
William Zhou <wzhouwzhou@gmail.com>
2017-07-27 19:56:00 +0800

    paginator test

--- commands/fun/text.cpscmd/big.js
+++ commands/fun/text.cpscmd/big.js
@@ -1,6 +1,7 @@
 
 const snekfetch = require('snekfetch');
 const twe = require('twemoji');
+const Paginator = require('../../../rewrite-all/src/struct/client/Paginator').Paginator;
 
 module.exports = {
   name: "big",
@@ -13,6 +14,22 @@ module.exports = {
     if(msg.content.replace(new RegExp(`${prefix}big\\s*`),'').length != 0) {
       let str = msg.content.match(/<:[\w0-9_]+:\d+>/g);
       if(str&&str[0]) {
+        if(str.length>1){
+          const emojis = [];
+          str.forEach(e=>{
+            let id = e.substring(1+e.lastIndexOf(':'),e.length-1);
+            emojis.push(`https://cdn.discordapp.com/emojis/${id}.png`);
+          });
+          const p = new P ( msg,  {
+            type:'paged',
+            embedding: true,
+            image: emojis,
+            }, Discord
+          );
+          if(emojis.length>0)
+            return p.sendFirst();
+          return reply('No emoji images found');
+        }
         let id = str[0].substring(1+str[0].lastIndexOf(':'),str[0].length-1);
         let fetched= await snekfetch.get(`https://cdn.discordapp.com/emojis/${id}.png`);
         if(fetched&&fetched.body)
