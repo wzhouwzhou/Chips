8ef5b35e7f86d6455b5f8aeec2f22c9d76954558
William Zhou <wzhouwzhou@gmail.com>
2017-07-27 09:45:14 +0800

    nsfw help

--- commands/nsfw/nsfw.cpscmd/nsfw.js
+++ commands/nsfw/nsfw.cpscmd/nsfw.js
@@ -1,11 +1,15 @@
 module.exports = {
   name: "nsfw",
-  async func(msg, { send, prefix, channel }) {
-    if(channel.nsfw)
-      return send(`**NSFW commands:**
-  **{}boobs** to get some boobs
-  **{}ass** to get some ass`.replace(/{}/g, prefix));
-    else
+  async func(msg, { send, prefix, channel, member }) {
+    if(channel.nsfw){
+      const list = [
+        '**{}boobs** to get some boobs',
+        '**{}ass** to get some ass',
+        '**{}hentai** for hentai',
+      ].join('\n').replace(/{}/g, prefix);
+
+      send('', new Discord.RichEmbed().setTitle('Nsfw commands').setDescription(list).setColor(member?member.displayColor:12435));
+    }else
       return send(`You are not in a nsfw channel!`);
   }
 };
