cb3fb4eee3213e60563071d26ff3b8411a9a267e
Evildeathpro <elawrence152@gmail.com>
2017-07-28 16:05:34 -0400

    Create index.js

---
+++ commands/info/avatar.cpscmd/index.js
@@ -0,0 +1,16 @@
+
+let avatar = require('./avatar');
+
+console.log('[CPSCMD][INFO][avatar] Building objects...');
+avatar.metadata = {
+  category: require('../').category,
+  description: 'This shows avatars of users!',
+  usage: 'avatar [mention user]',
+  example: 'avatar @Evildeathpro#7553',
+  perm: [['global.info.help.help']],
+  customperm: ['SEND_MESSAGES'],
+};
+console.log('[CPSCMD][INFO][avatar] Build objects complete!');
+module.exports = [
+  [avatar.name,avatar]
+];
