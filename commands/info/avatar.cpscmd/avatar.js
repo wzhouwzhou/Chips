c44f1864125bbc2abcd90623dd4ac42383ccba65
William Zhou <wzhouwzhou@gmail.com>
2017-07-29 06:36:57 +0800

    ...

--- commands/info/avatar.cpscmd/avatar.js
+++ commands/info/avatar.cpscmd/avatar.js
@@ -3,7 +3,7 @@ const Paginator = require('../../../rewrite-all/src/struct/client/Paginator').Pa
 
 module.exports = {
   name: "avatar",
-  async func(msg, { Discord, author, guild }) {
+  async func(msg, { send, Discord, author, guild }) {
     let start = process.hrtime();
     console.log(chalk.bold.bgBlue.green(author.tag+" ")+chalk.bgWhite.red(author.id+" ")+chalk.black.bgWhite(guild.id)+chalk.cyan(" [Avatar] "));
     let targetmember = msg.mentions.members;
@@ -21,19 +21,12 @@ module.exports = {
           page: ['Avatar Link: ', `[Click Here](${member.user.avatarURL})`],
           image: member.user.avatarURL
         });
-
-        /*const avator = new Discord.RichEmbed()
-          .setTitle(`Avatar Image of ${msg.member.user.tag} `, ``   , true)
-          .setColor(msg.member.displayColor)
-          .addField('Avatar Link: ', `[Click Here](${targetmember.user.avatarURL})`)
-          .setFooter(`--User Avatar lookup took ${(end)}.--`,``, true)
-          .setImage(targetmember.user.avatarURL);*/
       });
       const pages = [], title = [], image = [];
       memberList.forEach( e => {
         pages.push([e.page]);
         title.push(e.title);
-        image.push(e.image);
+        image.push(e.image.replace(/\?size=\d+/g,''));
       });
 
       let hrTime = process.hrtime(start);
@@ -44,6 +37,16 @@ module.exports = {
         end = (hrTime[0] * 1000000 + hrTime[1] / 1000);
       }
       µs ? end += 'µs' : end += 'ms';
+
+      if(image.length===1){
+        const avator = new Discord.RichEmbed()
+          .setTitle(`Avatar Image of ${msg.member.user.tag} `, ``   , true)
+          .setColor(msg.member.displayColor)
+          .addField('Avatar Link: ', `[Click Here](${targetmember.user.avatarURL})`)
+          .setFooter(`--User Avatar lookup took ${(end)}.--`,``, true)
+          .setImage(targetmember.user.avatarURL);
+        return send('', {embed: avator});
+      }
 
       const p = new Paginator ( msg,  {
         type:'paged',
