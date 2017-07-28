c28d3528884910637c2ec0580f0e3a6b8654ee99
William Zhou <wzhouwzhou@gmail.com>
2017-07-29 07:23:19 +0800

    Colors

--- rewrite-all/src/struct/music/MusicPlayer.js
+++ rewrite-all/src/struct/music/MusicPlayer.js
@@ -46,7 +46,7 @@ const MusicPlayer = class MusicPlayer {
 
     this.joinVC().then( () => {
       const song = this.looping?this.lastPlayed:this.queue.shift();
-      let embed = new this.Discord.RichEmbed().setTitle('Now playing...');
+      let embed = new this.Discord.RichEmbed().setTitle('Now playing...').setColor(1);
       embed.setDescription(song.title).setFooter('Length: '+song.length);
       embed.setImage(song.image);
       this.textchannel.send('', { embed });
@@ -69,7 +69,7 @@ const MusicPlayer = class MusicPlayer {
             this.leaveVC();
             this.connection = null;
             this.dispatcher = null;
-            embed = new this.Discord.RichEmbed.setTitle('Queue ended!').setDescription('Queue another song!').setTimestamp(new Date());
+            embed = new this.Discord.RichEmbed.setTitle('Queue ended!').setDescription('Queue another song!').setTimestamp(new Date().setColor(8060672));
             this.textchannel.send('', { embed });
           }
           else if(!this.shuttingDown)
@@ -92,7 +92,7 @@ const MusicPlayer = class MusicPlayer {
     if(this.shuttingDown) return null;
     return song.loadInfo().then(()=>{
       this.queue.push(song);
-      const embed = new this.Discord.RichEmbed().setTitle(`Successfully queued ${song.title}`);
+      const embed = new this.Discord.RichEmbed().setTitle(`Successfully queued ${song.title}`).setColor(2386431);
       embed.setDescription(`There ${this.queue.length==1?'is':'are'} now ${this.queue.length} song${this.queue.length==1?'':'s'} in the queue.`);
 
       if(this.textchannel) this.textchannel.send('', { embed });
