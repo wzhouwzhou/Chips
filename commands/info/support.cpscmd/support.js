a12001d50c6d2e4ed285d79221b734a4c607ae1d
Evildeathpro <elawrence152@gmail.com>
2017-08-18 00:19:24 -0400

    Update support.js

--- commands/info/support.cpscmd/support.js
+++ commands/info/support.cpscmd/support.js
@@ -3,6 +3,6 @@ module.exports = {
   async func(msg, { member, author }) {
     const used = member || author;
     used.send('**Chips Support Server: https://discord.gg/jj5FzF7**');
-  return reply(`Support server link has been sent to ${used}'s direct messages!`);
+  return send(`Support server link has been sent to ${used}'s direct messages!`);
   }
 };
