4b784d31d35917becf3b70737c8ea5ce03df8bc5
William <wzhouwzhou@gmail.com>
2017-08-18 23:18:36 -0400

    fix

--- commands/info/support.cpscmd/support.js
+++ commands/info/support.cpscmd/support.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "support",
-  async func(msg, { member, author }) {
+  async func(msg, { member, author, send }) {
     const used = member || author;
     used.send('**Chips Support Server: https://discord.gg/jj5FzF7**');
-  return send(`Support server link has been sent to ${used}'s direct messages!`);
+    return send(`Support server link has been sent to ${used}'s direct messages!`);
   }
 };
