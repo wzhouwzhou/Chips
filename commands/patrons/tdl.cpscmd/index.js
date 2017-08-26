c4e10ce91cff02c8cc93186eedb3a0a1eb5c4e75
Evildeathpro <elawrence152@gmail.com>
2017-08-26 11:16:21 -0400

    Update index.js

--- commands/patrons/tdl.cpscmd/index.js
+++ commands/patrons/tdl.cpscmd/index.js
@@ -1,6 +1,7 @@
 
 let tdl = require('./tdl');
 let wendy = require('./wendy');
+let patrons = require('./patrons');
 console.log('[CPSCMD][PATRONS][tdl] Building objects...');
 
 tdl.metadata = {
@@ -21,8 +22,18 @@ wendy.metadata = {
   customperm: ['SEND_MESSAGES'],
 };
 
+patrons.metadata = {
+  category: require('../').category,
+  description: 'A cmd to see all of our patrons!',
+  usage: 'patrons',
+  example: 'patrons',
+  perm: [['public.info.support.support']],
+  customperm: ['SEND_MESSAGES'],
+};
+
 console.log('[CPSCMD][UTILITY][perm] Build objects complete!');
 module.exports = [
   [tdl.name,tdl],
-  [wendy.name,wendy]
+  [wendy.name,wendy],
+  [patrons.name,patrons]
 ];
