4551b5a8fe21b0fc106a39b99fc41159f9c90939
Evildeathpro <elawrence152@gmail.com>
2017-08-26 01:12:38 -0400

    Create wendy.js

---
+++ commands/patrons/tdl.cpscmd/wendy.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "wendy",
+  async func(msg, { send, Discord }) {
+    const embed = new Discord.RichEmbed()
+    .setAuthor('Jay(okay)#7885', "http://i.imgur.com/kAXq1HD.png") 
+    .setTitle('').setDescription('The second Patron of Chips, :heart: Wendys! Aka Jay Okay!')
+    .setTimestamp(new Date())
+    .setImage("http://i.imgur.com/qGrvSGF.png")
+    .setColor(0x0fa134);
+      await send("Loading......").then(m => setTimeout(()=>m.delete(800)),1000);
+      return send('', { embed });
+  }
+};
