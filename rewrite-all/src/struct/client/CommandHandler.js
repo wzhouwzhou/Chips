71b1320aeb2bdcc5611105c35707d4a4cd56aa17
William <wzhouwzhou@gmail.com>
2017-08-28 13:11:28 -0400

    handle ctx

--- rewrite-all/src/struct/client/CommandHandler.js
+++ rewrite-all/src/struct/client/CommandHandler.js
@@ -16,6 +16,28 @@ const Handler = class CommandHandler {
   }
 
   handle (msg, prefix) {
+    const precmd = msg.content.split(/\s+/)[0].toLowerCase().replace(this._.escapeRegExp(prefix),'');
+    if(!this.cmds.has(precmd)) return false;
+    const ctx = {
+      'msg': msg,
+      'message': msg,
+      'channel': msg.channel,
+      'guild': msg.guild,
+      'server': msg.guild,
+      'author': msg.author,
+      'member': msg.member,
+      'send': msg.channel.send.bind(msg.channel),
+      'reply': msg.reply.bind(msg),
+      'client': this.client,
+      'bot': this.client,
+      'content': msg.content,
+      'args': this._.drop(msg.content.split(/\s+/)),
+      'precmd': precmd;
+      'suffix': msg.content.split(/\s+/).length>1?msg.content.substring(msg.content.indexOf(msg.content.split(/\s+/)[1])+1):null,
+      'prefix': prefix,
+      'Discord': this.Discord,
+    };
+
 
   }
 
