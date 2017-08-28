eb409dee3be042b67540f86670ae2fd50ada956c
William <wzhouwzhou@gmail.com>
2017-08-27 23:43:30 -0400

    path,_,require

--- rewrite-all/src/deps/index.js
+++ rewrite-all/src/deps/index.js
@@ -5,9 +5,16 @@ const rrequire = (m) => {
   delete require.cache[require.resolve(m)];
   return require(m);
 };
+exports.rrequire = rrequire;
 
 const Discord = rrequire('discord.js');
 exports.Discord = Discord;
 
 const fs = rrequire('fs');
 exports.fs = fs;
+
+const _ = rrequire('lodash');
+exports._ = _;
+
+const path = rrequire('path');
+exports.path = path;
