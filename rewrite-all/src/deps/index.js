cf87a2e82c6f6f77e8c46c95f8dd9154cf7904cc
William <wzhouwzhou@gmail.com>
2017-08-28 11:44:47 -0400

    Rewrite the whole file again to incorporate functions

--- rewrite-all/src/deps/index.js
+++ rewrite-all/src/deps/index.js
@@ -22,55 +22,69 @@ const Exporter = class Exporter {
     return temp;
   }
   serialize(){
-    const data = {};
+    const data = {
+      packages: {},
+      functions: {},
+    };
     [
-      ['discord.js', ['Discord', 'djs']],
-      'fs',
-      ['lodash',['_','lodash']],
-      'path',
-      'body-parser',
-      'cookie-parser',
-      'express',
-      'express-session',
-      'connect-flash',
-      'morgan',
-      ['rotating-file-stream', ['rfs','rotating_file_stream']],
-      'crypto',
-      ['ytdl-core', 'ytdl'],
-      'moment',
-      'ytsearcher',
-      'discordblacklist',
-      'pm2',
-      'connect-four',
-      'needle',
-      'request',
-      'got',
-      'snekfetch',
-      'child_process',
-      'jimp',
-      'jsonfile',
-      'url-download',
-      ['events', 'EventEmitter'],
-      'rethinkdbdash',
-      'rethinkdb',
-      ['google-spreadsheet',['google-spreadsheet','GoogleSpreadsheet']],
-      'chalk',
-      'express-ejs-extend',
-      'passport',
-      'http-proxy',
-      'asciify',
-      'assert',
-      'pmx',
-      ['cheerio',['cheerio','$']],
-    ].forEach(m => {
-      if(typeof m === 'string')
-        return this.define(data, m, m);
-      if(m[1] === undefined)
-        return this.define(data, m[0], m[0]);
-      if(typeof m[1] === 'string')
-        return this.define(data, m[1], m[0]);
-      m[1].forEach(k => {
-        this.define(data, k, m[0]);
+      [
+        'packages',
+        [ // node_modules packages
+          ['discord.js', ['Discord', 'djs']],
+          'fs',
+          ['lodash',['_','lodash']],
+          'path',
+          'body-parser',
+          'cookie-parser',
+          'express',
+          'express-session',
+          'connect-flash',
+          'morgan',
+          ['rotating-file-stream', ['rfs','rotating_file_stream']],
+          'crypto',
+          ['ytdl-core', 'ytdl'],
+          'moment',
+          'ytsearcher',
+          'discordblacklist',
+          'pm2',
+          'connect-four',
+          'needle',
+          'request',
+          'got',
+          'snekfetch',
+          'child_process',
+          'jimp',
+          'jsonfile',
+          'url-download',
+          ['events', 'EventEmitter'],
+          'rethinkdbdash',
+          'rethinkdb',
+          ['google-spreadsheet', ['google-spreadsheet','GoogleSpreadsheet']],
+          'chalk',
+          'express-ejs-extend',
+          'passport',
+          'http-proxy',
+          'asciify',
+          'assert',
+          'pmx',
+          ['cheerio', ['cheerio','$']],
+        ],
+      ],
+      [
+        'functions',
+        [ // functions
+          'mee6rankF',
+        ].map(e=>[`./functions/${e[0]}`, e[1]]),
+      ],
+    ].forEach(s=>{
+      s[1].forEach(m => {
+        if(typeof m === 'string')
+          return this.define(data[s[0]], m, m);
+        if(m[1] === undefined)
+          return this.define(data[s[0]], m[0], m[0]);
+        if(typeof m[1] === 'string')
+          return this.define(data[s[0]], m[1], m[0]);
+        m[1].forEach(k => this.define(data[s[0]], k, m[0]));
       });
     });
 
