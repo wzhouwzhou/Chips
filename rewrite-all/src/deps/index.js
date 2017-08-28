43c5b0d8631af3456dab82fdc9c20681930b9662
William <wzhouwzhou@gmail.com>
2017-08-27 21:51:04 -0400

    index

---
+++ rewrite-all/src/deps/index.js
@@ -0,0 +1,10 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const rrequire = (m) => {
+  
+}
+const Discord = rrequire('discord.js');
+const fs = rrequire('fs');
+
+exports.fs = fs;
