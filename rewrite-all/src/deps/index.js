c93e73e38eb85389220c0348760b7da39b7279c1
William <wzhouwzhou@gmail.com>
2017-08-27 23:34:47 -0400

    .

--- rewrite-all/src/deps/index.js
+++ rewrite-all/src/deps/index.js
@@ -2,9 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 
 const rrequire = (m) => {
-  
-}
-const Discord = rrequire('discord.js');
-const fs = rrequire('fs');
+  delete require.cache[require.resolve(m)];
+  return require(m);
+};
 
+const Discord = rrequire('discord.js');
+exports.Discord = Discord;
+
+const fs = rrequire('fs');
 exports.fs = fs;
