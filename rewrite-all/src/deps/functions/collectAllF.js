a25fb80f9371191e8e0a9225075cb23f28768a75
William <wzhouwzhou@gmail.com>
2017-08-29 11:04:30 -0400

    halp

--- rewrite-all/src/deps/functions/collectAllF.js
+++ rewrite-all/src/deps/functions/collectAllF.js
@@ -1,11 +1,37 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 
-exports.default = ({ Discord }) => {
+exports.default = ({ }) => {
+  // options must contain mfilter and rfilter,
+  // a users object that either has an allowAll = true or array of userids
+  // accepted/deniedMsgs and accepted/deniedRxns
+  // expire (time in ms)
+  // reply (boolean whether to reply in prompt)
   const collectAll = (msg, { options }) => {
-    const guild = msg.guild, member = msg.member || null, author = msg.author, channel = msg.channel;
+    const channel = msg.channel;
+    const { users, promptMsg, expire, numMsgs, acceptedMsgs, deniedMsgs } = options;
+    const allMsgs = [...acceptedMsgs, ...deniedMsgs];
     return new Promise( (res, rej) => {
+      let externalMsgCounter = 0;
+      const mcol = channel.createMessageCollector(
+        m=>{
+          if(users.allowAll) return true;
+          if(users.indexOf(m.author.id)){
+            if(options.mfilter(m)) {
+              externalMsgCounter++;
+            }
+            if(externalMsgCounter>numMsgs) mcol.stop();
+          }
+          return false;
+        },
+        { max: numMsgs || 1, time: expire }
+      );
 
+      mcol.on('end', collected => {
+        if(collected.size === 0) rej('timeout');
+      });
+
+      if(options.reply) msg.
     });
   };
   return collectAll;
