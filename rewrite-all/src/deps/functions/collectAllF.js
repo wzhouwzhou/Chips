0f47c7a6fb455e67e18d79e90b52ce622369bfff
William <wzhouwzhou@gmail.com>
2017-08-29 10:21:16 -0400

    begin collectAllF

---
+++ rewrite-all/src/deps/functions/collectAllF.js
@@ -0,0 +1,12 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports.default = ({ Discord }) => {
+  const collectAll = (msg, { options }) => {
+    const guild = msg.guild, member = msg.member || null, author = msg.author, channel = msg.channel;
+    return new Promise( (res, rej) => {
+
+    });
+  };
+  return collectAll;
+};
