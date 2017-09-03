264e4d316b68d4695706e921898954a834dc75e3
William <wzhouwzhou@gmail.com>
2017-09-02 22:13:42 -0400

    Fix recursion and enable Array.from to avoid internal cache

--- rewrite-all/src/deps/functions/fetchMessagesRF.js
+++ rewrite-all/src/deps/functions/fetchMessagesRF.js
@@ -3,17 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 const firstF = require('./firstF').default();
 exports.default = ({ Discord }) => {
-  const { Collection } = Discord.Collection;
-  return ( channel, options, collection = new Collection() ) => {
+  const { Collection } = Discord;
+  const fetchMessagesR = ( channel, options, collection = new Collection() ) => {
     const limit = options.limit;
     return new Promise( (res,rej) => {
-      if(collection.size >= limit) return res(new Collection(firstF(collection.array(),limit).map(e=>[e.id,e])));
-  channel.fetchMessages(Object.assign({}, options, {limit: Math.min(options.limit,100)} )).then(async msgs=>{
+      if(collection.size >= limit) return res(new Collection(firstF(Array.from(collection.values()),limit).map(e=>[e.id,e])));
+      channel.fetchMessages(Object.assign({}, options, {limit: Math.min(options.limit,100)} )).then(async msgs=>{
         if(msgs.size == 0) return res(collection);
-        msgs.array().forEach(m=> collection.set(m.id, m));
-        if(collection.size >= limit) return res(new Collection(firstF(collection.array(),limit).map(e=>[e.id,e])));
-        return res(await fetchMessagesR( channel, { limit, before: msgs.array().sort((a,b)=>a.createdAt-b.createdAt)[0].id }, collection));
+        Array.from(msgs.values()).forEach(m => collection.set(m.id, m));
+        return res(await fetchMessagesR( channel, { limit, before: Array.from(msgs.values()).sort((a,b)=>a.createdAt-b.createdAt)[0].id }, collection));
       }).catch(rej);
     });
   };
+  return fetchMessagesR;
 };
