b3b7aa95222e2508fe38217ff22ae4a4908a667b
William <wzhouwzhou@gmail.com>
2017-09-03 13:15:14 -0400

    switch the firstF and support for clone

--- rewrite-all/src/deps/functions/firstF.js
+++ rewrite-all/src/deps/functions/firstF.js
@@ -1,15 +1,20 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 
-exports.default = () => (arr, n) => {
-    if(typeof n === 'object')
+exports.default = ({_}) => (arr, n) => {
+    let clone=!0;
+    if(typeof n === 'object'){
       n = n.count || n.num || n.n || 0;
-
-    if(arr.constructor.name === 'Collection')
-      return new (arr.constructor)(Array.from(arr.values()).slice(0,n).map(e=>[e.id,e]));
-    else if(arr.constructor.name === 'Array')
-      return arr.slice(0,n);
-
-    return arr;
+      if(n.clone!=null) clone = n.clone;
+    }
+    switch(arr.constructor.name){
+      case 'Collection':
+      case 'Map':
+        return new (arr.constructor)(Array.from(arr.values()).slice(0,n).map(e=>[e.id,e]));
+      case 'Array':
+        return _&&clone?_.clone(arr,!0).slice(0,n):arr.slice(0,n);
+      default:
+        return arr;
+    }
   }
 ;
