30c96bda7ae59710aaf4ebc5350ab0f09abe8873
William <wzhouwzhou@gmail.com>
2017-09-02 23:03:25 -0400

    Handle collection case smartly : no need for importing Discord

--- rewrite-all/src/deps/functions/firstF.js
+++ rewrite-all/src/deps/functions/firstF.js
@@ -4,8 +4,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => (arr, n) => {
     if(typeof n === 'object')
       n = n.count || n.num || n.n || 0;
-    if(arr.constructor.name === 'Array')
+
+    if(arr.constructor.name === 'Collection')
+      return new (arr.constructor)(Array.from(arr.values()).slice(0,n).map(e=>[e.id,e]));
+    else if(arr.constructor.name === 'Array')
       return arr.slice(0,n);
+
     return arr;
   }
 ;
