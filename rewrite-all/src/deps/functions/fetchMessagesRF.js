6bb593ba86a9ae8b562650e6761e46e06f77327d
William <wzhouwzhou@gmail.com>
2017-09-02 22:26:33 -0400

    try catch the await and use Logger

--- rewrite-all/src/deps/functions/fetchMessagesRF.js
+++ rewrite-all/src/deps/functions/fetchMessagesRF.js
@@ -1,7 +1,9 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 
+const Logger = require('../../struct/client/Logger').create('exports','fetchMessagesRF');
 const firstF = require('./firstF').default();
+
 exports.default = ({ Discord }) => {
   const { Collection } = Discord;
   const fetchMessagesR = ( channel, options, collection = new Collection() ) => {
@@ -11,7 +13,12 @@ exports.default = ({ Discord }) => {
       channel.fetchMessages(Object.assign({}, options, {limit: Math.min(options.limit,100)} )).then(async msgs=>{
         if(msgs.size == 0) return res(collection);
         Array.from(msgs.values()).forEach(m => collection.set(m.id, m));
-        return res(await fetchMessagesR( channel, { limit, before: Array.from(msgs.values()).sort((a,b)=>a.createdAt-b.createdAt)[0].id }, collection));
+        try{
+          return res(await fetchMessagesR( channel, { limit, before: Array.from(msgs.values()).sort((a,b)=>a.createdAt-b.createdAt)[0].id }, collection));
+        }catch(err){
+          Logger.error(err);
+          return rej(err);
+        }
       }).catch(rej);
     });
   };
