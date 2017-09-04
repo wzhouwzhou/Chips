5cb9fdc84ecbfc44aea4a94f33859c94dccc15d9
William <wzhouwzhou@gmail.com>
2017-09-04 15:34:20 -0400

    Fix up -derp

--- commands/fun/text.cpscmd/derp.js
+++ commands/fun/text.cpscmd/derp.js
@@ -2,6 +2,6 @@
 module.exports = {
   name: "derp",
   async func(msg, { args, send }) {
-    send(args.join(' ').replace(/[^\s]{2,2}/g, m=>`${m[0].toUpperCase()}${m[1]}`));
+    send(args.join(' ').replace(/[^\s]{1,2}/g, m=>`${m[0].toUpperCase()}${m[1]?m[1].toLowerCase():''}`));
   }
 };
