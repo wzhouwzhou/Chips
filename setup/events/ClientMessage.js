9a7aaff589ea7f08fcfeecb11b1ebe84ebc18d7c
William <wzhouwzhou@gmail.com>
2017-09-11 12:09:44 -0400

    Return await

--- setup/events/ClientMessage.js
+++ setup/events/ClientMessage.js
@@ -66,7 +66,7 @@ module.exports = function() {
     //rekt
     if(muteTrigger&& (message.author.id=="244533925408538624" && (message.content.toLowerCase().indexOf("user muted successfully")>-1||message.content.toLowerCase().indexOf("user banned successfully")>-1)))
       return message.channel.send("Omg rekt! https://giphy.com/gifs/TEcDhtKS2QPqE");
-    if (message.guild.id==Constants.servers.SINX&&message.author.id=='304322292769488906'&&(/^[^]*(commandnotfound)[^]*$/).test(message.content.toLowerCase().replace(/\s+/g,''))) message.delete(); 
+    if (message.guild.id==Constants.servers.SINX&&message.author.id=='304322292769488906'&&(/^[^]*(commandnotfound)[^]*$/).test(message.content.toLowerCase().replace(/\s+/g,''))) return await message.delete(); 
     if (message.author.bot) return;
     if(await handleAntiLink(message)) return;
     if(await handleAntiDiepLink(message)) return;
