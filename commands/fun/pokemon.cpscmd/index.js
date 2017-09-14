16a02b1e0837a8c0ec37a038b88db88b5350c165
Ethan <elawrence152@gmail.com>
2017-09-13 22:49:37 -0400

    WOOOOOOOOOOOOO AM NOT YOUR PICKLEEEEEEEEE

--- commands/fun/pokemon.cpscmd/index.js
+++ commands/fun/pokemon.cpscmd/index.js
@@ -1,6 +1,15 @@
-
+let pokemon = require('./pokemon');
 let pokemontest = require('./pokemontest');
 console.log('[CPSCMD][FUN][pokemon] Building objects...');
+
+pokemon.metadata = {
+  category: require('../').category,
+  description: 'A work in progress!',
+  usage: 'pokemon [gen or "egg"] [possible egg?]',
+  example: 'pokemon egg 3undisc',
+  perm: [['global.fun.pokemon.start']],
+  customperm: ['SEND_MESSAGES'],
+};
 
 pokemontest.metadata = {
   category: require('../').category,
@@ -13,5 +22,6 @@ pokemontest.metadata = {
 
 console.log('[CPSCMD][FUN][pokemon] Build objects complete!');
 module.exports = [
+  [pokemon.name,pokemon],
   [pokemontest.name,pokemontest],
 ];
