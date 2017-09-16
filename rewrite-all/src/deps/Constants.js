a4182046984de7db199f6f42b8bf231c623a6b34
William Zhou <wzhouwzhou@gmail.com>
2017-09-16 12:46:55 -0400

    Add numbers

--- rewrite-all/src/deps/Constants.js
+++ rewrite-all/src/deps/Constants.js
@@ -2,3 +2,16 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 exports.chess = require('./constants/ChessConstants');
+exports.numbersA = [
+  'zero',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'keycap_ten'
+].map(e=>`:${e}:`);
