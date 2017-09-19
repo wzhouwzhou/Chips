e0cf3a44f40c6b8b0ee62ab7a4fe82fa12ee8282
Ethan <elawrence152@gmail.com>
2017-09-19 17:30:25 -0400

    rawr

--- commands/fun/search.cpscmd/lmgtfy.js
+++ commands/fun/search.cpscmd/lmgtfy.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "lmgtfy",
-  async func(msg, { send, prefix, args }) {
+  async func(msg, { send, prefix, args }) -async {
     if (!args[0]) {
         return reply('Heres how you can use this command:\n**${_.escapeRegExp(prefix)}** google [message content]\n**${_.escapeRegExp(prefix)}** bing [message content]\n**${_.escapeRegExp(prefix)}** yahoo [message content]\n**${_.escapeRegExp(prefix)}** aol [message content]\n**${_.escapeRegExp(prefix)}** ask [message content]\n**${_.escapeRegExp(prefix)}** duck [message content]');
     }
@@ -21,5 +21,7 @@ module.exports = {
 	}
 	if (args[0]=="duck") {
     return reply(`Heres how you search this! http://www.lmgtfy.com/?s=d&iie=1&q=${message.content.split(/\s+/).slice(2).join('+')}`);
+	} else {
+	return reply(`Heres how you google this! http://www.lmgtfy.com/?q=${args.join('+')}`);	
 	}
   }};
