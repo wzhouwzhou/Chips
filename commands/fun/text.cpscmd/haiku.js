83a87d4a1b1125573a6f065c22527c07309b71cd
William Zhou <wzhouwzhou@gmail.com>
2017-09-19 15:27:52 -0400

    LOL

--- commands/fun/text.cpscmd/haiku.js
+++ commands/fun/text.cpscmd/haiku.js
@@ -13,3 +13,15 @@ module.exports = {
     send(str);
   }
 };
+
+/*
+`eval const nlp = require('nlp_compromise');
+const nlpSyllables = require('nlp-syllables');
+nlp.plugin(nlpSyllables);
+const firstF = require('./rewrite-all/src/deps/functions/firstF').default({_});
+const lastF = require('./rewrite-all/src/deps/functions/lastF').default({_});
+const syllables =_.flatten(nlp.term('chips is the dumbest bot that has ever graced this server confirmed exposed idek').syllables());
+const first17 = firstF(syllables, 17);
+    const str = `${firstF(first17,5).join(' ')}\n${lastF(firstF(first17,5+7),7).join(' ')}\n${lastF(firstF(first17,5+7+5),5).join(' ')}`;
+    send(str);
+    *./
