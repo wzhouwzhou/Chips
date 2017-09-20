5e1811c4d6145e368ce6fe7f0fbb69a8da4e4d0b
William Zhou <wzhouwzhou@gmail.com>
2017-09-19 23:12:12 -0400

    Even better?

--- commands/fun/search.cpscmd/lmgtfy.js
+++ commands/fun/search.cpscmd/lmgtfy.js
@@ -12,7 +12,7 @@ const lmgtfy = (searchQ, engine='google') => {
   const prms = {'iie':1};  if(engines.get(engine)) prms.s=engines.get(engine);
   return 'http://lmgtfy.com/?'+qs.stringify(prms)+`&q=${qP}`;
 };
-
+const grammarJoin = require('../../../rewrite-all/src/deps/functions/grammarJoinF').default();
 module.exports = {
   name: "lmgtfy",
   async func(msg, { reply, prefix, args }) {
@@ -20,13 +20,10 @@ module.exports = {
       return reply(
       [
         'Heres how you can use this command:',
-        `**${_.escapeRegExp(prefix)}** google [message content]`,
-        `**${_.escapeRegExp(prefix)}** bing [message content]`,
-        `**${_.escapeRegExp(prefix)}** yahoo [message content]`,
-        `**${_.escapeRegExp(prefix)}** aol [message content]`,
-        `**${_.escapeRegExp(prefix)}** ask [message content]`,
-        `**${_.escapeRegExp(prefix)}** duck [message content]`,
-      ].join('\n'));
+        '{} [search engine] [query]',
+        'Where [search engine] can be:',
+        grammarJoin('google, bing, yahoo, aol, ask, duck (duckduckgo)'.split(/,\s*/g))
+      ].join('\n').replace(/{}/g, `${_.escapeRegExp(prefix)}${this.name}`));
     return reply(`Here's how you search this: ${lmgtfy(args[0])}`);
   }
 };
