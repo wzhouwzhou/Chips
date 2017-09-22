a5a667f0d5042d195fb62cd262efd2b7995edd06
William Zhou <wzhouwzhou@gmail.com>
2017-09-21 22:06:06 -0400

    ..

--- setup/logging/changeConsole.js
+++ setup/logging/changeConsole.js
@@ -3,14 +3,16 @@ const colors = require('chalk');
 const moment = require('moment');
 function editConsole(isMng, shardIDObj) {
     console.oldLog = console.log;
-    let time = colors.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss.SSS')}] | `);
+
     console.log = function () {
+      let time = colors.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss.SSS')}] | `);
         const args = Array.from(arguments);
         args.unshift(colors.bgYellow.bold(isMng ? `[MNG]` : `[S${shardIDObj.id == null ? '?' : shardIDObj.id}]`) + ' ');
         return console.oldLog.apply({}, [time, ...args]);
     };
     console.oldError = console.error;
     console.error = function () {
+      let time = colors.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss.SSS')}] | `);
         const args = Array.from(arguments);
         args.unshift(colors.bgYellow.bold(isMng ? `[MNG]` : `[S${shardIDObj.id == null ? '?' : shardIDObj.id}]`) + ' ');
         return console.oldError.apply({},[time, ...args]);
