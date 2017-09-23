a75b14c7ecd7ab537c80df725f3ee1cac7f5ebcf
lucaslsg <lucaslucaso2002@gmail.com>
2017-09-23 20:59:37 +0200

    gud version of cc

--- commands/info/count.cpscmd/channelcount.js
+++ commands/info/count.cpscmd/channelcount.js
@@ -1,8 +1,17 @@
 module.exports = {
   name: "channelcount",
-  async func(msg, { send, guild, channel }) {
-    if(guild)
-      return send(guild.channels.size);
-    else return send('You must be in a server to use this!');
+  async func(msg, { send, guild, channel, args }) {
+    if(!guild)
+      return send('You must be in a server to use this!');    
+    
+    if (!args[0]==='text') 
+      return send(guild.channel.size);
+    
+    if (!args[0]==='voice') 
+      return send(guild.voiceChannel.size); 
+
+    if (!args[0]) 
+      return send(guild.channel.size);     
+
   }
 };
