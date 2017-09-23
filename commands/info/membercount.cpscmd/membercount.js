f47bd42506242af4ecea2f8e831c676829901a23
William Zhou <wzhouwzhou@gmail.com>
2017-09-23 13:50:25 -0400

    Dpn’t need channel and guild.members

--- commands/info/membercount.cpscmd/membercount.js
+++ commands/info/membercount.cpscmd/membercount.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "membercount",
-  async func(msg, { send, channel, guild }) {
+  async func(msg, { send, guild }) {
     if(guild)
-      return send(guild.member.size);
+      return send(guild.members.size);
     else return send('You must be in a server to use this!');
   }
 };
