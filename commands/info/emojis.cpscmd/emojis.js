750fe5925c49622ca51d704b08329fdcf4730104
William Zhou <wzhouwzhou@gmail.com>
2017-09-24 10:37:13 -0400

    fix

--- commands/info/emojis.cpscmd/emojis.js
+++ commands/info/emojis.cpscmd/emojis.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "emojis",
-  async func(msg, { send, guild, }) {
+  async func(msg, { send, guild, member }) {
     if(guild)
        return send((new Discord.RichEmbed).setDescription(guild.emojis.array().map(e=>e+[]).join(' ')).setTitle(guild.emojis.size+' emojis').setColor(member.displayColor));
     else return ("You need to be in a server to use this command!");
