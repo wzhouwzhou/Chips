3987bc02f8cabfaab208f43e286cfc59c5d93071
lucaslsg <lucaslucaso2002@gmail.com>
2017-09-24 09:22:40 +0200

    hopesthatthisworks

--- commands/info/channeltopic.cpscmd/channeltopic.js
+++ commands/info/channeltopic.cpscmd/channeltopic.js
@@ -1,8 +1,11 @@
+const embed = new Discord.RichEmbed()
+.addField(channel.topic, true);
+
 module.exports = {
   name: "channeltopic",
   async func(msg, { send, channel, guild }) {
     if(guild)
-      return send(channel.topic);
+      return send({embed})
     else return send('You must be in a server to use this!');
   }
 };
