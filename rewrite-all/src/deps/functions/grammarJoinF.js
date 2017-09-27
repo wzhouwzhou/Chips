9f272854a55571f1341d29ea6dd5862717e7df7b
William Zhou <wzhouwzhou@gmail.com>
2017-09-27 13:35:31 -0400

    better grammarJoin

--- rewrite-all/src/deps/functions/grammarJoinF.js
+++ rewrite-all/src/deps/functions/grammarJoinF.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 exports.default = ({_}) => {
   const first = require('./firstF').default({_});
-  return a => first(a,a.length-1).join(', ')+(a.length>2?',':'')+(a.length>1?' or ':'')+(a[a.length-1]);
+  return (a, c = 'or') => first(a,a.length-1).join(', ')+(a.length>2?',':'')+(a.length>1?' '+c+' ':'')+(a[a.length-1]);
 };
