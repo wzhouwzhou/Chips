16984896ef259c777c5b82bf1425dc06ae28a541
lucaslsg <lucaslucaso2002@gmail.com>
2017-09-27 18:45:20 +0200

    gr8 gif

--- commands/fun/triggers.cpscmd/triggered.js
+++ commands/fun/triggers.cpscmd/triggered.js
@@ -6,7 +6,8 @@ const gifs = [
   "https://giphy.com/gifs/tony-phillips-2kVi3pspuXXYA",
   "https://giphy.com/gifs/knuckles-echidna-enchilada-yaAbruchhQFFu",
   "https://giphy.com/gifs/vk7VesvyZEwuI",
-  "https://www.tenor.co/viTW.gif"
+  "https://www.tenor.co/viTW.gif",
+  "https://cdn.discordapp.com/attachments/257895860757725186/362640603256717312/everyone_triggered.gif"
 ];
 
 module.exports = {
