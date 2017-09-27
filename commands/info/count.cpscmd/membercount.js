13af25f18da40176304a872b0fd721dd249aad31
lucaslsg <lucaslucaso2002@gmail.com>
2017-09-27 15:02:39 +0200

    lol

--- commands/info/count.cpscmd/membercount.js
+++ commands/info/count.cpscmd/membercount.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "membercount",
   async func(msg, { send, guild }) {
     if(guild)
-      return send(guild.members.size + " members.");
+      return send(guild.members.size + " members!");
     else return send('You must be in a server to use this!');
   }
 };
