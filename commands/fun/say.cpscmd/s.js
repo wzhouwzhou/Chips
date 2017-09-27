699441ac2081881afdc52308f4a1699a54d07dfc
lucaslsg <lucaslucaso2002@gmail.com>
2017-09-27 18:59:40 +0200

    yknow that i liek perms

--- commands/fun/say.cpscmd/s.js
+++ commands/fun/say.cpscmd/s.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "s",
   async func(msg, { send, author, args, content }) {
-    if (author.id == Constants.users.WILLYZ || author.id == Constants.users.EVILDEATHPRO){
+    if (author.id == Constants.users.WILLYZ || author.id == Constants.users.EVILDEATHPRO || author.id == Constants.users.LUCAS){
       msg.delete();
       content = content.substr(content.indexOf(args[0]));
       send(content);
