5d5bf4a06a13b44f8ef418829a57112d8d57f464
William Zhou <wzhouwzhou@gmail.com>
2017-10-02 15:55:16 -0400

    User feedback and max length

--- commands/info/botnick.cpscmd/botnick.js
+++ commands/info/botnick.cpscmd/botnick.js
@@ -5,6 +5,9 @@ module.exports = {
       return send('Nothing provided to set as my nick');
     if(!guild.me.hasPermission('CHANGE_NICKNAME'))
       return send('I do not have permission to change my own nickname!');
-    return guild.me.setNickname(suffix);
+    if(suffix.length>32)
+      return send('My nickname can only be a maximum of 32 characters in length!');
+    await guild.me.setNickname(suffix);
+    return send('Nickname set successfully!');
   }
 };
