eefc22dae4577ed8c3005f304e4efcda8a9f5b35
William Zhou <wzhouwzhou@gmail.com>
2017-10-02 15:43:52 -0400

    Fix

--- commands/info/botnick.cpscmd/botnick.js
+++ commands/info/botnick.cpscmd/botnick.js
@@ -1,16 +1,10 @@
 module.exports = {
-    name: "botnick",
-    async func(msg, { send, guild, suffix }) {
-      if(!suffix)
-        return send('Nothing provided to set as my nick');
-      if(!guild.me.hasPermission('CHANGE_NICKNAME'))
-        return send('I do not have permission to change my own nickname!');
-    let nick;
-    if(args[0])
-      nick = content.substring(content.indexOf(args[0]));
-        if(nick == null)
-          return send('No nickname provided');
-
-    guild.me.setNickname(nick);
+  name: "botnick",
+  async func(msg, { send, guild, suffix }) {
+    if(!suffix)
+      return send('Nothing provided to set as my nick');
+    if(!guild.me.hasPermission('CHANGE_NICKNAME'))
+      return send('I do not have permission to change my own nickname!');
+    return guild.me.setNickname(suffix);
   }
 };
