3645122e5bc85190a01d49cb2547b77a7517e90f
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-03 10:06:25 +0200

    aboose

--- commands/moderation/botnick.cpscmd/botnick.js
+++ commands/moderation/botnick.cpscmd/botnick.js
@@ -1,12 +1,14 @@
 module.exports = {
   name: "botnick",
-  async func(msg, { send, guild, suffix }) {
+  async func(msg, { send, guild, suffix, user }) {
     if(!guild)
      return send('You need to be in a server to use this!');
     if(!suffix)
       return send('Nothing provided to set as my nick');
     if(!guild.me.hasPermission('CHANGE_NICKNAME'))
       return send('I do not have permission to change my own nickname!');
+    if(!user.hasPermission('CHANGE_NICKNAME'))
+      return send('You do not have permission to change my own nickname!');           
     if(suffix.length>32)
       return send('My nickname can only be a maximum of 32 characters in length!');
     await guild.me.setNickname(suffix);
