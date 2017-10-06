06f090ffee62d001c02d8061f95bf96b522a915b
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-06 08:42:01 +0200

    space

--- commands/info/count.cpscmd/emojicount.js
+++ commands/info/count.cpscmd/emojicount.js
@@ -3,7 +3,7 @@ module.exports = {
     async func(msg, { send, guild, args }) {
       if(!guild)
         return send('You must be in a server to use this!');
-      else return send(guild.emojis.size +  'emojis.');  
+      else return send(guild.emojis.size +  ' emojis.');  
        
       }
   };
