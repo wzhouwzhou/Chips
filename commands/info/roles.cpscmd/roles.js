64bbe21e5d6b1e5ac8cda6ac2f3766b917a47674
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-06 09:14:09 +0200

    test

---
+++ commands/info/roles.cpscmd/roles.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "roles",
+    async func(msg, { send, guild }) {
+      if(guild)
+        return send(guild.roles);
+      else return send('You must be in a server to use this!');
+    }
+  };
