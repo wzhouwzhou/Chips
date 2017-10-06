c3e070a5c9e55e7d3ffb55cae854b779c648b361
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-06 09:39:49 +0200

    test

--- commands/fun/webhooktest.cpscmd/webhooktest.js
+++ commands/fun/webhooktest.cpscmd/webhooktest.js
@@ -1,15 +1,17 @@
 module.exports = {
     name: "webhooktest",
-    async func(msg, { send, guild, channel, author }) {
+    async func(msg, { send, guild, channel, author, args }) {
 
-    if (!author.id == Constants.users.LUCAS)
+    const content = content.substr(content.indexOf(args[0]));    
+    
+    if (author.id == Constants.users.LUCAS)
       return;
 
   const lucas = guild.members.get('205608598233939970')?guild.members.get('205608598233939970').displayName:null;
     if(lucas)
       channel.createWebhook(lucas, Constants.avatars.LUCAS)
         .then (whook => whook.edit(lucas, Constants.avatars.LUCAS))
-        .then(hook => hook.send(`Test!`)
+        .then(hook => hook.send(content)
         .then(() => { hook.delete(); })
         .catch(console.error)
   
@@ -17,3 +19,5 @@ module.exports = {
    }
 
 };
+
+
