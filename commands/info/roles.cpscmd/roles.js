e67a43ece94ca697f0901214e2fcd39af8c4eaca
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-06 09:19:17 +0200

    i tried

--- commands/info/roles.cpscmd/roles.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "roles",
-    async func(msg, { send, guild }) {
-      if(guild)
-        return send(guild.roles.list);
-      else return send('You must be in a server to use this!');
-    }
-  };
