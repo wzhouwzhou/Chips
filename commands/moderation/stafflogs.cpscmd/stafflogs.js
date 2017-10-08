30666283911c5b64e1a77a9ac8010ddd75d2538f
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-08 10:54:22 +0200

    o

--- commands/moderation/stafflogs.cpscmd/stafflogs.js
+++ commands/moderation/stafflogs.cpscmd/stafflogs.js
@@ -18,43 +18,7 @@ module.exports = {
 
     if (args[0]==='add')
      return send('Creating a staff-logs channel.')
-      .then (channel => channel.create('staff-logs'))
-      await send(stafflogs + ' channel succesfully created!');
-}
-
-/*    
-Inspired by uhm emojiben
-
-const mem = gMember(target);
-
-    let ebanRole=guild.roles.find("name","Emoji Banned");
-    if (ebanRole==null)
-      ebanRole= await guild.createRole(
-        { name: 'Emoji Banned'}
-      );
-
-    if(ebanRole==null)console.log("Error getting emoji banned role");
-
-    const channels = guild.channels.filter(c => c.type === 'text');
-    if(channels==null)console.log("Error getting text channels");
-    for (const channel of channels.values())
-      await channel.overwritePermissions(ebanRole, {
-        USE_EXTERNAL_EMOJIS: false,
-        ADD_REACTIONS: false,
-      });
-
-    mem.addRole(ebanRole);
-
-    const usernm = mem.user.username;
-
-    await reply(`User ${usernm} emoji banned successfully!`);
-
-    let emb = new Discord.RichEmbed()
-      .setAuthor("Emoji Ban Log")
-      .setTitle(`<@${mem.user.id}> was emoji banned by <@${author.id}>`)
-      .setColor(9109504)
-      .setThumbnail(Constants.images.WARNING)
-      .addField("Emoji Ban reason: ", `${reason}`, true);
-    await send(emb);
+       .then (channel => channel.create('staff-logs'));
+     await send(stafflogs + ' channel succesfully created!');
   }
-};*/
+}
