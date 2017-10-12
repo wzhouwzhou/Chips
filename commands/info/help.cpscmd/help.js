f54b93f2051cb9619669df7dc9995aaaff0ce671
William Zhou <wzhouwzhou@gmail.com>
2017-10-12 01:32:24 -0400

    Better help escaping

--- commands/info/help.cpscmd/help.js
+++ commands/info/help.cpscmd/help.js
@@ -13,8 +13,8 @@ module.exports = {
       'The 🔢 button will let you jump to a specific page number.',
       'The ⏏ button will close this menu!',
       "Each command will update with your server's custom prefix!",
-      `The prefix for your server is **${_.escapeRegExp(prefix)}**`,
-    ].join('\n'));
+      `The prefix for your server is **{}**`,
+    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix.replace(/`/g,'\\`'))));
 
     const Table = ([
       ['1_     _','Introductions'],
@@ -52,7 +52,7 @@ module.exports = {
       '**{}membercount/{}mc** to get amount of people in the server.',
       '**{}rolecount/{}rc** to get the amount of roles in the server.',
       '**{}channelcount/{}cc** to get the amount of channels in the server.',
-    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix)));
+    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix.replace(/`/g,'\\`'))));
 
     const ModMenu = ([
       '**{}ban [mention user]** to ban someone.',
@@ -68,7 +68,7 @@ module.exports = {
       '**{}undeafen [mention user]** to undeafen someone.',
       '**{}rmute [mention user]** to give someone a muted role! (Disclaimer: this does not create a Muted role, manually remove the mute role to unmute)',
       '**{}chipsprefix on** to begin custom prefix setup, use **{}chipsprefix off** to turn off custom prefix',
-    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix)));
+    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix.replace(/`/g,'\\`'))));
 
     const FunMenu = ([
       '**{}con4 length width** to play connect four.',
@@ -86,7 +86,7 @@ module.exports = {
       '**{}happy** for some gifs(credits to Tani)',
       '**{}lenny** to make a lenny face.',
       '**{}ship [mention user]** to ship some people.',
-    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix)));
+    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix.replace(/`/g,'\\`'))));
 
     const FunMenu2 = ([
       '**{}nsfw** to get NSFW commands',
@@ -111,7 +111,7 @@ module.exports = {
       '**{}stoptyping** for if a chips command errored and is stuck "typing" in the channel.',
       '**{}password (length) (numerical/alphanumeric/hex/unicode)** to generate a cryptographically strong password!',
       '**{}settopic**/**{}st [newtopic]** to set the channel topic',
-    ].join('\n').replace(/{}/g,_.escapeRegExp(prefix)));
+    ].join('\n').replace(/{}/g, _.escapeRegExp(prefix.replace(/`/g,'\\`'))));
 
     const MusicMenu = ([
       '**{}music demo** to begin the music demo.',
