e823a8d4d1a74864c684fbeb78ded96754dba048
William Zhou <wzhouwzhou@gmail.com>
2017-10-12 12:01:01 -0400

    Rock paper scissors

---
+++ commands/fun/rps.cpscmd/rockpaperscissors.js
@@ -0,0 +1,38 @@
+const rps = a=>b=>a==b?0:!(a+b)[12]^a>b||-1
+const choices = ['rock','paper','scissors'];
+const reg = new RegExp(`${choices.map(e=>_.escapeRegExp(e)).join('|')}`,'i');
+
+module.exports = {
+  name: "roles",
+  async func(msg, { send, channel, author }) {
+    let user, computer = _.sample(choices);
+    const filter = m => {
+      if(m.author.id !== author.id) return false;
+      const cont = _.escapeRegExp(m.content.replace(/[\s.\-,]+/,''));
+      if(reg.test(cont)) {
+        user = cont.match(reg)[0];
+        mCol&&mCol.stop();
+        return true;
+      }
+      return false;
+    };
+    let sentMsg = await send(new Discord.RichEmbed().setTitle('Rock Paper Scissors').setDescription('Choose Rock, Paper, or Scissors!'));
+    mCol = channel.createMessageCollector(
+      filter,
+      { time: 1000*(time+1), maxMatches: 1 }
+    );
+    mCol.on('end', collected => {
+      if(!user&&!collected.first()) return send('Timed out');
+      else {
+        const result = (() => {
+          switch(rps(user)(computer)){
+            case 0: return ('It was a tie!');
+            case 1: return ('You won!');
+            case -1:return ('The computer won!');
+          }
+        })();
+        sentMsg.edit(result, new Discord.RichEmbed().setTitle('Rock Paper Scissors').setDescription(`The computer went ${computer}`));
+      }
+    });
+  }
+};
