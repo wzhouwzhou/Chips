4fbde23af2e806f773b089ea4e8be22cb6a036cf
William Zhou <wzhouwzhou@gmail.com>
2017-10-12 17:31:26 -0400

    Fix -roles

--- commands/info/roles.cpscmd/roles.js
+++ commands/info/roles.cpscmd/roles.js
@@ -3,6 +3,6 @@ module.exports = {
   async func(msg, { send, guild, member }) {
     if(!guild)
       return send('You must be in a server to use this!');
-    return send(new Discord.MessageEmbed().setColor(member.displayColor).setTitle(`Role List (${guild.roles.size})`).setDescription(guild._sortedRoles.map(e=>_.escapeRegExp(e.name)).reverse().join(', ')));
+    return send(new Discord.MessageEmbed().setColor(member.displayColor).setTitle(`Role List (${guild.roles.size})`).setDescription(guild._sortedRoles().map(e=>_.escapeRegExp(e.name)).reverse().join(', ')));
   }
 };
