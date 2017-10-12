5888b52303eabf89ddcaa1ac710b63cd3f29a49e
William Zhou <wzhouwzhou@gmail.com>
2017-10-12 14:37:32 -0400

    Blank

--- rewrite-all/src/struct/games/chess/ChessGame.js
+++ rewrite-all/src/struct/games/chess/ChessGame.js
@@ -55,7 +55,7 @@ const ChessGame = class ChessGame extends require('../BoardGame').BoardGame {
   embedify (end = false) {
     if(!this.embed) throw new Error('Embed is missing !!11!1!!!!');
     this.embed = new (this.embed.constructor);
-    this.embed.addField(end?this.game.in_draw()?'The game was a draw!':this.turn&&this.turn.toLowerCase()==='black'?'Black won!':'White won!':`${this.turn} to move`,'\u0200');
+    this.embed.addField(end?this.game.in_draw()?'The game was a draw!':this.turn&&this.turn.toLowerCase()==='black'?'Black won!':'White won!':`${this.turn} to move`,'\u200B');
     this.embed.setDescription(this.toString());
     this.embed.addField('Last move', this.game.history()&&this.game.history()[0]?this.game.history().reverse()[0]:'None');
     this.embed.setAuthor('Chess');
