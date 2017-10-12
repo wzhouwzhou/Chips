b02a0106d8a419e8d0632abf763eadfb2df28739
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-12 16:53:21 +0200

    lol

--- commands/info/roles.cpscmd/roles.js
+++ commands/info/roles.cpscmd/roles.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "roles",
   async func(msg, { send, guild, member }) {
     if(!guild)
-      return send('You must be in a server to use this');
+      return send('You must be in a server to use this!');
     return send(new Discord.RichEmbed().setColor(member.displayColor).setTitle(`Role List (${guild.roles.size})`).setDescription(guild._sortedRoles.map(e=>_.escapeRegExp(e.name)).reverse().join(', ')));
   }
 };
