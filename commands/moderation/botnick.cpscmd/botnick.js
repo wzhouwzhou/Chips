1e9a6c8db46a648536992c0aa1e085b928a3851b
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-13 21:56:33 +0200

    naisu

--- commands/moderation/botnick.cpscmd/botnick.js
+++ commands/moderation/botnick.cpscmd/botnick.js
@@ -1,3 +1,8 @@
+let embed = (new Discord.MessageEmbed)
+  .setTitle('Bot nickname')
+  .setDescription(suffix)
+  .setColor(member.displayColor);
+
 module.exports = {
   name: "botnick",
   async func(msg, { send, guild, suffix }) {
@@ -7,6 +12,8 @@ module.exports = {
       return send('Nothing provided to set as my nick');
     if(!guild.me.hasPermission('CHANGE_NICKNAME'))
       return send('I do not have permission to change my own nickname!');
+    if(!member.hasPermission("MANAGE_NICKNAMES"))
+      return;
     if(suffix.length>32)
       return send('My nickname can only be a maximum of 32 characters in length!');
     await guild.me.setNickname(suffix);
