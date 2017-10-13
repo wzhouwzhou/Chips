a4005ec1ad164afb021a37fd7333c830421b034e
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-13 09:06:00 +0200

    owo

---
+++ commands/info/serverowner.cpscmd/index.js
@@ -0,0 +1,19 @@
+
+let so = require('./serverowner');
+
+console.log('[CPSCMD][INFO][roles] Building objects...');
+
+so.metadata = {
+  category: require('../').category,
+  description: 'what do you think.',
+  usage: 'serverowner',
+  example: 'serverowner',
+  perm: [['global.info.info']],
+  cusomperm: ['SEND_MESSAGES'],
+};
+
+console.log('[CPSCMD][INFO][roles] Build objects complete!');
+module.exports = [
+  [so.name,so],
+  ['serverowner', so],
+];
---
+++ commands/info/serverowner.cpscmd/serverowner.js
@@ -0,0 +1,8 @@
+let owner = guild.ownerID
+
+module.exports = {
+    name: "serverowner",
+    async func(msg, { send }) {
+      return send(`<@!${owner}>`)
+    }
+  };
