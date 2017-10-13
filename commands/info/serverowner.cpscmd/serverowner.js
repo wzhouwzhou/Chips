db993e34219d561a290e6152d583464c91b7f04d
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-13 13:28:30 +0200

    uhm

--- commands/info/serverowner.cpscmd/serverowner.js
+++ commands/info/serverowner.cpscmd/serverowner.js
@@ -1,11 +1,8 @@
-const embed = (new Discord.MessageEmbed)
-.setTitle('Channel Topic')
-.setDescription(`<@${guild.ownerID}`)
-.setColor(member.displayColor);
+const embed = (new Discord.MessageEmbed).setDescription(`<@${guild.ownerID}`).setColor(member.displayColor);
 
 module.exports = {
     name: "serverowner",
-    async func(msg, { send, guild }) {
+    async func(msg, { send, guild, member }) {
       return send(embed);
     }
 };
