fc443eb7945978842281f69ef0b3c67ca926bcd9
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-13 13:26:55 +0200

    o

--- commands/info/serverowner.cpscmd/serverowner.js
+++ commands/info/serverowner.cpscmd/serverowner.js
@@ -1,6 +1,11 @@
+const embed = (new Discord.MessageEmbed)
+.setTitle('Channel Topic')
+.setDescription(`<@${guild.ownerID}`)
+.setColor(member.displayColor);
+
 module.exports = {
     name: "serverowner",
     async func(msg, { send, guild }) {
-      return send(`<@${guild.ownerID}>`);
+      return send(embed);
     }
 };
