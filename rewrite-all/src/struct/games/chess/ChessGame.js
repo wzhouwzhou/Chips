63f68541fb84ee63b6ee72725c1412cefac28ae5
William Zhou <wzhouwzhou@gmail.com>
2017-10-15 11:34:46 -0400

    reverse array

--- rewrite-all/src/struct/games/chess/ChessGame.js
+++ rewrite-all/src/struct/games/chess/ChessGame.js
@@ -208,7 +208,7 @@ const ChessGame = class ChessGame extends require('../BoardGame').BoardGame {
     if((/w(?:hite)?/).test(colorBottom))
       str = this.board.map((e,i)=>[Constants.numbersA[i+1]].concat(Object.keys(e).map(k=>e[k]).reverse()).join('')).reverse().concat(label2.join('')).join('\n');
     else
-      str = this.board.map((e,i)=>[Object.assign([], Constants.numbersA[i+1]).reverse()].concat(Object.keys(e).map(k=>e[k])).join('')).concat(Object.assign([], label2).reverse().join('')).join('\n');
+      str = this.board.map((e,i)=>[Object.assign([], Constants.numbersA).reverse()[i+1]].concat(Object.keys(e).map(k=>e[k])).join('')).concat(Object.assign([], label2).reverse().join('')).join('\n');
     this.board.reverse();
 
     return str;
