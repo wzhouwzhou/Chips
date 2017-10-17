8ba8ca927d28017cf69282829f8f5c428f1f9f72
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-17 19:35:07 +0200

    Revert "triggered"
    This reverts commit 6d9b8f7d2bf5dae09e6497d1d4bed02138808784.

---
+++ commands/moderation/setchannel.cpscmd/setchannel.js
@@ -0,0 +1,38 @@
+const top = args[1];
+const nam = args[1];
+
+const embed = (new Discord.RichEmbed)
+  .setTitle('Help')
+  .setDescription('All commands for \"setchannel\" are coming soon!')
+  .setColor(member.displayColor);
+
+module.exports = {
+    name: "setchannel",
+    async func(msg, { send, guild, args, member, author }) {
+      
+      if(!author.id==='205608598233939970')
+        return send('only lucas can aboose lmao');
+      
+      if(!guild)
+        return send('You must be in a server to use this');
+
+      if (!args[0])
+        return send(embed);
+  
+      if (args[0]==='help')
+        return send(embed);
+          
+      if (args[0]==='name')
+        if (args[1]<100||args[1]>2)
+          return channel.setName(nam)
+        if (args[1]<100||args[1]>2) 
+          return send('Name stuff...');
+  
+      if (args[0]==='topic')
+        if (args[1]&&args[1].length<1024)
+          return channel.setTopic(top)
+        if (!args[1]&&args[1].length<1024)
+          return send('Topic stuff..');
+    }
+  
+};
