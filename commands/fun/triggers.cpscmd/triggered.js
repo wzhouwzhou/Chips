a46a0308262a01f8b77ed739a38577bc80529d38
Ethan <elawrence152@gmail.com>
2017-10-16 20:50:42 -0400

    uhhhh i did nothing wrong in the previous commit

--- commands/fun/triggers.cpscmd/triggered.js
+++ commands/fun/triggers.cpscmd/triggered.js
@@ -2,7 +2,7 @@ const gifs = [
   "https://i.imgur.com/th1RDAK.gif",
   "https://i.imgur.com/BvVEdP0.gif",
   "https://i.imgur.com/JnXRZUf.gif",
-  "https://i.imgur.com/HgT06sl.gif,
+  "https://i.imgur.com/HgT06sl.gif",
   "https://i.imgur.com/uCBhiRw.gif",
   "https://i.imgur.com/kTIU4TF.gif",
   "https://i.imgur.com/HrshEbg.gif",
