152efd95b04c0e35f04d838ef97f855e621aef6c
William Zhou <wzhouwzhou@gmail.com>
2017-10-17 12:42:50 -0400

    Fix addemoji

--- commands/moderation/addemoji.cpscmd/addemoji.js
+++ commands/moderation/addemoji.cpscmd/addemoji.js
@@ -1,27 +1,30 @@
 module.exports = {
-    name: "addemoji",
-    async func(msg, { send, args, member, guild }) {
+  name: "addemoji",
+  async func(msg, { send, args, member, guild }) {
 
     if(!member.hasPermission('MANAGE_EMOJIS'))
       return send('no');
-    
-    const emojiurl = args[1].match(/^(https?\:\/\/[^.]+\.[^]+)$/)[1]    
-    
-    if (!args[0]) 
-      return reply("No emoji name given :(");   
-    
+
+    if (!args[0])
+      return reply("No emoji name given :(");
+
     let name = args[0];
 
-    if(!args[1]) 
+    if(!args[1])
       return send('no url');
 
-    if(!args[1].match(/^https?\:\/\/[^.]+\.[^]+$/)) 
+    const emojiurl = (args[1].match(/^(https?:\/\/[^.]+\.[^]+)$/)||[0,null])[1];
+
+    if(!args[1].match(/^https?:\/\/[^.]+\.[^]+$/))
       return send("ensure you've given a url");
 
-    
-    guild.createEmoji(emojiurl, name)
-      .then(emoji => send(`Created new emoji with name ${emoji.name}!`))
-      .catch(console.error);
-
+    let emoji;
+    try {
+      emoji = await guild.createEmoji(emojiurl, name);
+      send(`Created new emoji with name ${emoji.name}!`);
+    }catch(err){
+      send('The emoji could not be created…');
+      throw err;
     }
-}   
+  }
+};
