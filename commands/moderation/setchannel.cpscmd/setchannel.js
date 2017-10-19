f690393f984e608dfe12474f2013ba5537f616a8
William Zhou <wzhouwzhou@gmail.com>
2017-10-19 00:26:54 -0400

    success

--- commands/moderation/setchannel.cpscmd/setchannel.js
+++ commands/moderation/setchannel.cpscmd/setchannel.js
@@ -19,13 +19,17 @@ module.exports = {
           return send('No name given to set');
         else if(!suffix.substring(suffix.indexOf(args[1])).match(/^[0-9a-z\-_]+$/i))
           return send('Text channel name must be alphanumeric with dashes or underscores.');
-        else
-          return await channel.setName(suffix.substring(suffix.indexOf(args[1])));
+        else {
+          await channel.setName(suffix.substring(suffix.indexOf(args[1])));
+          return send(`Channel name set to ${_.escapeRegExp(channel.name).replace(/@/g,'(at)')} successfully`);
+        }
 
       if (args[0]==='topic')
         if(!args[1])
           return send('No topic given to set');
-        else
-          return await channel.setTopic(suffix.substring(suffix.indexOf(args[1])));
+        else {
+          await channel.setTopic(suffix.substring(suffix.indexOf(args[1])));
+          return send(`Channel topic set to ${_.escapeRegExp(channel.topic).replace(/@/g,'(at)')} successfully`);
+        }
     }
 };
