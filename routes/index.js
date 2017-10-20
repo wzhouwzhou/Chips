76e7b12fdba2f27881ff9968b967c3ca2bdd766e
William Zhou <wzhouwzhou@gmail.com>
2017-10-19 20:40:11 -0400

    Hmm

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-// main chips info 
+// main chips info
 const express = require('express');
 const router = express.Router();
+const snek = require('snekfetch');
 
 router.get("/", (req, res) => {
-  res.render("index", { timestamp: new Date().toString() });
+  snek.get('http://localhost:8880/api/guildcount').then(resp => {
+    res.render("index", {
+      timestamp: new Date().toString(),
+      servercount: resp.body?resp.body.count:450,
+    });
+  });
 });
 
 module.exports = router;
