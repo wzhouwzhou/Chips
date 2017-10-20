a593c9b0928ac42c41c6999ffa964272b1db5230
William Zhou <wzhouwzhou@gmail.com>
2017-10-20 16:15:02 -0400

    lets try this

--- commands/info/serverowner.cpscmd/serverowner.js
+++ commands/info/serverowner.cpscmd/serverowner.js
@@ -1,11 +1,11 @@
 module.exports = {
-    name: "serverowner",
-    async func(msg, { send, guild, member }) {
-      
-      let embed = (new Discord.MessageEmbed)
-      .setDescription(`<@${guild.ownerID}>`)
-      .setColor(member.displayColor);
-      
-      return send(embed);
-    }
+  name: "serverowner",
+  async func(msg, { send, guild, member }) {
+
+    let embed = (new Discord.MessageEmbed)
+    .setDescription([]+guild.members.get(ownerID))
+    .setColor(member.displayColor);
+
+    return send(embed);
+  }
 };
--- rewrite-all/src/struct/games/chess/ChessGame.js
+++ rewrite-all/src/struct/games/chess/ChessGame.js
@@ -73,7 +73,22 @@ const ChessGame = class ChessGame extends require('../BoardGame').BoardGame {
   embedify (end = false) {
     if(!this.embed) throw new Error('Embed is missing !!11!1!!!!');
     this.embed = new (this.embed.constructor);
-    this.embed.addField(end?this.game.in_draw()?'The game was a draw!': this.turn? `${this.movers.get(this.turn.toLowerCase())? this.movers.get(this.turn.toLowerCase()==='white'?'black':'white').username: this.turn} won${this.game.history()?' after '+ this.game.history().length + ' moves':''}!` :`${this.movers.get(this.turn.toLowerCase())? this.movers.get(this.turn.toLowerCase()).username: this.turn} to move`:'White to move' ,`Last move: ${this.game.history()&&this.game.history()[0]?this.game.history().reverse()[0]:'None'}`);
+    let comment = '';
+    if (end) {
+      if(this.game.in_draw())
+        comment += 'The game was a draw';
+      else if(this.turn)
+        if(this.movers.get(this.turn.toLowerCase()))
+          comment += this.movers.get(this.turn.toLowerCase()==='white'?'black':'white').username + 'won';
+      comment +=` after ${this.game.history().length} moves!`;
+    } else {
+      if (this.movers.get(this.turn.toLowerCase()))
+        comment += this.movers.get(this.turn.toLowerCase()).username;
+      else
+        comment += this.turn;
+      comment += ' to move';
+    }
+    this.embed.addField(comment, `Last move: ${this.game.history()&&this.game.history()[0]?this.game.history().reverse()[0]:'None'}`);
     this.embed.setDescription(this.toString());
     this.embed.setAuthor('Chess');
     this.embed.setTitle(`${this.movers.get('white').username}⬜ vs ⬛${this.movers.get('black').username}`);
