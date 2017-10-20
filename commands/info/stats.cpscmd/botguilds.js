583a88679c1988208d27228744eb5c26fec27189
William Zhou <wzhouwzhou@gmail.com>
2017-10-20 16:48:57 -0400

    bg

---
+++ commands/info/stats.cpscmd/botguilds.js
@@ -0,0 +1,31 @@
+const _ = require('lodash');
+const splitChunkF = require('../../../deps/functions/splitChunkF').default({ _ });
+const firstF = require('../../../deps/functions/firstF').default({ _ });
+const { Paginator } = require('../../../rewrite-all/src/struct/client/Paginator');
+
+module.exports = {
+  name: "botguilds",
+  async func(msg, { client, Discord, send }) {
+    const results = await client.shard.broadcastEval('Array.from(client.guilds.values()).sort((a,b)=>b.members.size-a.members.size).map(g=>[g.name, g.members.size])');
+    let total = results.reduce((a,b)=>[...a,...b],[]);
+    total = splitChunkF(firstF(total, 100).map(e=>`__${_.escapeRegExp(e[0])}__ (${e[1]} members)`), {size: 10});
+
+    const p = new Paginator ( msg,  {
+      type:'paged',
+      embedding: true,
+      fielding: false,
+      text: 'Top 100 largest servers I am in!',
+      pages:
+      [
+        ...total
+      ],
+      }, Discord
+    );
+    try{
+      await p.sendFirst();
+    }catch(err){
+      console.error(err);
+      return send ('Something went wrong...');
+    }
+  }
+};
