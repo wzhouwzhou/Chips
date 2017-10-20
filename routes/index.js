4d19145cce397d99546633fe5f5bb9f62a91bb5e
William Zhou <wzhouwzhou@gmail.com>
2017-10-19 20:46:19 -0400

    How about this

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const snek = require('snekfetch');
 
 router.get("/", (req, res) => {
-  snek.get('http://localhost:8880/api/guildcount').then(resp => {
-    res.render("index", {
-      timestamp: new Date().toString(),
-      servercount: resp.body?resp.body.count:450,
+  snek.get('http://localhost:8880/api/guildcount').then(gc => {
+    snek.get('http://localhost:8880/api/membercount').then(mc => {
+      res.render("index", {
+        timestamp: new Date().toString(),
+        servercount: gc.body?gc.body.count:450,
+        membercount: mc.body?mc.body.count:null,
+      });
     });
   });
 });
