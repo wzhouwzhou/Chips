ab5897cd8990f46d7d01a7e5f44ec5e66455a6b0
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-21 20:53:36 +0200

    Lenyyyy

--- commands/fun/triggers.cpscmd/lenny.js
+++ commands/fun/triggers.cpscmd/lenny.js
@@ -4,7 +4,10 @@ const lenny = [
   "https://i.imgur.com/P8dHrF9.gif",
   "https://i.imgur.com/svNztwx.gif",
   "https://i.imgur.com/0VgpGVM.gif",
-  "https://i.imgur.com/0daFo5U.gif"
+  "https://i.imgur.com/0daFo5U.gif",
+  "https://www.tenor.co/uhvl.gif",
+  "https://www.tenor.co/IEEk.gif",
+  "https://www.tenor.co/vb3T.gif "
 ];
 
 module.exports = {
