76e2fdd303e389bd63c44d120dd65cc7c3516639
William Zhou <wzhouwzhou@gmail.com>
2017-10-23 11:57:15 -0400

    ?

--- routes/error.js
+++ routes/error.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (err, req, res) => {
-  res.status(err.status || 500);
   res.render('error', { type: err.status || 500, timestamp: new Date().toString() });
 });
 
--- setup/AppSetup.js
+++ setup/AppSetup.js
@@ -151,11 +151,11 @@ module.exports = function() {
 
   // error handler
   app.use((req, res, next) => {
-    var err = new Error('Not Found');
+    let err = {error: 'Not found'}; //new Error('Not Found');
     err.status = 404;
     res.locals.message = err.message;
     res.locals.error = {};//req.app.get('env') === 'development' ? err : {};
-
+    res.status(err.status || 500);
     if (!err.status || err.status == 500) console.error("Internal error: " + err);
     next(err, req, res);
   });
