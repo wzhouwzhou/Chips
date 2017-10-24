01dc42b2feaae6c931cba911e274553799bf6e18
William Zhou <wzhouwzhou@gmail.com>
2017-10-23 23:39:34 -0400

    Typos

--- commands/chipsmusic/music.cpscmd/music.js
+++ commands/chipsmusic/music.cpscmd/music.js
@@ -8,7 +8,7 @@ module.exports = {
   name: "music",
   async func(msg, { args, guild, client, member, channel, reply, send, prefix }) {
     if(!args[0]||args[0]==='help'){
-      return send(new Discord.MessageEmbed().setTitle('Available action').setDescription([
+      return send(new Discord.MessageEmbed().setTitle('Available actions').setDescription([
         `**${_.escapeRegExp(prefix)}${this.name} demo** to start the music module, more instructions will follow.`,
         `**${_.escapeRegExp(prefix)}${this.name} radio** to see details about chips radio`,
       ].join('\n')));
@@ -16,7 +16,7 @@ module.exports = {
     if(args[0]==='radio'){
       return send(new Discord.MessageEmbed().setTitle('Chips radio (Beta™)').setDescription([
         '**Introducing Chips 24/7 radio!**\n',
-        'Step 1: Have a voice channel with **`Chips stream NCS`** or **`Chips Stream Monstercat`** in the name',
+        'Step 1: Have a voice channel with **`Chips Stream NCS`** or **`Chips Stream Monstercat`** in the name',
         'Step 2: Chips will check every 30 minutes and join that voice channel and stream 24/7 ncs or monstercat music',
       ].join('\n')));
     }
