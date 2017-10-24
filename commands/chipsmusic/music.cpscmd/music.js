3950ac45f2919e216837197cfe0e5689524be3d0
William Zhou <wzhouwzhou@gmail.com>
2017-10-23 23:34:15 -0400

    Music help

--- commands/chipsmusic/music.cpscmd/music.js
+++ commands/chipsmusic/music.cpscmd/music.js
@@ -4,15 +4,30 @@ const Handler = require('../../../rewrite-all/src/struct/music/MusicHandler.js')
 const _handlers = new Map();
 
 module.exports = {
+  _handlers,
   name: "music",
-  async func(msg, { args, guild, client, member, channel, reply }) {
+  async func(msg, { args, guild, client, member, channel, reply, send, prefix }) {
+    if(!args||args[0]==='help'){
+      return send(new Discord.MessageEmbed().setTitle('Available action').setDescription([
+        `**{_.escapeRegExp(prefix)}${this.name} demo** to start the music module, more instructions will follow.`,
+        `**{_.escapeRegExp(prefix)}${this.name} radio** to see details about chips radio`,
+      ].join('\n')));
+    }
+    if(args[0]==='radio'){
+      return send(new Discord.MessageEmbed().setTitle('Chips radio (Beta™)').setDescription([
+        '**Introducing Chips 24/7 radio!**\n',
+        'Step 1: Have a voice channel with **`Chips stream NCS`** or **`Chips Stream Monstercat`** in the name',
+        'Step 2: Chips will check every 30 minutes and join that voice channel and stream 24/7 ncs or monstercat music',
+      ].join('\n')));
+    }
+
     let h;
     if(_handlers.has(guild.id)) h = _handlers.get(guild.id);
     else{
-      h = new Handler(guild.id, client);
+      h = new Handler(guild.id, client, prefix);
       _handlers.set(guild.id, h);
     }
-    if(args&&args[0]==='demo'){
+    if(args[0]==='demo'){
       if(!member.voiceChannel) return reply('Please join a voice channel first');
       h.startDemo(member.voiceChannel,channel);
     }
