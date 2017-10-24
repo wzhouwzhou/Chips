66fb8fa5febed808b4d7b47446d0936c9f871231
ClashWithMeYT <32519731+ClashWithMeYT@users.noreply.github.com>
2017-10-24 22:07:39 +0530

    Create fliptext.js

---
+++ commands/fliptext.js
@@ -0,0 +1,18 @@
+const flip = require('flip-text')
+let arg = args.join(" ");
+
+let flipped = flip(arg);
+
+if (!arg) {
+        message.channel.send(':no_entry_sign: You must specify some text!').then(m => m.delete(2000));
+        return;
+    }
+
+message.channel.send("```" + flipped + "```")
+
+module.exports = {
+  name: "fliptext",
+  perm: ["global.flip"],
+  async func(msg, { send }) {
+  }
+};
