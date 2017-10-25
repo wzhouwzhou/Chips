12d98363b49d0d9ea9145d9f0661e232c850fb01
William Zhou <wzhouwzhou@gmail.com>
2017-10-25 12:13:32 -0400

    Clean Database.js

--- rewrite-all/src/struct/util/Database.js
+++ rewrite-all/src/struct/util/Database.js
@@ -1,19 +1,19 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 
+const Logger = require('../client/Logger').create('Database', 'Main');
+
 const PermissionsHandler = require('../../../../handlers/Permissions');
 const GoogleSpreadsheet = require('google-spreadsheet');
 
 const reql = require('rethinkdbdash');
 
 const Database = class Database {
-
   /**
    * Constructs a database object
    *
    * @method constructor
    * @param  {Discord.Client}    client The bot client this database belongs to.
-   * @return {Database}          This database
    */
   constructor(client) {
     this.client = client;
@@ -30,13 +30,13 @@ const Database = class Database {
    * Connects this database to Reql
    *
    * @method connect
-   * @return {Database} Returns this database
+   * @returns {Database} Returns this database
    */
-  connect () {
+  connect() {
     this.rethink = null;
     this.rethink = reql({
       servers: [
-        {host: process.env.RETHINKIP, port: process.env.RETHINKPORT}
+        { host: process.env.RETHINKIP, port: process.env.RETHINKPORT },
       ],
       db: 'Chips',
       user: 'admin',
@@ -49,10 +49,10 @@ const Database = class Database {
    * Helper function to ensure that rethinkdb is initiated and usable.
    *
    * @method ensureRethink
-   * @return {Database}      this if test passed
+   * @returns {Database}      this if test passed
    */
-  ensureRethink () {
-    if(!this.rethink) throw new Error('Rethink not connect');
+  ensureRethink() {
+    if (!this.rethink) throw new Error('Rethink not connect');
     return this;
   }
 
@@ -61,13 +61,13 @@ const Database = class Database {
    * - Namely, rethinkdb and google sheets;
    *
    * @method load
-   * @return {Promise} resolves to this
+   * @returns {Promise} resolves to this
    */
-  async load () {
+  async load() {
     this.ensureRethink();
 
     this.startLog = await this.rethink.table('botStartLog').run();
-    this.latestStart = this.startLog&&this.startLog[0]?this.startLog[0]['status']:'Unknown';
+    this.latestStart = this.startLog && this.startLog[0] ? this.startLog[0].status : 'Unknown';
 
     this.sinxUsers = new Map();
     await this.loadPrivateGS();
@@ -80,18 +80,18 @@ const Database = class Database {
    * Loads internal google sheets database
    *
    * @method loadPrivateGS
-   * @return {Promise}      Resolves to this
+   * @returns {Promise}      Resolves to this
    */
-  loadPrivateGS () {
-    return new Promise((res,rej) => {
+  loadPrivateGS() {
+    return new Promise((res, rej) => {
       this.numloads = -1;
 
       this.privateSheet = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
       this._sheets = {};
 
       this.privateSheet.useServiceAccountAuth(this.glogin, () => {
-        this.privateSheet.getInfo((err,info) => {
-          if(err) return rej(err);
+        this.privateSheet.getInfo((err, info) => {
+          if (err) return rej(err);
           this.numloads = info.worksheets.length;
           for (const sheet of info.worksheets) {
             this.loadGSheet(sheet);
@@ -108,16 +108,16 @@ const Database = class Database {
    * Loads SBK sheets
    *
    * @method loadSBKGS
-   * @return {Promise}  Resolves to this
+   * @returns {Promise}  Resolves to this
    */
-  loadSBKGS () {
+  loadSBKGS() {
     return new Promise((res, rej) => {
       this.sbkPoints = new GoogleSpreadsheet('1UHXrqeaapyCXv-xJV7YmA9r5c_6tjjS9t_55YJhIFVc');
       this.numsbkloads = -1;
 
       this.sbkPoints.useServiceAccountAuth(this.glogin, () => {
-        this.sbkPoints.getInfo((err,info) => {
-          if(err) return rej(err);
+        this.sbkPoints.getInfo((err, info) => {
+          if (err) return rej(err);
           this.numsbkloads = info.worksheets.length;
           for (const sheet of info.worksheets) {
             this.loadGSheet(sheet);
@@ -134,37 +134,39 @@ const Database = class Database {
    *
    * @method loadGSheet
    * @param  {GoogleSpreadsheet}   gsheet The sheet to load
-   * @return {Promise}         Resolves to this._sheets
+   * @returns {Promise}         Resolves to this._sheets
    */
-  async loadGSheet (gsheet) {
+  async loadGSheet(gsheet) {
     this._sheets[gsheet.title] = gsheet;
+    const toLoad = [];
     for (const preF in this.loadFunctions) {
-      console.log('Seeing preF: '+preF);
-      if(gsheet.title === preF) {
-        console.log('Loading preF: '+preF);
-        await this.loadFunctions[preF].load({
+      Logger.debug(`Seeing preF: ${preF}`);
+      if (gsheet.title === preF) {
+        Logger.info(`Loading preF: ${preF}`);
+        toLoad.push(this.loadFunctions[preF].load({
           sheet: gsheet,
           client: this.client,
           database: this,
-          Permissions: PermissionsHandler
-        });
+          Permissions: PermissionsHandler,
+        }));
       }
     }
+    await Promise.all(toLoad);
     return this._sheets;
   }
 
-  async getTable (tablename, cache = true) {
+  async getTable(tablename, cache = true) {
     this.ensureRethink();
 
     const table = await this.rethink.table(tablename).run();
-    if(cache) {
-      if(!this.rtables) this.rtables = {};
+    if (cache) {
+      if (!this.rtables) this.rtables = {};
       this.rtables[tablename] = table;
     }
     return table;
   }
 
-  async insertInTable (tablename, id = Date.now(), data = { status: false }) {
+  async insertInTable(tablename, id = Date.now(), data = { status: false }) {
     this.ensureRethink();
 
     const entry = await this.rethink.table(tablename).insert(Object.assign(
@@ -172,9 +174,8 @@ const Database = class Database {
       data,
     ), {
       conflict: 'replace',
-    }).run(_=>_);
-    if(entry.inserted == 1 || entry.replaced == 1)
-      return entry;
+    }).run(_ => _);
+    if (entry.inserted === 1 || entry.replaced === 1) return entry;
     throw new Error(`Data id [${id}] was not inserted: ${JSON.stringify(data)}`);
   }
 };
