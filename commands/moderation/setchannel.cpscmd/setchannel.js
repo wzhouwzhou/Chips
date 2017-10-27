194b7472807a22836ec0d832738741a1ad35b185
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-27 09:38:12 +0200

    owo

--- commands/moderation/setchannel.cpscmd/setchannel.js
+++ commands/moderation/setchannel.cpscmd/setchannel.js
@@ -16,6 +16,10 @@ module.exports = {
           .setColor(member.displayColor));
 
       if (args[0]==='name')
+        if(suffix.substring(suffix.indexOf(args[1]))>100)   
+          return send('The channel name can only be a maximum of 100 characters in length!');        
+        if(suffix.substring(suffix.indexOf(args[1]))<2)
+          return send('The channel name can only be a minumum of 2 characters in length');
         if(!args[1])
           return send('No name given to set');
         else if(!suffix.substring(suffix.indexOf(args[1])).match(/^[0-9a-z\-_]+$/i))
@@ -25,15 +29,15 @@ module.exports = {
           return send(`Channel name set to ${_.escapeRegExp(channel.name).replace(/@/g,'(at)')} successfully`);
         }
       
-      if (args[0]==='position') 
+      if (args[0]==='position') {
         if(!args[1]) 
           return send('No position given.');
         if(!suffix.substring(suffix.indexOf(args[1])).match(/\d+/g)) 
           return send('No number given');
-        else { (args[1])
-            await channel.setPosition(`${suffix.substring(suffix.indexOf(args[1])) - 1}`);
-            return send(`Channel position set to ${_.escapeRegExp(channel.position - 1).replace(/@/g, '(at)')} position higher / lower succesfully!`);
-        }
+        if(args[1])
+          await channel.setPosition(`${suffix.substring(suffix.indexOf(args[1])) - 1}`);
+          return send(`Channel position set to ${_.escapeRegExp(channel.position - 1).replace(/@/g, '(at)')} succesfully`)
+      }
       
       if (args[0]==='topic')
         if(!args[1])
