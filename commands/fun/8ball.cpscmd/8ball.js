8deb7e19b3536828ac898c1fc0279f53021a6891
William Zhou <wzhouwzhou@gmail.com>
2017-10-28 17:18:55 -0400

    Fix again

--- commands/fun/8ball.cpscmd/8ball.js
+++ commands/fun/8ball.cpscmd/8ball.js
@@ -3,21 +3,24 @@ const answer = [
   'No.',
   'Yes!',
   'Maybe.',
-  `${client.member.size} people would agree.`,
+  '{shardusercount} people would agree.',
   'Eat chips!',
 ];
 
 module.exports = {
   name: '8ball',
-  func(msg, { send, args, suffix, author, Discord, member }) {
+  func(msg, { send, args, suffix, Discord, member, client }) {
     if (!args[0]) return send('Do you have a question?');
 
     if (suffix.split('').reverse()[0] !== '?') return send('Question mark?');
 
     const embed = (new Discord.MessageEmbed)
-      .setDescription(`🎱 | **${(answer[~~(answer.length * Math.random())])}**`)
+      .setDescription(`🎱 | **${
+        answer[~~(answer.length * Math.random())]
+          .replace(/\{shardusercount\}/g, client.users.size)
+      }**`)
       .setColor(member ? member.displayColor : 34203);
 
-      return send(embed);
+    return send(embed);
   },
 };
