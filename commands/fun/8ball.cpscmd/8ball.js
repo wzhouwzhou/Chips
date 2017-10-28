0c10b8530d224ba392eca88292f39f35247dd48c
William Zhou <wzhouwzhou@gmail.com>
2017-10-28 16:10:26 -0400

    Fixed 8ball

--- commands/fun/8ball.cpscmd/8ball.js
+++ commands/fun/8ball.cpscmd/8ball.js
@@ -5,21 +5,19 @@ const answer = [
   'Maybe.',
 ];
 
-const embed = (new Discord.MessageEmbed)
-      .setDescription(`🎱 | ${(answer[~~(answer.length * Math.random())])}`)
-      .setColor(member.displayColor);
-
 module.exports = {
   name: '8ball',
-  async func(msg, { send, args, suffix, member }) {
+  func(msg, { send, args, suffix, author, Discord, member }) {
     if (!args[0]) return send('Do you have a question?');
 
-    if (suffix.split('').reverse()[0]!=='?') return send('Question mark?');
+    if (suffix.split('').reverse()[0] !== '?') return send('Question mark?');
 
-    if (author.id = '205608598233939970')
-      return send('lucas is a narb')
+    if (author.id === '205608598233939970') return send('lucas is a narb');
 
-    if (args[0]) return send(embed)
+    const embed = (new Discord.MessageEmbed)
+      .setDescription(`🎱 | ${(answer[~~(answer.length * Math.random())])}`)
+      .setColor(member ? member.displayColor : 34203);
 
+      return send(embed);
   },
 };
