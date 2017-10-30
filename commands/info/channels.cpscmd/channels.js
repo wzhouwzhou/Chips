c5931b857a45d2bfd66cf997b0869a1a6ee639e2
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-30 07:21:08 +0100

    Channels,

---
+++ commands/info/channels.cpscmd/channels.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'channels',
+    async func(msg, { send, guild, member }) {
+        if (guild) {
+        return send((new Discord.MessageEmbed).setDescription(guild.channels.array().map(e => e + []).join(' ')).setTitle(`${guild.channels.size} channels!`)
+          .setColor(member.displayColor));
+      } else { return 'You need to be in a server to use this command!'; }
+    },
+  };
+  
