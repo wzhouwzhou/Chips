7704ad378d4a19ba77edbb2fbb0e79515d0e9480
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-30 07:37:39 +0100

    another test

--- commands/info/channels.cpscmd/channels.js
+++ commands/info/channels.cpscmd/channels.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'channels',
     async func(msg, { send, guild, member }) {
         if (guild) {
-        let chanels = guild.channels.filter(c => c.type === 'text')
-          return send((new Discord.MessageEmbed).setDescription(guild.chanels.array().map(e => e + []).join(' ')).setTitle(`${guild.channels.size} channels!`)
+        const text = guild.channels.filter(c => c.type === 'text')
+          return send((new Discord.MessageEmbed).setDescription(guild.channels.array(text).map(e => e + []).join(' ')).setTitle(`${guild.channels.size} channels!`)
           .setColor(member.displayColor));
       } else { return 'You need to be in a server to use this command!'; }
     },
