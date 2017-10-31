0bd81d911f3a1491e41e23baa205267e63dd3e22
lucaslsg <lucaslucaso2002@gmail.com>
2017-10-31 15:09:25 +0100

    Added setNSFW to "Setchannel"

--- commands/moderation/setchannel.cpscmd/setchannel.js
+++ commands/moderation/setchannel.cpscmd/setchannel.js
@@ -10,6 +10,7 @@ module.exports = {
           '{}<name> name [new-name] - sets the channel name.',
           '{}<name> topic [new topic] - sets the channel topic.',
           '{}<name> position [new position] - sets the channel position.',
+          '{}<name> nsfw [true/false] - bla bla bla no comment',
         ].map(e => e.replace(/^(\{\})\s*(<name>)\s+(\w+)\s*(\[[\w\s-_]+\])?\s*-\s*([^]*)$/gi, (e, pre, nm, item, args, suff) => `**${_.escapeRegExp(prefix || '-')}${this.name || 'setchannel'} ${item} ${args || ''}** ${suff}`)).join('\n'))
         .setColor(member.displayColor));
     }
@@ -31,9 +32,21 @@ module.exports = {
     }
 
     if (args[0] === 'topic') {
-      if (!args[1]) { return send('No topic given to set'); } else {
+      if (!args[1]) { return send('No topic given to set.'); } else {
         await channel.setTopic(suffix.substring(suffix.indexOf(args[1])));
         return send(`Channel topic set to ${_.escapeRegExp(channel.topic).replace(/@/g, '(at)')} successfully`);
+      }
+    }
+    if (args[0] === 'nsfw') {
+      if (!args[1] === 'true' || 'false') { return send('No answer given to set.'); } else {
+        if (args[1] === 'true') { 
+          await channel.setNSFW(true, `Channel set to true by ${author}`);
+          return send('did it bish')}
+        if (args[1] === 'false') {
+          await channel.setNSFW(false, `Channel set to false by ${author}`);
+          return send('did it bish');
+        }
+
       }
     }
   },
