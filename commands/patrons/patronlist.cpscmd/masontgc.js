a8001efcf4154b888c688bf395b40d1531c5c311
Ethan <elawrence152@gmail.com>
2017-10-30 22:19:23 -0400

    Create masontgc.js

---
+++ commands/patrons/patronlist.cpscmd/masontgc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'masontgc',
+  async func(msg, { send, Discord }) {
+    const embed = new Discord.MessageEmbed()
+      .setAuthor('MasonTheCoolGuy#8263', 'https://i.imgur.com/KkRF64b.png')
+      .setTitle('')
+      .setDescription('The third Patron of Chips, :heart: Mason! Thank you for showing your support!')
+      .setTimestamp(new Date())
+      .setImage('https://i.imgur.com/Wlty9eI.png')
+      .setColor(0x33c9e0);
+    await send('Loading......').then(m => setTimeout(() => m.delete({ timeout: 800 })), 1000);
+    return send('', { embed });
+  },
+};
