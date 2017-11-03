e7091a2cafa0a6dc1f0bd3614fb383f0e4a401d7
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-03 15:20:27 +0100

    Update: Removed some "bugs"

--- commands/moderation/setchannelname.cpscmd/setchannelname.js
+++ commands/moderation/setchannelname.cpscmd/setchannelname.js
@@ -1,8 +1,6 @@
 module.exports = {
   name: 'setchannelname',
   async func(msg, { send, guild, member, args, channel, suffix, author }) {
-    if (!member.hasPermission('MANAGE_CHANNELS')) return send('You need `MANAGE_CHANNELS` permissions to use this command!');
-    if (!guild) return;
     if (!suffix) return send('Nothing provided to set as channel name');
     if (!suffix.substring(suffix.indexOf(args[0])).match(/^[0-9a-z\-_]+$/i)) return send('Text channel name must be alphanumeric with dashes or underscores.');
     if (suffix.length > 100) return send('The channel name can only be a maximum of 100 characters in length!');
