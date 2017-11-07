abd89fc86b6e513ce378366f2ddec641923c6daf
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-07 17:52:49 +0100

    Update || -math.js

--- commands/utility/-math.cpscmd/-math.js
+++ commands/utility/-math.cpscmd/-math.js
@@ -3,14 +3,28 @@ const splitter = (/(\s|,)+/)
 
 module.exports = {
   name: '-math',
-  async func(msg, { member, author, content, channel, args, Discord, reply }) {
+  async func(msg, { suffix, args, send }) {
 
-    if(args[0]==='add')
+    if (!suffix.substring(suffix.indexOf(args[0])).match(/[0-9,]/))
+      return send('Numbers/Comma\'s?')
+    
+    if(args[0].match(/add|plus/)) 
       suffix.split(splitter)
-      return send(`${math.add(splitter)}`)
-
-  }
+      return send(`${math.add(splitter)}`) 
+    
+    if(args[0]==='round') 
+      suffix.split(splitter)
+      return send(`${math.round(spliter)}`)
+    
+    if(args[0].match(/min|minimum|low|lowest/)) 
+      suffix.split(splitter)
+      return send(`${math.min(splitter)}`)
+    
+    if(args[0].match(/highest|high|maximum|max/)) 
+      suffix.split(splitter)
+      return send(`${math.max(splitter)}`) 
+    
+    if(args[0].match(/pi|mathpi/)) 
+      return send(`${math.PI}`)
+  }  
 }
-
-   
-
