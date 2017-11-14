fd1b894ece92ad376f280716ee00bc8e172f1572
Ethan <elawrence152@gmail.com>
2017-11-13 21:58:01 -0500

    Update index.js

--- commands/fun/insult.cpscmd/index.js
+++ commands/fun/insult.cpscmd/index.js
@@ -1,4 +1,5 @@
 let insult = require('./insult');
+let kill = require('./kill');
 
 console.log('[CPSCMD][FUN][insult] Building objects...');
 insult.metadata = {
@@ -10,7 +11,20 @@ insult.metadata = {
   customperm: ['SEND_MESSAGES'],
 };
 
+console.log('[CPSCMD][FUN][kill] Building objects...');
+kill.metadata = {
+  category: require('../').category,
+  description: 'Kills someone.',
+  usage: 'kill (mention)',
+  example: 'kill @Evildeathpro#4442',
+  perm: [['global.fun.insult']],
+  customperm: ['SEND_MESSAGES'],
+};
+
 console.log('[CPSCMD][FUN][insult] Build objects complete!');
 module.exports = [
   [insult.name, insult],
+  [kill.name, kill],
+  ['kill', kill],
+  ['omar', kill],
 ];
