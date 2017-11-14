66bdc10f4f3032ae45d4e7f46e93def79575c835
William <wzhouwzhou@gmail.com>
2017-11-14 18:12:05 -0500

    Update -math

--- commands/utility/-math.cpscmd/-math.js
+++ commands/utility/-math.cpscmd/-math.js
@@ -1,34 +1,39 @@
-require('nodecpp-test')
-const splitter = (/(\s|,)+/)
+const math = require('nodecpp-test');
+const splitter = (/(\s|,)+/);
 
 module.exports = {
   name: '-math',
-  async func(msg, { suffix, args, send, math }) {
+  func(msg, { suffix, args, send }) {
+    if (!suffix.substring(suffix.indexOf(args[0])).match(/^\d*(,|\.)?\d+$/)) {
+      return send('Numbers/Comma\'s?');
+    }
 
-    if (!suffix.substring(suffix.indexOf(args[0])).match(/^\d*(\,|\.)?\d+$/))
-      return send('Numbers/Comma\'s?')
-    
-    if(args[0].match(/add|plus/)) 
-      suffix.split(splitter)
-      return send(`${math.add(splitter)}`) 
-    
-    if(args[0]==='round') 
+    if (args[0].match(/add|plus/)) {
+      suffix.split(splitter);
+      return send(`${math.add(splitter)}`);
+    }
+
+    if (args[0] === 'round') {
       let round = args[1];
-      return send(`${math.round(round)}`)
-    
-    if(args[0].match(/min|minimum|low|lowest/)) 
-      suffix.split(splitter)
-      return send(`${math.min(splitter)}`)
-    
-    if(args[0].match(/highest|high|maximum|max/)) 
-      suffix.split(splitter)
-      return send(`${math.max(splitter)}`) 
-    
-    if(args[0].match(/pi|mathpi/)) 
-      return send(`${math.PI}`)
+      return send(`${math.round(round)}`);
+    }
 
-    if(args[0].match(/sr|square|root|squareroot|sqrt|rt/)) 
+    if (args[0].match(/min|minimum|low|lowest/)) {
+      return send(`${math.min(suffix.split(splitter))}`);
+    }
+
+    if (args[0].match(/highest|high|maximum|max/)) {
+      suffix.split(splitter);
+      return send(`${math.max(splitter)}`);
+    }
+
+    if (args[0].match(/pi|mathpi/)) return send(`${math.PI}`);
+
+    if (args[0].match(/sr|square|root|squareroot|sqrt|rt/)) {
       let root = args[1];
-      return send(`${math.sqrt(root)}`) 
-  }  
-}
+      return send(`${math.sqrt(root)}`);
+    }
+
+    return send('Invalid operation');
+  },
+};
