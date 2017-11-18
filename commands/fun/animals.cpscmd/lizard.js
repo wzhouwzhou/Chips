f26fef7d351f7d208c5ba9df716de512b630a29e
Ethan <elawrence152@gmail.com>
2017-11-18 17:28:33 -0500

    Indentation fix and retardation fix

--- commands/fun/animals.cpscmd/lizard.js
+++ commands/fun/animals.cpscmd/lizard.js
@@ -3,19 +3,17 @@ const snekfetch = require('snekfetch');
 module.exports = {
   name: 'lizard',
   async func(msg, { send }) {
-		snekfetch.get('https://nekos.life/api/lizard').then((res) => {
-		if (res.status !== 200) {
-			return send('An error has occurred!');
-		}
-
-		send('', {
-			embed: new  Discord.MessageEmbed()
-				.setColor(member.displayColor)
-				.setTitle('Random Lizards')
-				.setDescription('This message will be deleted in 3 minutes')
-				.setImage(res.body.url)
-				.setTimestamp();
-		}).then(msg.delete(180000));
-		});
+	snekfetch.get('https://nekos.life/api/lizard').then((res) => {
+	if (res.status !== 200) {
+		return send('An error has occurred!');
+	}
+	cosnt embed = new  Discord.MessageEmbed()
+			.setColor(member.displayColor)
+			.setTitle('Random Lizards')
+			.setDescription('This message will be deleted in 3 minutes')
+			.setImage(res.body.url)
+			.setTimestamp();
+	send('', { embed: embed}).then(msg.delete(180000));
+	});
 },
 };
