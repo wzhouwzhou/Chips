799231ec60142bbb4f264382250a5443c1e7b32c
William <wzhouwzhou@gmail.com>
2017-11-19 11:50:26 -0500

    Hide lizard API

--- commands/fun/animals.cpscmd/lizard.js
+++ commands/fun/animals.cpscmd/lizard.js
@@ -1,21 +1,24 @@
 const snekfetch = require('snekfetch');
 
 module.exports = {
-	name: 'lizard',
-	async func(msg, { send, author, Discord }) {
-		snekfetch.get('https://nekos.life/api/lizard').then((res) => {
-		if (res.status !== 200) {
-			return send('An error has occurred!');
-		}
-		const color = "#"+((1<<24)*Math.random()|0).toString(16);
-		const embed = new  Discord.MessageEmbed()
-			.setColor(color)
-			.setTitle('Random Lizards')
-			.setDescription('This message will be deleted in 3 minutes')
-			.setImage(res.body.url)
-			.setFooter(`Requested by ${author.tag}`)
-			.setTimestamp();
-		send('', { embed: embed}).then(message.delete({ timeout: 3*60*1000 }));
-		});
-	},
+  name: 'lizard',
+  async func(msg, { send, author, member, Discord }) {
+    const res = await snekfetch.get('https://nekos.life/api/lizard');
+    if (res.status !== 200) {
+      send('An error occurred while spying on lizards!');
+      throw new Error(`Status code ${res.status}`);
+    }
+    const color = member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`;
+    const embed = new Discord.MessageEmbed()
+        .setColor(color)
+        .setTitle('Random Lizards')
+        .setDescription('This message will be deleted in 3 minutes')
+        .attachFiles([
+          new Discord.MessageAttachment(res.body.url || 'https://nekos.life/static/lizard/0118.png', 'lizard.png'),
+        ])
+        .setImage('attachment://lizard.png')
+        .setFooter(`Requested by ${author.tag}`)
+        .setTimestamp();
+    return send(embed).then(m => m.delete({ timeout: 3 * 60 * 1000 }));
+  },
 };
