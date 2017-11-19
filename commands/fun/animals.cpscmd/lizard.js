54f42713046f344436961333934642e6cf7193e5
Ethan <elawrence152@gmail.com>
2017-11-18 20:15:21 -0500

    Update for clean stuff in lizard :)

--- commands/fun/animals.cpscmd/lizard.js
+++ commands/fun/animals.cpscmd/lizard.js
@@ -1,20 +1,20 @@
 const snekfetch = require('snekfetch');
 
 module.exports = {
-  name: 'lizard',
-  async func(msg, { send, member, Discord }) {
-	snekfetch.get('https://nekos.life/api/lizard').then((res) => {
-	if (res.status !== 200) {
-		return send('An error has occurred!');
-	}
-	const color = "#"+((1<<24)*Math.random()|0).toString(16);
-	const embed = new  Discord.MessageEmbed()
-			.setColor(color)
-			.setTitle('Random Lizards')
-			.setDescription('This message will be deleted in 3 minutes')
-			.setImage(res.body.url)
-			.setTimestamp();
-	send('', { embed: embed}).then(msg.delete(180000));
-	});
-},
+	name: 'lizard',
+	async func(msg, { send, Discord }) {
+		snekfetch.get('https://nekos.life/api/lizard').then((res) => {
+		if (res.status !== 200) {
+			return send('An error has occurred!');
+		}
+		const color = "#"+((1<<24)*Math.random()|0).toString(16);
+		const embed = new  Discord.MessageEmbed()
+				.setColor(color)
+				.setTitle('Random Lizards')
+				.setDescription('This message will be deleted in 3 minutes')
+				.setImage(res.body.url)
+				.setTimestamp();
+		send('', { embed: embed}).then(msg.delete(180000));
+		});
+	},
 };
