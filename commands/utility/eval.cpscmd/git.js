a29c9d218001667899a6cba6675dd85f91a59600
William <wzhouwzhou@gmail.com>
2017-11-24 23:14:28 -0500

    Update git settings

--- commands/utility/eval.cpscmd/git.js
+++ commands/utility/eval.cpscmd/git.js
@@ -1,22 +1,28 @@
-const exec = require('../../../rewrite-all/src/deps/functions/execF').default({ _, child_process: require('child_process') });
+const exec = require('../../../rewrite-all/src/deps/functions/execF')
+  .default({ _: require('lodash'), child_process: require('child_process') });
 
-const whitelist = [
-  Constants.users.WILLYZ,
-  Constants.users.PGSUPER,
-  Constants.users.EVILDEATHPRO,
-  Constants.users.LUCAS,
-  '302252773427249163',
-];
+let whitelist;
 
 module.exports = {
   name: 'git',
-  async func(msg, { send, author, args, member }) {
-    if (whitelist.indexOf(author.id) < 0) return console.log(`Prohibited access to git to user ${author.id}`);
+  async func(msg, { send, author, args, member, Discord, Constants }) {
+    if (!whitelist) {
+      whitelist = [
+        Constants.users.WILLYZ,
+        Constants.users.PGSUPER,
+        Constants.users.EVILDEATHPRO,
+        Constants.users.LUCAS,
+        '302252773427249163',
+      ];
+    }
+    if (whitelist.indexOf(author.id) < 0) return true;
+    // Console.log(`Prohibited access to git to user ${author.id}`);
     let result = null;
     if (args[0] === 'pull') {
       result = await exec('git pull');
-      await send(new Discord.MessageEmbed().setColor(member.displayColor).setDescription(result.substring(0, 1800)));
+      await send(new Discord.MessageEmbed().setColor(member ? member.displayColor : 0x1213EE).setDescription(result.substring(0, 1800)));
     }
-    if (result && !~result.toLowerCase().replace(/(\s|-)+/g, '').indexOf('uptodate')) process.exit(42);
+    if (result && !~result.toLowerCase().replace(/(\s|-)+/g, '').indexOf('uptodate')) return process.exit(42);
+    return true;
   },
 };
