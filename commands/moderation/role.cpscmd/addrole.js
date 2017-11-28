980380b29ba70af7495610c9f305b2a44e7aa349
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-28 16:57:36 +0100

    Fix || addrole.js, removerole.js and setnick.js

--- commands/moderation/role.cpscmd/addrole.js
+++ commands/moderation/role.cpscmd/addrole.js
@@ -1,21 +1,19 @@
 module.exports = {
-    name: 'addrole',
-    async func(msg, { send, guild, member, author, args, content }) {
-      
-    if(author.id == '205608598233939970') {
-      if(!args[0]) {
-        return send('Role? Mention?')
+  name: 'addrole',
+  async func(msg, { send, guild, member, author, args, content }) {
+    if (author.id === '205608598233939970') {
+      if (!args[0]) {
+        return send('Role? Mention?');
       }
-      if(!args[1]) {
-        return send('Role?')
+      if (!args[1]) {
+        return send('Role?');
       }
-      if(args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1]) {
-        let targetUser = msg.mentions.members.first() 
+      if (args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1]) {
+        let targetUser = msg.mentions.members.first();
         let targetRole = content.substring(content.indexOf(args[1]));
-        await targetUser.addRole(guild.roles.find('name', `${targetRole}`))
+        await targetUser.addRole(guild.roles.find('name', `${targetRole}`));
         return send(`Gucci.`);
-
       }
-    }     
+    }
   },
 };
--- commands/moderation/setnick.cpscmd/setnick.js
+++ commands/moderation/setnick.cpscmd/setnick.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'setnick',
-    async func(msg, { send, guild, member, author, args, content }) {
+  name: 'setnick',
+  async func(msg, { send, guild, member, author, args, content }) {
       
-    if(author.id == '205608598233939970') {
+    if(author.id === '205608598233939970') {
     
     if(!args[0]) {
         return send('Name? Mention?')
