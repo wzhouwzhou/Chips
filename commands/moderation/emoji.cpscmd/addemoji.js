c61fe608c1b30397021625e1a0a7cc53c35ed478
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-28 18:18:06 +0100

    Fix || "Removed unused parameters"

--- commands/moderation/emoji.cpscmd/addemoji.js
+++ commands/moderation/emoji.cpscmd/addemoji.js
@@ -2,7 +2,7 @@ const reg = /^(https?:\/\/[^.]+\.[^]+)$/;
 
 module.exports = {
   name: 'addemoji',
-  async func(msg, { send, args, member, guild }) {
+  async func(msg, { send, args, guild }) {
     if (guild.emojis.size >= 50) return send(`Maximum number of emojis reached (${guild.emojis.size})`);
 
     if (!args[0]) return send('No emoji name given :(');
--- commands/utility/botnick.cpscmd/botnick.js
+++ commands/utility/botnick.cpscmd/botnick.js
@@ -1,8 +1,6 @@
-const _ = require('lodash');
-
 module.exports = {
   name: 'botnick',
-  async func(msg, { send, guild, suffix, member, args }) {
+  async func(msg, { send, guild, suffix }) {
     if (!guild) return send('You need to be in a server to use this!');
     if (!suffix) return send('Nothing provided to set as my nick');
     if (!guild.me.hasPermission('CHANGE_NICKNAME')) return send('I do not have permission to change my own nickname!');
