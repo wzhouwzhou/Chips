6f5c65b0f48539135b9a437e9fa3566948d74cd3
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-28 18:08:32 +0100

    Update || 8ball.js

--- commands/fun/8ball.cpscmd/8ball.js
+++ commands/fun/8ball.cpscmd/8ball.js
@@ -4,14 +4,32 @@ const answer = [
   'Yes!',
   'Maybe.',
   '{shardusercount} people would agree.',
-  'Eat chips!',
+  'I think so.',
+  'I doubt it.',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes, definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Signs point to yes',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  "Don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Indeed',
 ];
 
 module.exports = {
   name: '8ball',
   func(msg, { send, args, suffix, Discord, member, client }) {
     if (!args[0]) return send('Do you have a question?');
-
     if (suffix.split('').reverse()[0] !== '?') return send('Question mark?');
 
     const embed = (new Discord.MessageEmbed)
