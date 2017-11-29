9009185adfbe4c9dc3148ef67c9232c2d22e4fb4
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-29 19:22:49 +0100

    Fix || re.js

--- commands/moderation/emoji.cpscmd/removeemoji.js
+++ commands/moderation/emoji.cpscmd/removeemoji.js
@@ -1,13 +1,15 @@
 module.exports = {
   name: 'removeemoji',
-  async func(msg, { send, args, member, guild }) {
+  async func(msg, { send, args, guild }) {
     if (!args[0]) return send('No emoji name given :(');
 
     let name = args[0];
 
+    let emote = guild.emojis.find('name', `${name}`);
+
     let emoji;
     try {
-      emoji = await guild.deleteEmoji(name);
+      emoji = await guild.deleteEmoji(emote);
       send(`Deleted emoji with name ${emoji.name}!`);
     } catch (err) {
       send('The emoji could not be deleted…');
