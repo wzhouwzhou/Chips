637ca79086e6e1ef823bf12db139a2fead51fe15
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-29 07:43:00 +0100

    Update || role.cpscmd

--- commands/moderation/role.cpscmd/removerole.js
+++ commands/moderation/role.cpscmd/removerole.js
@@ -2,13 +2,13 @@ module.exports = {
   name: 'removerole',
   async func(msg, { send, guild, member, author, args, content, Discord }) {
     if (author.id === '205608598233939970') {
-      if (!args[0]) {
+      if (guild) {
+        return send('You cannot use this command in Direct Messages.');
+      } if (!args[0]) {
         return send('Role? Mention?');
-      }
-      if (!args[1]) {
+      } if (!args[1]) {
         return send('Role?');
-      }
-      if (args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1]) {
+      } if (args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1]) {
         let targetUser = msg.mentions.members.first();
         let targetRole = content.substring(content.indexOf(args[1]));
         let targetRoleSend = guild.roles.find('name', `${targetRole}`);
