c489d8d7ca4db573876737897a44edef6aae6320
William Zhou <wzhouwzhou@gmail.com>
2017-11-29 15:41:09 -0500

    Fix spooky

--- commands/fun/text.cpscmd/spooky.js
+++ commands/fun/text.cpscmd/spooky.js
@@ -1,21 +1,21 @@
 module.exports = {
   name: 'spooky',
-  async func(msg, { author, member, guild, content, prefix, Discord, reply }) {
+  func(msg, { author, member, guild, content, prefix, Discord, send }) {
     let truecontent = content.substring(`${prefix}spooky `.length);
     let spookymsg = truecontent;
     let toMatch = /(\s*?(?:num(?:ber)?):?\s*((?:sp(?:ace)?)((?:s)?)?\s*:?(\s*))?)/i.exec(truecontent);
     let numSpaces = 1;
     if (toMatch) {
       let numMatcher = toMatch[0];
-      console.log(`[SPOOKY] numMatcher: ${numMatcher}`);
+      // Console.log(`[SPOOKY] numMatcher: ${numMatcher}`);
       let numquery = truecontent.substring(truecontent.indexOf(numMatcher) + numMatcher.length);
-      console.log(`[SPOOKY] numquery: ${numquery}`);
+      // Console.log(`[SPOOKY] numquery: ${numquery}`);
       numSpaces = numquery.split(/\s+/)[0];
       numSpaces = isNaN(numSpaces) ? 1 : parseInt(numSpaces);
-      console.log(`[SPOOKY] numspaces: ${numSpaces}`);
+      // Console.log(`[SPOOKY] numspaces: ${numSpaces}`);
       spookymsg = truecontent.replace(numMatcher && numSpaces ? numMatcher + numSpaces : '', '');
       spookymsg = spookymsg.length < 1 ? ' ' : spookymsg;
-      console.log(`[SPOOKY] stuff to convert: ${spookymsg}`);
+      // Console.log(`[SPOOKY] stuff to convert: ${spookymsg}`);
     }
     // Else console.log('[SPOOKY] No repeat number specified..using 1');
     let s = '';
@@ -25,6 +25,6 @@ module.exports = {
     bad.setColor(guild ? member.displayColor : 15152469);
     bad.setAuthor(author.tag, author.displayAvatarURL({ format: 'png', size: 2048 }));
     bad.setDescription(converted);
-    return reply({ embed: bad });
+    return send({ embed: bad });
   },
 };
