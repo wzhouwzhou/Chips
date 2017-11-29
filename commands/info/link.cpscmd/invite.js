848bcd7e2fc7a48cb79981fa9aa1de912f697eed
lucaslsg <lucaslucaso2002@gmail.com>
2017-11-29 15:49:25 +0100

    Update || invite.js

--- commands/info/link.cpscmd/invite.js
+++ commands/info/link.cpscmd/invite.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'invite',
   async func(msg, { send }) {
-    send(`My invite link: https://discordapp.com/oauth2/authorize?client_id=296855425255473154&scope=bot&permissions=2146958591`);
+    send(`My invite link: https://invite.chipsbot.me/`);
   },
 };
