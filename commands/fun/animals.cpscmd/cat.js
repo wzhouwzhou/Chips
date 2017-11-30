fe8d12e31e26eeec170d60efc06a00a849ca8f19
William Zhou <wzhouwzhou@gmail.com>
2017-11-30 12:10:45 -0500

    Check response status

--- commands/fun/animals.cpscmd/cat.js
+++ commands/fun/animals.cpscmd/cat.js
@@ -2,11 +2,13 @@ const snekfetch = require('snekfetch');
 
 const getCat = () => new Promise((resolve, rej) => snekfetch.get('http://www.random.cat/meow').then(async res => {
   try {
+    if (res.status !== 200) return rej(new Error(`Status ${res.status}`));
+
     let f = res.body;
     if (!f || !f.file) return rej(new Error('File not found'));
 
     if (!~f.file.indexOf('png')) f = await getCat();
-    return resolve(f.file);
+    return resolve(f);
   } catch (err) {
     return rej(err);
   }
@@ -17,7 +19,7 @@ module.exports = {
   async func(msg, { send, channel, Discord }) {
     channel.startTyping();
     try {
-      await send(new Discord.MessageAttachment(await getCat(), 'cat.png'));
+      await send(new Discord.MessageAttachment((await getCat()).file, 'cat.png'));
     } catch (err) {
       channel.stopTyping();
       await send('The cat photographer went missing!');
