7bbc2729313a9813a0295d617d9ca2a53cf23c9c
William Zhou <wzhouwzhou@gmail.com>
2017-11-30 12:07:32 -0500

    Fix extra JSON parsing

--- commands/fun/animals.cpscmd/cat.js
+++ commands/fun/animals.cpscmd/cat.js
@@ -2,11 +2,13 @@ const snekfetch = require('snekfetch');
 
 const getCat = () => new Promise((resolve, rej) => snekfetch.get('http://www.random.cat/meow').then(async res => {
   try {
-    let f = JSON.parse(res.body).file;
-    if (!~f.indexOf('png')) f = await getCat();
-    if (f) resolve(f); else rej(new Error('File not found'));
+    let f = res.body;
+    if (!f || !f.file) return rej(new Error('File not found'));
+
+    if (!~f.file.indexOf('png')) f = await getCat();
+    return resolve(f.file);
   } catch (err) {
-    rej(err);
+    return rej(err);
   }
 }).catch(rej));
 
