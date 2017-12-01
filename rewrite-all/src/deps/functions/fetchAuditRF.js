c4bb3c0c6f1c63cfd79df72848913d90e7b3343f
William <wzhouwzhou@gmail.com>
2017-11-30 22:51:42 -0500

    FetchAudit recursively

---
+++ rewrite-all/src/deps/functions/fetchAuditRF.js
@@ -0,0 +1,34 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const Logger = require('../../struct/client/Logger').create('exports', 'fetchMessagesRF');
+
+exports.default = ({ Discord, _ }) => {
+  const firstF = require('./firstF').default({ _ });
+  const { Collection } = Discord;
+  const fetchAuditR = (guild, options = { limit: 100 }, collection = new Collection()) => {
+    const limit = options.limit;
+
+    return new Promise((res, rej) => {
+      if (typeof limit !== 'number') return rej(new Error('Invalid limit'));
+      if (collection.size >= limit) return res(firstF(collection, limit));
+      return guild.fetchAuditLogs(Object.assign({}, options,
+        { limit: Math.min(options.limit, 100) })
+      ).then(async logs => {
+        if (logs.entries.size === 0) return res(collection);
+        Array.from(logs.entries.values()).forEach(e => collection.set(e.id, e));
+        try {
+          return res(await fetchAuditR(guild, {
+            limit,
+            before: Array.from(logs.entries.values())
+              .sort((a, b) => a.createdAt - b.createdAt)[0].id,
+          }, collection));
+        } catch (err) {
+          Logger.error(err);
+          return rej(err);
+        }
+      }).catch(rej);
+    });
+  };
+  return fetchAuditR;
+};
