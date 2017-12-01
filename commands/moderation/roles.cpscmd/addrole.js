30437aa6d84e3e6a96d21759041b281a102ab1a5
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-01 17:18:13 +0100

    Beta || Addrole.js

--- commands/moderation/roles.cpscmd/addrole.js
+++ commands/moderation/roles.cpscmd/addrole.js
@@ -4,7 +4,7 @@ module.exports = {
     if (author.id === '205608598233939970') {
       if (!guild) {
         return send('You cannot use this command in Direct Messages.');
-      } if ((!args[0].match(/^[^]*<@!?(\d+)>[^]*$/)) && (args[1].match(/^[^]*<@!?(\d+)>[^]*$/))) {
+      } if (!args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1].match(/^[^]*<@!?(\d+)>[^]*$/)) {
         return send('Mention?');
       } if (!args[1]) {
         return send('Role?');
@@ -12,6 +12,9 @@ module.exports = {
         let targetUser = msg.mentions.members.first();
         let targetRole = content.substring(content.indexOf(args[1]));
         let targetRoleSend = guild.roles.find('name', `${targetRole}`);
+        if (targetUser.highestRole.position < targetRoleSend.position && !author.id === '205608598233939970') {
+          return send('You don\'t have enough permissions!');
+        }
         await targetUser.addRole(guild.roles.find('name', `${targetRole}`));
         return send(new Discord.MessageEmbed().setColor(member.displayColor).setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
       }
