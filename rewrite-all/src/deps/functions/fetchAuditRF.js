5dc80ffc85e1b8a02a68d4dacb1b7268ce0dc77a
William <wzhouwzhou@gmail.com>
2017-11-30 22:59:55 -0500

    Filter type properly

--- rewrite-all/src/deps/functions/fetchAuditRF.js
+++ rewrite-all/src/deps/functions/fetchAuditRF.js
@@ -8,12 +8,12 @@ exports.default = ({ Discord, _ }) => {
   const { Collection } = Discord;
   const fetchAuditR = (guild, options = { limit: 100 }, collection = new Collection()) => {
     const limit = options.limit;
-
+    const type = options.type;
     return new Promise((res, rej) => {
       if (typeof limit !== 'number') return rej(new Error('Invalid limit'));
       if (collection.size >= limit) return res(firstF(collection, limit));
       return guild.fetchAuditLogs(Object.assign({}, options,
-        { limit: Math.min(options.limit, 100) })
+        { limit: Math.min(options.limit, 100), type })
       ).then(async logs => {
         if (logs.entries.size === 0) return res(collection);
         Array.from(logs.entries.values()).forEach(e => collection.set(e.id, e));
@@ -22,6 +22,7 @@ exports.default = ({ Discord, _ }) => {
             limit,
             before: Array.from(logs.entries.values())
               .sort((a, b) => a.createdAt - b.createdAt)[0].id,
+            type,
           }, collection));
         } catch (err) {
           Logger.error(err);
