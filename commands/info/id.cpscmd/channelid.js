18aaa05da0f5707e0efdfcb4fa94a18f7e940a57
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-01 07:40:29 +0100

    Update || channelid.js

--- commands/info/id.cpscmd/channelid.js
+++ commands/info/id.cpscmd/channelid.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'channelid',
   func(msg, { send, channel, guild }) {
     if (guild) {
-      return send(channel.id);
+      return send(`${channel + []}`);
     } else {
       return send('You must be in a server to use this!');
     }
