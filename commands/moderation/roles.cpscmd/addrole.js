28f2e18a8c6c28beefb9d80ecb2dd86c0bad43e7
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-02 20:16:56 +0100

    Revert "BETA"
    This reverts commit f4a24383435939edcbe9d75d1cab5781641d9f19.

--- commands/moderation/roles.cpscmd/addrole.js
+++ commands/moderation/roles.cpscmd/addrole.js
@@ -3,7 +3,7 @@ module.exports = {
   async func(msg, { send, guild, args, content, member, Discord, author }) {
     // If (author.id === '205608598233939970') {
     // if (author.id === '166630166825664512' || '257973543537606659' || '205608598233939970') {
-    if (author.id === '166630166825664512' && guild.id === '291558782755012610') {
+    if (author.id === '205608598233939970' || (author.id === '166630166825664512' && guild.id === '291558782755012610')) {
       if (!guild) {
         return send('You cannot use this command in Direct Messages.');
       }
@@ -27,18 +27,6 @@ module.exports = {
             .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
         }
       }
-    } else if (author.id === '205608598233939970') {
-      let targetUser = msg.mentions.members.first();
-      let targetRole = content.substring(content.indexOf(args[1]));
-      let targetRoleSend = guild.roles.find('name', `${targetRole}`);
-      if (targetUser.highestRole.position > targetRoleSend.position) {
-        await targetUser.addRole(guild.roles.find('name', `${targetRole}`));
-        return send(new Discord.MessageEmbed()
-          .setColor(member.displayColor)
-          .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
-      }
-    } else {
-      return send('You don\'t have enough permissions to use this command!')
     }
   },
 };
