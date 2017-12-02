a28dc92891114a1b4b50d1362cce2bcaae65ebce
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-02 20:07:43 +0100

    BETA

--- commands/moderation/roles.cpscmd/addrole.js
+++ commands/moderation/roles.cpscmd/addrole.js
@@ -31,14 +31,12 @@ module.exports = {
       let targetUser = msg.mentions.members.first();
       let targetRole = content.substring(content.indexOf(args[1]));
       let targetRoleSend = guild.roles.find('name', `${targetRole}`);
-      if (targetUser.highestRole.position > targetRoleSend.position) {
-        await targetUser.addRole(guild.roles.find('name', `${targetRole}`));
-        return send(new Discord.MessageEmbed()
-          .setColor(member.displayColor)
-          .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
-      }
+      await targetUser.addRole(guild.roles.find('name', `${targetRole}`));
+      return send(new Discord.MessageEmbed()
+        .setColor(member.displayColor)
+        .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
     } else {
-      return send('You don\'t have enough permissions to use this command!')
+      return send('You don\'t have enough permissions to use this command!');
     }
   },
 };
