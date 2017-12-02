df6b69c661ba59c37aafab0609ae4c9691192922
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-02 10:30:16 +0100

    Fix || membercount.js

--- commands/info/membercount.cpscmd/membercount.js
+++ commands/info/membercount.cpscmd/membercount.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'membercount',
-  async func(msg, { send, guild, role }) {
+  async func(msg, { send, guild }) {
     if (guild) {
-      let trueMemC = role.members.filter(member => !member.user.bot);
+      let trueMemC = guild.members.filter(member => !member.user.bot);
       return send(`**${guild.members.size - trueMemC.size === 1 ? 'bot' : 'bots'}** members are in **${guild.name}**!`);
     } else {
       return send('You must be in a server to use this!');
