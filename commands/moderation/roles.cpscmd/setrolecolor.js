00f0799de8cd0fa2bb3048521d5467730bb00964
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-03 11:11:08 +0100

    Fix || src.js

--- commands/moderation/roles.cpscmd/setrolecolor.js
+++ commands/moderation/roles.cpscmd/setrolecolor.js
@@ -2,22 +2,25 @@ const reg = /#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/;
 
 module.exports = {
   name: 'setrolecolor',
-  async func(msg, { send, guild, args, author }) {
+  async func(msg, { send, guild, args, author, content }) {
     if (!guild) {
       return send('You need to use this command in a guild!');
     }
 
-    if (!args[0]) {
-      return send('Role? Hex color?');
-    }
+    // Args[0] === hex color
+    // Content == Role
 
-    if (args[0] && !args[1].match(reg)) {
+    if (!args[0].match(reg)) {
       return send('Hex color?');
     }
 
-    if (args[0] && args[1].match(reg)) {
-      let role = guild.roles.find('name', `${args[0]}`);
-      let hexcolor = args[1];
+    if (args[0].match(reg) && !content.substring(content.indexOf(args[1]))) {
+      return send('Role?');
+    }
+
+    if (args[0].match(reg) && content.substring(content.indexOf(args[1]))) {
+      let role = guild.roles.find('name', `${content.substring(content.indexOf(args[1]))}`);
+      let hexcolor = args[0];
       if (role && (author.highestRole.position > role.position)) {
         await role.setColor(`${hexcolor}`);
         return send(`Role color set to ${hexcolor}!`);
