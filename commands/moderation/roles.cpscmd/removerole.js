9ef346f5101633664b1a820fe7cdee9f2717ff49
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-04 18:15:42 +0100

    BETA Fix || rr.js

--- commands/moderation/roles.cpscmd/removerole.js
+++ commands/moderation/roles.cpscmd/removerole.js
@@ -3,7 +3,7 @@ module.exports = {
   async func(msg, { send, guild, args, content, member, Discord, author }) {
     // If (author.id === '205608598233939970') {
     // if (author.id === '166630166825664512' || '257973543537606659' || '205608598233939970') {
-    if (author.id === '205608598233939970' || (author.id === '166630166825664512' && guild.id === '291558782755012610')) {
+    if (author.id === '166630166825664512' && guild.id === '291558782755012610') {
       if (!guild) {
         return send('You cannot use this command in Direct Messages.');
       }
@@ -24,9 +24,21 @@ module.exports = {
           await targetUser.removeRole(guild.roles.find('name', `${targetRole}`));
           return send(new Discord.MessageEmbed()
             .setColor(member.displayColor)
-            .setDescription(`**Succesfully removed** <@&${targetRoleSend.id}> || **${targetRole}** **from** <@${targetUser.id}> || ${targetUser.username}`));
+            .setDescription(`**Succesfully removed** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
         }
       }
+    } else if (author.id === '205608598233939970') {
+      let targetUser = msg.mentions.members.first();
+      let targetRole = content.substring(content.indexOf(args[1]));
+      let targetRoleSend = guild.roles.find('name', `${targetRole}`);
+      if (targetUser.highestRole.position > targetRoleSend.position) {
+        await targetUser.removeRole(guild.roles.find('name', `${targetRole}`));
+        return send(new Discord.MessageEmbed()
+          .setColor(member.displayColor)
+          .setDescription(`**Succesfully removed** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
+      }
+    } else {
+      return send('You don\'t have enough permissions to use this command!')
     }
   },
 };
