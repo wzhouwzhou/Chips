5bea2df00db48816739ec5c03b93e731ed4571ff
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-04 11:07:25 +0100

    Update || emojis.js

--- commands/info/emojis.cpscmd/emojis.js
+++ commands/info/emojis.cpscmd/emojis.js
@@ -1,13 +1,15 @@
 module.exports = {
   name: 'emojis',
   async func(msg, { send, guild, member, Discord }) {
-    if (guild) {
+    if (guild && (guild.emojis.size >= '1')) {
       return send((new Discord.MessageEmbed)
         .setDescription(guild.emojis.array().map(e => e + []).join(' '))
         .setTitle(`${guild.emojis.size} emojis.`)
         .setColor(member.displayColor));
+    } else if (guild.emojis.size < '1') {
+      return send('The server doesn\'t have any emojis!');
     } else {
-      return 'You need to be in a server to use this command!';
+      return send('You need to be in a server to use this!');
     }
   },
 };
