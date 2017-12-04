df3e5467a6a10a321d00fa4df2386ea38bdc91bb
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-04 17:38:56 +0100

    Fix || emojis.js

--- commands/info/emojis.cpscmd/emojis.js
+++ commands/info/emojis.cpscmd/emojis.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'emojis',
   async func(msg, { send, guild, member, Discord }) {
-    if (guild && (guild.emojis.size >= '1')) {
+    if (guild && guild.emojis.size >= '1') {
       return send((new Discord.MessageEmbed)
         .setDescription(guild.emojis.array().map(e => e + []).join(' '))
         .setTitle(`${guild.emojis.size} emojis.`)
