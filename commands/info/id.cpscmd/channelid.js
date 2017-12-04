2103b7f86439bc1d85587f11d0501c394cb834c9
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-04 10:58:22 +0100

    Fix || channelid.js

--- commands/info/id.cpscmd/channelid.js
+++ commands/info/id.cpscmd/channelid.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'channelid',
   func(msg, { send, channel, guild }) {
     if (guild) {
-      return send(`${channel + []}`);
+      return send(`${channel.id}`);
     } else {
       return send('You must be in a server to use this!');
     }
