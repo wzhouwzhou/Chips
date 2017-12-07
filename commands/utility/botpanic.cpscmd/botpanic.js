f56d4fc8f2c66f66ae459e62d55d67d126949862
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-07 10:02:21 +0100

    Update || botnick.js

--- commands/utility/botpanic.cpscmd/botpanic.js
+++ commands/utility/botpanic.cpscmd/botpanic.js
@@ -13,7 +13,7 @@ module.exports = {
     client.database._sheets.botlog.addRow({ time: `${moment().format('ddd, Do of MMM @ HH:mm:ss')}`, action: 'PANIC', mainvalue: 'SIGTERM' }, err => { console.log(`Error : ${err}`); });
     // Await client.user.setStatus("idle");
     // await client.user.setGame("Restarting");
-    await reply(new Discord.MessageEmbed().setDescription('Bot restarting!').setColor(member.displayColor))
+    await reply(new Discord.MessageEmbed().setDescription('Bot restarting!').setColor((member ? member.displayColor : 0x1213EE));
       .then(() => process.exit(-100));
     // Await client.user.setStatus("invisible");
   },
