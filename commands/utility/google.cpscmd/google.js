d35d748f62695966aa277bfab02c338e6b4f11fd
Ethan <elawrence152@gmail.com>
2017-12-06 23:51:10 -0500

    temp google

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -32,9 +32,6 @@ module.exports = {
             .addField('Search results for  `' + `${args.join(' ')}` + '`', ebrackets.map(r => r.link + '\n\t' + r.description + '\n').join('\n'))
             .setFooter(`Requested by ${author.tag}`);
           send(embed);
-        } else { 
-          send(`Error! (${res.statusCode}): ${res.statusMessage}`); 
-          console.log(`Error! (${res.statusCode}): ${res.statusMessage}`);
         }
       }
     }
