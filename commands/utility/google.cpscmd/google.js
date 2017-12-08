e77b7767a80296f58bd4f4e9d379563451a6b9fd
Ethan <elawrence152@gmail.com>
2017-12-08 15:18:53 -0500

    indentation and formatting

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -10,14 +10,13 @@ module.exports = {
       if (err) console.error(err);
       for (const i = 0; i < res.links.length; ++i) {
         const link = res.links[i];
-        send('', {
-          embed: new MessageEmbed()
-            .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
-            .setTitle(`Search Result for: ${args.join(" ")}`)
-            .addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
-            .setFooter(`Requested by: ${author.tag}`)
-        });
-			}
-		})
-	}
+	const embed = new MessageEmbed()
+          .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
+          .setTitle(`Search Result for: ${args.join(" ")}`)
+          .addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
+          .setFooter(`Requested by: ${author.tag}`);
+        send(embed);
+      }
+    })
+  }
 };
