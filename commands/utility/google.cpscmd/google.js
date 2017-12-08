6bc93740a272a132599968a9fcc031eefd007b8f
Ethan <elawrence152@gmail.com>
2017-12-08 15:11:15 -0500

    Update google.js

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -1,46 +1,24 @@
-const request = require('request');
-const honeynut = require('cheerio');
+const google = require('google');
 
 module.exports = {
   name: 'google',
-  async func(msg, { send, args, author, Discord, member }) {
-    if (!args[0]) return send('You must enter something to search for!');
-    request.get('http://google.com/search?client=ubuntu&rls=en&ie=UTF-8&oe=UTF-8&q=' + args.join('+'), (err, res, body) => {
-      if (!err && res.statusCode === 200) {
-        let $$ = honeynut.load(body);
-        let brackets = [];
-        $$('.g').each((i) => {
-          brackets[i] = {};
-        });
-        $$('.g>.r>a').each((i, e) => {
-          let raw = e.attribs['href'];
-          brackets[i]['link'] = raw.substr(7, raw.indexOf('&sa=U') - 7);
-        });
-        $$('.g>.s>.st').each((i, e) => {
-          brackets[i]['description'] = getSearchResults(e);
-        });
-
-        let ebrackets = brackets.filter(r => r.link && r.description);
-        ebrackets = ebrackets.splice(0, 3);
-                
-        if(ebrackets=="") return send("Oh no! You input an invalid search request!");  
-        else { 
-          let embed = new Discord.MessageEmbed()
-            .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
-            .setTitle(``)
-            .setDescription(``)
-            .addField('Search results for  `' + `${args.join(' ')}` + '`', ebrackets.map(r => r.link + '\n\t' + r.description + '\n').join('\n'))
-            .setFooter(`Requested by ${author.tag}`);
-          send(embed);
-        }
-      }
-    }
-  );}
+  async func(msg, { send, author, Discord, member }) {
+		google.resultsPerPage = 1;
+		google.protocol = 'http';
+		var nextCounter = 0;
+		const args = message.content.split(/\s+/).slice(1);
+		google(args, function (err, res){
+			if (err) console.error(err);
+			for (var i = 0; i < res.links.length; ++i) {
+				var link = res.links[i];
+				message.channel.send('', {
+					embed: new RichEmbed()
+						.setColor(config.discord.defaultColor)
+						.setTitle(`Search Result for: ${args.join(" ")}`)
+						.addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
+						.setFooter(`Requested by: ${message.author.tag}`)
+				});
+			}
+		})
+	}
 };
-
-function getSearchResults(children) {
-  if (children.children) return getSearchResults(children.children);
-  return children.map(c => {
-    return c.children ? getSearchResults(c.children) : c.data;
-  }).join('');
-}
