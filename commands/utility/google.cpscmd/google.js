d30cf1de9ba38f85611e124f7ad6b03550510b36
Ethan <elawrence152@gmail.com>
2017-12-08 15:13:48 -0500

    Note: This is not my selfbot. Google Fix

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -2,22 +2,21 @@ const google = require('google');
 
 module.exports = {
   name: 'google',
-  async func(msg, { send, author, Discord, member }) {
-		google.resultsPerPage = 1;
-		google.protocol = 'http';
-		var nextCounter = 0;
-		const args = message.content.split(/\s+/).slice(1);
-		google(args, function (err, res){
-			if (err) console.error(err);
-			for (var i = 0; i < res.links.length; ++i) {
-				var link = res.links[i];
-				message.channel.send('', {
-					embed: new RichEmbed()
-						.setColor(config.discord.defaultColor)
-						.setTitle(`Search Result for: ${args.join(" ")}`)
-						.addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
-						.setFooter(`Requested by: ${message.author.tag}`)
-				});
+  async func(msg, { send, args, author, Discord, member }) {
+    google.resultsPerPage = 1;
+    google.protocol = 'http';
+    const nextCounter = 0;
+    google(args, function (err, res){
+      if (err) console.error(err);
+      for (const i = 0; i < res.links.length; ++i) {
+        const link = res.links[i];
+        send('', {
+          embed: new MessageEmbed()
+            .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
+            .setTitle(`Search Result for: ${args.join(" ")}`)
+            .addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
+            .setFooter(`Requested by: ${author.tag}`)
+        });
 			}
 		})
 	}
