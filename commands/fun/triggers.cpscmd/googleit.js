2282c188fc53c54ce0986366b025fc87ed4f7e71
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-08 19:16:44 +0100

    Fix || googleit.js

--- commands/fun/triggers.cpscmd/googleit.js
+++ commands/fun/triggers.cpscmd/googleit.js
@@ -1,6 +1,6 @@
 const a = require('nodecpp-test').arrays;
 const urls = [
-  'https://giphy.com/gifs/lastweektonight-hbo-google-3oKIPmM4hkQIf4p5T2',
+  'https://i.imgur.com/X20kba7.gif',
   'https://giphy.com/gifs/lastweektonight-hbo-google-3oKIPmM4hkQIf4p5T2',
 ];
 
