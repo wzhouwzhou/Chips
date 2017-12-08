21dc3bae6d41b48b2fd901bebf38b93e89d2bfc2
Ethan <elawrence152@gmail.com>
2017-12-08 16:50:37 -0500

    indent

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -10,13 +10,13 @@ module.exports = {
       if (err) console.error(err);
       for (let i = 0; i < res.links.length; ++i) {
         const link = res.links[i];
-	const embed = new MessageEmbed()
+        const embed = new MessageEmbed()
           .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
           .setTitle(`Search Result for: ${args.join(" ")}`)
           .addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
           .setFooter(`Requested by: ${author.tag}`);
         send(embed);
       }
-    })
+    });
   }
 };
