3f45b88aa52cc57984650e09588019302036b5f5
William <wzhouwzhou@gmail.com>
2017-12-10 18:05:28 -0500

    Fix google attempt 1

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -3,24 +3,21 @@ const google = require('google');
 module.exports = {
   name: 'google',
   func(msg, { send, args, author, Discord, member }) {
-    google.resultsPerPage = 1;
-    google.protocol = 'http';
-    let nextCounter = 0;
+    google.resultsPerPage = 5;
+    google.protocol = 'https';
+
     google(args, (err, res) => {
-      if (err) send(err);
-      for (let i = 0; i < res.links.length; ++i) {
-        const link = res.links[i];
-        const embed = new Discord.MessageEmbed()
-          .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
-          .setTitle(`Search Result for: ${args.join(' ')}`)
-          .addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
-          .setFooter(`Requested by: ${author.tag}`);
-        send(embed);
+      if (err) return send(err);
+
+      const embed = new Discord.MessageEmbed()
+        .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
+        .setTitle(`Search Result for: ${args.join(' ')}`)
+        .setFooter(`Requested by: ${author.tag}`);
+
+      for (const link of res.links) {
+        embed.addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`);
       }
-      if (nextCounter < 4) {
-        nextCounter += 1;
-        if (res.next) res.next();
-      }
+      return send(embed);
     });
   },
 };
