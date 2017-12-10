5f106025e5fb46aba9ff308ee5bb48b5b75379d8
Ethan <elawrence152@gmail.com>
2017-12-10 15:11:20 -0500

    Update google.js

--- commands/utility/google.cpscmd/google.js
+++ commands/utility/google.cpscmd/google.js
@@ -2,21 +2,21 @@ const google = require('google');
 
 module.exports = {
   name: 'google',
-  async func(msg, { send, args, author, Discord, member }) {
+  func(msg, { send, args, author, Discord, member }) {
     google.resultsPerPage = 1;
     google.protocol = 'http';
     const nextCounter = 0;
-    google(args, function (err, res){
-      if (err) console.error(err);
+    google(args, (err, res) => {
+      if (err) console.log(err);
       for (let i = 0; i < res.links.length; ++i) {
         const link = res.links[i];
-        const embed = new MessageEmbed()
+        const embed = new Discord.MessageEmbed()
           .setColor(member ? member.displayColor : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
-          .setTitle(`Search Result for: ${args.join(" ")}`)
+          .setTitle(`Search Result for: ${args.join(' ')}`)
           .addField(`**Title:** ${link.title}`, `**Link Description** ${link.description}\n**Link:** ${link.link}`)
           .setFooter(`Requested by: ${author.tag}`);
         send(embed);
       }
     });
-  }
+  },
 };
