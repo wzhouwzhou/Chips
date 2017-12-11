962d1188d31bd2df3ffbce7d8369bfca40bb110b
Ethan <elawrence152@gmail.com>
2017-12-10 22:52:16 -0500

    Oops

--- commands/fun/triggers.cpscmd/confoosed.js
+++ commands/fun/triggers.cpscmd/confoosed.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'confoosed',
-  async func(msg, { send }) {
-    send(`*Conf${'o'.repeat(_.random(2, 15 - 4))}sed!*`);
+  func(msg, { send }) {
+    return send(`*Conf${'o'.repeat(_.random(2, 15 - 4))}sed!*`);
   },
 };
