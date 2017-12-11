344e35a7a50be9c48005153c4a4da023c2d2b288
Ethan <elawrence152@gmail.com>
2017-12-11 00:17:51 -0500

    uh fix tting

--- commands/fun/-ban.cpscmd/-ban.js
+++ commands/fun/-ban.cpscmd/-ban.js
@@ -46,6 +46,54 @@ const neko = [
   // Devon, for making chips logos
 ];
 
+const ex = {/* eslint no-console: 'off', consistent-return: 'off' */
+const neko = [
+  '265015624252653568',
+  // Arkhalis
+  '240651964424126464',
+  // Loaf
+  '259209114268336129',
+  // Willyz, that one owner guy
+  '250815960250974209',
+  // Evildeathpro, that one other owner guy
+  '242366901194457088',
+  // Wowie
+  '270834390643376129',
+  // Harbinger
+  '213312750703607808',
+  // Zalgo
+  '220182743126900737',
+  // Garhu5
+  '278734113735966720',
+  // Pie
+  '223811504833691648',
+  // Nolife
+  '208736038577897473',
+  // Xanthi
+  '279270217417228288',
+  // Abhinav
+  '220951507070222337',
+  // Gotem
+  '259287315044630528',
+  // Apex
+  '90647484623261696',
+  // Hero
+  '237270037528969218',
+  // Arx
+  '292971521159200768',
+  // JTJosh, not a neko but wanted immunity lol
+  '279959411693322241',
+  // TheGamingBolt, not a neko but also wanted immunity for whatever reason xd
+  '365972456139390977',
+  // Vee
+  '260024920757633025',
+  // Xena, a nEkO SLaVE
+  '205608598233939970',
+  // Lucas
+  '262127229385965570',
+  // Devon, for making chips logos
+];
+
 const ex = {
   name: '-ban',
   async func(msg, { send, author, content, channel, guild, args, gMember, Discord, reply, bot }) {
@@ -53,12 +101,12 @@ const ex = {
     // Const used = member || author;
     if (!guild) return send('You must use this command in a server.');
     if (!args[0]) return send('No user given :(');
-    const target = args[0].match(Constants.patterns.MENTION)[1];
+    const target = args[0].match(/^[^]*<@!?(\d+)>[^]*$/)[1];
     const split = content.replace(/\s+/g, ' ').trim().split(' ');
     let reason = split.slice(2, split.length).join(' ');
-    if (reason == '') reason = 'None';
+    if (reason === '') reason = 'None';
     const user = gMember(target).user;
-    if (user.id == bot.user.id) return send(`NO!!`);
+    if (user.id === bot.user.id) return send(`NO!!`);
     /* If(!used.hasPermission("BAN_MEMBERS")){
       switch (used.id) {
         case Constants.users.WILLYZ:
@@ -90,7 +138,7 @@ const ex = {
     let emb = new Discord.MessageEmbed()
       .setAuthor('Ban Notice!')
       .setColor(9109504)
-      .setThumbnail(Constants.images.WARNING)
+      .setThumbnail('https://i.imgur.com/S789uIe.png')
       .addField('Ban reason: ', `${reason}`, true);
     try {
       await user.send(' ', { embed: emb.setTitle(`You were banned from the server: ${guild.name}!`) });
@@ -113,8 +161,9 @@ const ex = {
     const mee6name = guild.members.get('159985870458322944') ? guild.members.get('159985870458322944')
       .displayName : null;
     if (mee6name) {
-      channel.createWebhook(mee6name, { avatar: Constants.avatars.MEE6, reason: `Fake ban executed by ${author.tag}` })
-        .then(whook => whook.edit(mee6name, { avatar: Constants.avatars.MEE6 }))
+      channel.createWebhook(mee6name, { avatar: 'https://i.imgur.com/WX2hGHk.jpg',
+        reason: `Fake ban executed by ${author.tag}` })
+        .then(whook => whook.edit(mee6name, { avatar: 'https://i.imgur.com/WX2hGHk.jpg' }))
         .then(hook => hook.send(`**${usernm}** left!`)
           .then(() => hook.delete())
           .catch(console.error)
