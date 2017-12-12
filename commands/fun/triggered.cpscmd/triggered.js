51b11b4189afe760c787fd1e3beaf01e29bace1a
Ethan <elawrence152@gmail.com>
2017-12-12 16:59:52 -0500

    Create triggered.js

---
+++ commands/fun/triggered.cpscmd/triggered.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'triggered',
+  func(msg, { send, author, member, Discord}) {
+  const result = await require('snekfetch').get('http://localhost:51001/api/triggered')
+    .set('src', (msg.mentions.users.first() || author).displayAvatarURL({ format: 'png', size: 2048 })); 
+  return send(new Discord.MessageEmbed()
+    .setColor(member ? member.displayColor : 134984)
+    .attachFiles([{ attachment: Buffer.from(result.body, 'base64'), name: 'image.gif'}])
+    .setImage('attachment://image.gif')
+    .setFooter(`Requested by: ${author.tag}`));
+  },
+};
