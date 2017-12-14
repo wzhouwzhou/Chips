ab3f9bd4a8b541599bee89d442e9a8eb03a23d53
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-14 07:13:11 +0100

    Fix

--- commands/fun/text.cpscmd/oof.js
+++ commands/fun/text.cpscmd/oof.js
@@ -3,9 +3,7 @@ const _ = require('lodash');
 module.exports = {
   name: 'oof',
   func(msg, { send }) {
-    return send(`O${[...new Array(_.random(2, 7))]
-      .map(() => _.sample(['o', 'O'])).join``}${[...new Array(_.random(4, 8))]
-      .map(() => _.sample(['f', 'F'])).join``}${[...new Array(_.random(1, 9))]
-      .map(() => _.sample(['!', '@'])).join``}!`);
+    return send(`O${[...new Array(_.random(1, 6))]
+      .map(() => _.sample(['o', 'O'])).join``}${_.sample(['f', 'F'])}${_.shuffle(['!', '1']).join``}`);
   },
 };
