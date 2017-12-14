dba05c03b08dbf8c0e52cbf2f89718847704fbc3
William Zhou <wzhouwzhou@gmail.com>
2017-12-13 20:17:30 -0500

    Fix -triggered

--- commands/fun/triggered.cpscmd/triggered.js
+++ commands/fun/triggered.cpscmd/triggered.js
@@ -1,12 +1,20 @@
 module.exports = {
   name: 'triggered',
-  async func(msg, { send, author, member, Discord}) {
-  const result = await require('snekfetch').get('http://localhost:51001/api/triggered')
-    .set('src', (msg.mentions.users.first() || author).displayAvatarURL({ format: 'png', size: 2048 })); 
-  return send(new Discord.MessageEmbed()
-    .setColor(member ? member.displayColor : 134984)
-    .attachFiles([{ attachment: Buffer.from(result.body, 'base64'), name: 'image.gif'}])
-    .setImage('attachment://image.gif')
-    .setFooter(`Requested by: ${author.tag}`));
+  async func(msg, { send, author, member, Discord, channel }) {
+    channel.startTyping();
+    try {
+      const result = await require('snekfetch').get('http://localhost:51001/api/triggered')
+        .set('src', (msg.mentions.users.first() || author).displayAvatarURL({ format: 'png', size: 2048 }));
+      send(new Discord.MessageEmbed()
+        .setColor(member ? member.displayColor : 134984)
+        .attachFiles([{ attachment: Buffer.from(result.body, 'base64'), name: 'image.gif' }])
+        .setImage('attachment://image.gif')
+        .setFooter(`Requested by: ${author.tag}`));
+      return channel.stopTyping();
+    } catch (err) {
+      send('Triggered gif generation failed...');
+      channel.stopTyping();
+      throw err;
+    }
   },
 };
