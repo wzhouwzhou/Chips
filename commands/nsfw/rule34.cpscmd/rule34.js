0238766f9f65b84db19631d111a18af79ffbd05b
William <wzhouwzhou@gmail.com>
2017-12-17 14:31:55 -0500

    More filters

--- commands/nsfw/rule34.cpscmd/rule34.js
+++ commands/nsfw/rule34.cpscmd/rule34.js
@@ -10,7 +10,7 @@ const _default_ = {
   q: 'index',
 };
 
-const get_query = async(...tags) => {
+const get_query = async(i, ...tags) => {
   const search = `${qs.stringify(_default_)}&tags=${tags.map(tag => qs.escape(tag.trim())).join('+')}`;
 
   const resp = await get(`${base}?${search}`);
@@ -19,10 +19,11 @@ const get_query = async(...tags) => {
   const r = await parse(resp.body);
   let url;
   for (let tries = 0; tries < 5; tries++) {
+    if (!r || !r.posts) return null;
     const posts = r.posts.post;
     if (!posts || posts.length === 0) return null;
     const item = posts[_.random(0, Math.min(99, r.posts.$.count))].$;
-    if (item.tags.match(/(loli)|(shota)|(gore)/i)) continue;
+    if (i && item.tags.match(/(loli)|(shota)|(gore)|(pettanko)/i)) continue;
     const temp = item.file_url;
 
     if (!temp.match(/(webm)|(mp)/)) {
@@ -35,12 +36,12 @@ const get_query = async(...tags) => {
 
 module.exports = {
   name: 'rule34',
-  async func(msg, { send, suffix, member, args, Discord }) {
+  async func(msg, { send, suffix, author, member, args, Discord }) {
     if (!args[0]) return send('Nothing given to search');
 
     try {
       const tags = suffix.split(',');
-      const url = await get_query(...tags);
+      const url = await get_query(author.id !== '259209114268336129', ...tags);
       if (!url) return send('No results found after 5 tries after filtering');
       const ext = url.substr(url.lastIndexOf('.'));
       const embed = new Discord.MessageEmbed()
