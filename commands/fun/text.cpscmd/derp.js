4400bfadd2cda096374c53c818ce78801c3d89c0
William <wzhouwzhou@gmail.com>
2017-12-23 11:11:39 -0500

    Fix derp

--- commands/fun/text.cpscmd/derp.js
+++ commands/fun/text.cpscmd/derp.js
@@ -6,9 +6,9 @@ module.exports = {
   async func(msg, { suffix, send, channel }) {
     if (suffix[0]) {
       channel.startTyping();
-      send(suffix.join(' ').replace(/[^\s]{1,2}/g, m => `${m[0]
+      send(suffix.replace(/[^\s]{1,2}/g, m => `${m[0]
         .toUpperCase()}${m[1] ? m[1].toLowerCase() : ''}`));
-      channel.stopTyping();
+      return channel.stopTyping();
     } else {
       return send('', { files: [{ attachment: (await snekfetch.get(DERP)).body }] });
     }
