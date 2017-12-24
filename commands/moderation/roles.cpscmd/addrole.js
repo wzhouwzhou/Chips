64d79e9ed82c4cb6b6f31899b6763550fa887b6f
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-24 13:17:17 +0100

    Revert "Update || addrole.js"
    This reverts commit ee339dadb10d29bea0017525a755e43911f4ac1b.

--- commands/moderation/roles.cpscmd/addrole.js
+++ commands/moderation/roles.cpscmd/addrole.js
@@ -1,18 +1,20 @@
 module.exports = {
   name: 'addrole',
   async func(msg, { send, guild, args, content, member, Discord, author }) {
-    if (!guild) {
-      return send('You cannot use this command in Direct Messages.');
-    }
+    if (author.id === '166630166825664512' && guild.id === '291558782755012610') {
+      if (!guild) {
+        return send('You cannot use this command in Direct Messages.');
+      }
 
-    if (!args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && !args[1].match(/^[^]*<@!?(\d+)>[^]*$/)) {
-      return send('Mention?');
-    }
+      if (!args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && !args[1].match(/^[^]*<@!?(\d+)>[^]*$/)) {
+        return send('Mention?');
+      }
 
-    if (!args[1]) {
-      return send('Role?');
-    }
+      if (!args[1]) {
+        return send('Role?');
+      }
 
+<<<<<<< HEAD
     if (args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1] && !author.id === '205608598233939970') {
       let targetUser = msg.mentions.members.first();
       let targetRole = content.substring(content.indexOf(args[1]));
@@ -22,6 +24,18 @@ module.exports = {
         return send(new Discord.MessageEmbed()
           .setColor(member.displayColor)
           .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
+=======
+      if (args[0].match(/^[^]*<@!?(\d+)>[^]*$/) && args[1]) {
+        let targetUser = msg.mentions.members.first();
+        let targetRole = content.substring(content.indexOf(args[1]));
+        let targetRoleSend = guild.roles.find('name', `${targetRole}`);
+        if (member.highestRole.position > targetRoleSend.position) {
+          await targetUser.addRole(guild.roles.find('name', `${targetRole}`));
+          return send(new Discord.MessageEmbed()
+            .setColor(member.displayColor)
+            .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
+        }
+>>>>>>> parent of ee339dad... Update || addrole.js
       }
     } else {
       let targetUser = msg.mentions.members.first();
@@ -31,6 +45,8 @@ module.exports = {
       return send(new Discord.MessageEmbed()
         .setColor(member.displayColor)
         .setDescription(`**Succesfully gave** <@&${targetRoleSend.id}> || **${targetRole}** **to** <@${targetUser.id}> || ${targetUser.username}`));
+    } else {
+      return send('You don\'t have enough permissions to use this command!');
     }
   },
 };
