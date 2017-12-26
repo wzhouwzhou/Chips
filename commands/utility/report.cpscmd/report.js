11071f132824bce3646cadeada9889d832d12851
lucaslsg <lucaslucaso2002@gmail.com>
2017-12-26 17:36:13 +0100

    Fix?

--- commands/utility/report.cpscmd/report.js
+++ commands/utility/report.cpscmd/report.js
@@ -15,10 +15,11 @@ module.exports = {
       let desc = content.substring(content.indexOf(args[3]));
       return send(new Discord.MessageEmbed()
         .setTitle('Report', ` Sent by ${author.tag}!`)
-        .addField('Tag', `${tag}`, true)
-        .addField('ID', `${id}`, true)
-        .addField('ScreenshotLink | Proof', `${link}`, true)
-        .addField('Description | Information', `${desc}`, true)
+        .addBlankField(true)
+        .addField('Tag', `${tag}`)
+        .addField('ID', `${id}`)
+        .addField('ScreenshotLink | Proof', `${link}`)
+        .addField('Description | Information', `${desc}`)
         .setColor(member.displayColor)
         .setImage(`${link}`));
     }
