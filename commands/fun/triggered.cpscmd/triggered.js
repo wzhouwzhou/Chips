a4fec956b69ea1946cb956a97bf10b967eb2784b
William <wzhouwzhou@gmail.com>
2017-12-30 19:46:19 -0500

    Triggered gif generation is now cached

--- commands/fun/triggered.cpscmd/triggered.js
+++ commands/fun/triggered.cpscmd/triggered.js
@@ -1,13 +1,22 @@
+const cache = new Map;
+
 module.exports = {
   name: 'triggered',
   async func(msg, { send, author, member, Discord, channel }) {
     channel.startTyping();
     try {
-      const result = await require('snekfetch').get('http://localhost:51001/api/triggered')
-        .set('src', (msg.mentions.users.first() || author).displayAvatarURL({ format: 'png', size: 2048 }));
+      const target = msg.mentions.users.first() || author;
+
+      if (!cache.has(target.id)) {
+        const result = await require('snekfetch').get('http://api.localhost:51001/triggered')
+          .set('src', target.displayAvatarURL({ format: 'png', size: 2048 }));
+        const buffer = result.body instanceof Buffer ? result.body : Buffer.from(result.body, 'base64');
+        cache.set(target.id, buffer);
+      }
+
       send(new Discord.MessageEmbed()
         .setColor(member ? member.displayColor : 134984)
-        .attachFiles([{ attachment: Buffer.from(result.body, 'base64'), name: 'image.gif' }])
+        .attachFiles([{ attachment: cache.get(target.id), name: 'image.gif' }])
         .setImage('attachment://image.gif')
         .setFooter(`Requested by: ${author.tag}`));
       return channel.stopTyping();
