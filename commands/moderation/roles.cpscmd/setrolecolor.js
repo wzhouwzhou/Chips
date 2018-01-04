b0dff72d857028d52d2d96a8b561201588bf083c
lucaslsg <lucaslucaso2002@gmail.com>
2018-01-04 10:40:53 +0100

    Update || setrolecolor.js "resets rolecolor"

--- commands/moderation/roles.cpscmd/setrolecolor.js
+++ commands/moderation/roles.cpscmd/setrolecolor.js
@@ -1,4 +1,5 @@
 const reg = /#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/;
+const reset = /r|reset/;
 
 module.exports = {
   name: 'setrolecolor',
@@ -6,9 +7,6 @@ module.exports = {
     if (!guild) {
       return send('You need to use this command in a guild!');
     }
-
-    // Args[0] === hex color
-    // Content == Role
 
     if (!args[0] || !args[0].match(reg)) {
       return send('Hex color?');
@@ -20,10 +18,21 @@ module.exports = {
 
     if (args[0].match(reg) && content.substring(content.indexOf(args[1]))) {
       let role = guild.roles.find('name', `${content.substring(content.indexOf(args[1]))}`);
-      let hexcolor = args[0];
+      let hexcolor = args[0] || args[0].match(reg);
       if (role && (member.highestRole.position > role.position)) {
         await role.setColor(`${hexcolor}`);
         return send(`Role color set to ${hexcolor}!`);
+      } else if (!role) {
+        return send(`${role} not found!`);
+      } else {
+        return send('Not enoguh permissions!');
+      }
+    } else if (args[0].match(reset) && content.substring(content.indexOf(args[1]))) {
+      let role = guild.roles.find('name', `${content.substring(content.indexOf(args[1]))}`);
+      let resetcolor = '#000000';
+      if (role && (member.highestRole.position > role.position)) {
+        await role.setColor(`${resetcolor}`);
+        return send(`Role color resetted!`);
       } else if (!role) {
         return send(`${role} not found!`);
       } else {
