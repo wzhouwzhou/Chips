247449477cbc2872273f1fa95d35c20a9370dc8b
lucaslsg <lucaslucaso2002@gmail.com>
2018-01-04 10:53:39 +0100

    revert

--- commands/moderation/roles.cpscmd/setrolecolor.js
+++ commands/moderation/roles.cpscmd/setrolecolor.js
@@ -1,5 +1,4 @@
 const reg = /#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/;
-const reset = /r|reset/;
 
 module.exports = {
   name: 'setrolecolor',
@@ -8,7 +7,14 @@ module.exports = {
       return send('You need to use this command in a guild!');
     }
 
+<<<<<<< HEAD
     if (!args[0] || !args[0].match(reg) || !args[0].match(reset)) {
+=======
+    // Args[0] === hex color
+    // Content == Role
+
+    if (!args[0] || !args[0].match(reg)) {
+>>>>>>> parent of b0dff72d... Update || setrolecolor.js "resets rolecolor"
       return send('Hex color?');
     }
 
@@ -18,21 +24,10 @@ module.exports = {
 
     if (args[0].match(reg) && content.substring(content.indexOf(args[1]))) {
       let role = guild.roles.find('name', `${content.substring(content.indexOf(args[1]))}`);
-      let hexcolor = args[0] || args[0].match(reg);
+      let hexcolor = args[0];
       if (role && (member.highestRole.position > role.position)) {
         await role.setColor(`${hexcolor}`);
         return send(`Role color set to ${hexcolor}!`);
-      } else if (!role) {
-        return send(`${role} not found!`);
-      } else {
-        return send('Not enoguh permissions!');
-      }
-    } else if (args[0].match(reset) && content.substring(content.indexOf(args[1]))) {
-      let role = guild.roles.find('name', `${content.substring(content.indexOf(args[1]))}`);
-      let resetcolor = '#000000';
-      if (role && (member.highestRole.position > role.position)) {
-        await role.setColor(`${resetcolor}`);
-        return send(`Role color resetted!`);
       } else if (!role) {
         return send(`${role} not found!`);
       } else {
