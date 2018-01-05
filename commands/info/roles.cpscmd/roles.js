f0e390e727850c3c0eacb3b66b4453480d1a5b29
William <wzhouwzhou@gmail.com>
2018-01-05 18:41:09 -0500

    Split role name

--- commands/info/roles.cpscmd/roles.js
+++ commands/info/roles.cpscmd/roles.js
@@ -1,13 +1,12 @@
 /* eslint no-await-in-loop: 'off' */
 const _ = require('lodash');
-const chunk = require('../../../rewrite-all/src/deps/functions/splitChunkF').default({ _ });
 const split = require('../../../rewrite-all/src/deps/functions/splitChunkF').default({ _ });
 const { Paginator } = require('../../../rewrite-all/src/struct/client/Paginator');
 const { pack } = require('erlpack');
 
 module.exports = {
   name: 'roles',
-  async func(msg, { send, guild, member, args, Discord, prefix, author }) {
+  async func(msg, { send, guild, member, args, Discord, prefix }) {
     if (!guild) return send('You must be in a server to use this!');
     if (args[0] && args[0].toLowerCase() === 'all') {
       return send(new Discord.MessageEmbed().setColor(member.displayColor).setTitle(`Role List (${guild.roles.size})`)
@@ -17,7 +16,11 @@ module.exports = {
 
     const data = guild.roles.array()
       .sort((b, a) => b.position - a.position)
-      .map(r => [r.name.replace(/@/, '(at)'), r.members.size]);
+      .map(r => {
+        let name = Discord.Util.splitMessage(r.name, { maxLength: 30, char: '' });
+        if (Array.isArray(name)) name = name.join`\n`;
+        return [name, r.members.size];
+      });
 
     const data2 = split(data, { clone: true, size: 10 });
     const fields = [];
@@ -26,7 +29,7 @@ module.exports = {
         .get('http://api.localhost:51001/table')
         .set('X-Data', pack([['|-- Role name --|', 'Count'], ...list]).toString('base64'))
         .set('X-Data-Transform', 'ERLPACK64')
-        .then(r => Discord.Util.splitMessage(r.body.data, { maxLength: 900 }));
+        .then(r => Discord.Util.splitMessage(r.body.data, { maxLength: 975 }));
 
       if (Array.isArray(eached)) {
         const temp = [];
