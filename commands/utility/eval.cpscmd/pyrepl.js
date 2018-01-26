d916744fd7d19abfae09250d586ceb8ee7618663
William Zhou <wzhouwzhou@gmail.com>
2018-01-26 13:30:46 -0500

    Updates

--- commands/utility/eval.cpscmd/pyrepl.js
+++ commands/utility/eval.cpscmd/pyrepl.js
@@ -1,14 +1,18 @@
 const childProcess = require('child_process');
 const { spawn } = childProcess;
 const cb = '```';
+const inrepl = new Map;
 module.exports = {
   name: 'pyrepl',
   func(msg, { send, author, channel }) {
     const sp = spawn('python3', []);
     let mcol;
+    if (inrepl.has(channel.id)) return send('A pyrepl session is in progress');
+    inrepl.set(channel.id, true);
     const mcol_filter = m => {
       if (m.author.id === author.id && m.content.startsWith(cb) && m.content.endsWith(cb)) {
         const input = m.content(0, 1950).replace(new RegExp(`(^${cb})|(${cb})$`), '');
+        console.log(`PyREPL Input ${cb}\n${input}${cb}`);
         send(`PyREPL Input ${cb}\n${input}${cb}`);
         sp.stdin.write(input);
         return true;
@@ -18,6 +22,7 @@ module.exports = {
 
     const handleExit = code => {
       mcol.stop();
+      inrepl.delete(channel.id);
       return send(`Exit code: ${cb}${code.substr(0, 1950)}${cb}`);
     };
     sp.stdout.on('data', data => send(`stdout: ${cb}py\n${data.substr(0, 1950)}${cb}`));
@@ -31,6 +36,7 @@ module.exports = {
       mcol_filter,
       { time: 5 * 60 * 1000 }
     );
+    mcol.on('end', () => send('Pyrepl mcol ended'));
     return mcol;
   },
 };
