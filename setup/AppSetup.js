b61e9a16f43472b699a4f830d0b043807080858a
wzhouwzhou@gmail.com <wzhouwzhou@gmail.com>
2018-01-26 08:12:58 -0500

    Update

--- setup/AppSetup.js
+++ setup/AppSetup.js
@@ -59,7 +59,7 @@ module.exports = () => {
     if (req.hostname.match(/^invite/i)) {
       return res.redirect('https://discordapp.com/oauth2/authorize?client_id=296855425255473154&scope=bot&permissions=2146958591');
     } else if (req.hostname.match(/^support/i)) {
-      return res.redirect('https://discord.gg/jj5FzF7');
+      return res.redirect('https://guilds.chipsbot.me:2087/official');
     }
     next();
   });
