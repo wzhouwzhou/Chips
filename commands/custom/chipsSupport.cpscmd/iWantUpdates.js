2d6e994e7f585e2fd3a96ed610d62c82f200d32e
Ethan <elawrence152@gmail.com>
2018-02-19 02:50:35 -0500

    Update iWantUpdates.js

--- commands/custom/chipsSupport.cpscmd/iWantUpdates.js
+++ commands/custom/chipsSupport.cpscmd/iWantUpdates.js
@@ -4,16 +4,16 @@ exports.func = async(msg, ctx) => {
   if (!guild || guild.id !== '307623291479130132') return true;
   const role = guild.roles.get('413803807248613380') || guild.roles.find('name', 'Update-Notify');
   if (member.roles.has(role.id)) {
-    return send(`You already have access to the Update Channel here! Remove the Updates role with ${prefix}idontwantupdates`);
+    return send(`You already get pinged when there are updates! Remove the Updates role with ${prefix}idontwantupdates`);
   } else {
     await member.addRole(role);
-      return send('You now have access to the Updates Channel!');
+      return send('You will now be pinged whenever there is an update!');
   }
 };
 
 exports.metadata = {
   category: require('../').category,
-  description: 'This custom command allows you to gain access to Chips\' Support Server Updates Channel!',
+  description: 'This custom command lets you be pinged whenever theres a Chips update in the Chips Support Server Update Channel!',
   usage: 'iwantupdates <no args>',
   example: 'iwantupdates',
   perms: [['global.custom.chipsSupport.*']],
