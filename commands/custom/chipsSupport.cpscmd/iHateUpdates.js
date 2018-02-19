17a0072830ccadc5b3a59daadf400023c46c131b
Ethan <elawrence152@gmail.com>
2018-02-19 02:51:09 -0500

    Update iHateUpdates.js

--- commands/custom/chipsSupport.cpscmd/iHateUpdates.js
+++ commands/custom/chipsSupport.cpscmd/iHateUpdates.js
@@ -5,7 +5,7 @@ exports.func = async(msg, ctx) => {
   const role = guild.roles.get('413803807248613380') || guild.roles.find('name', 'Update-Notify');
   if (member.roles.has(role.id)) {
     await member.removeRole(role);
-      return send('You no longer have access to the Updates Channel!');
+      return send('You are no longer pinged in the Update Channel!');
   } else {
     return send(`You dont have the role yet!`);
   }
@@ -13,7 +13,7 @@ exports.func = async(msg, ctx) => {
 
 exports.metadata = {
   category: require('../').category,
-  description: 'This custom command allows you to lose access to Chips\' Support Server Updates Channel!',
+  description: 'This custom command allows you to not be pinged for Chips Updates!',
   usage: 'idontwantupdates <no args>',
   example: 'idontwantupdates',
   perms: [['global.custom.chipsSupport.*']],
