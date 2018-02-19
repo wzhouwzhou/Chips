ef871ba6bf9490299ed3ef1eab31ae5b7f3f1d78
Ethan <elawrence152@gmail.com>
2018-02-19 02:35:52 -0500

    Create index.js

---
+++ commands/custom/chipsSupport.cpscmd/index.js
@@ -0,0 +1,6 @@
+const iWantUpdates = require('./iWantUpdates');
+const iHateUpdates = require('./iHateUpdates');
+module.exports = [
+  [iWantUpdates.name, iWantUpdates],
+  [iHateUpdates.name, iHateUpdates],
+];
