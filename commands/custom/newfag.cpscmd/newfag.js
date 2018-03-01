6a236a2e18d82035d88566e223dcec577abd0e34
William <wzhouwzhou@gmail.com>
2018-02-28 22:27:53 -0500

    Update

--- commands/custom/newfag.cpscmd/newfag.js
+++ commands/custom/newfag.cpscmd/newfag.js
@@ -3,10 +3,11 @@ const _ = require('lodash');
 const split = require('../../../rewrite-all/src/deps/functions/splitChunkF').default({ _ });
 const { Paginator } = require('../../../rewrite-all/src/struct/client/Paginator');
 exports.name = 'newfag';
+//136176078199717888
 exports.func = async(msg, { prefix, guild, content, args }) => {
-  if (!guild || guild.id !== '136176078199717888') return true;
+  if (!guild || guild.id !== '396575145302556672') return true;
   if (content.match(new RegExp(`${_.escapeRegExp(prefix)}r`, 'i')) && args[0] !== 'n') return true;
-  const list = (guild.roles.get('304364013524090891') || guild.roles.find('name', 'newfag')).members.filter(m => m.id !== '257574409852813315');
+  const list = (guild.roles.get('408801791900385290') || guild.roles.find('name', 'new person')).members.filter(m => m.id !== '257574409852813315');
   const p = new Paginator(msg, {
     type: 'paged',
     embedding: true,
