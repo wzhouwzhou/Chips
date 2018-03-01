82a76e899c94ca82a83ae10647e1da13d63b9f99
William <wzhouwzhou@gmail.com>
2018-02-28 21:09:09 -0500

    Selfroles

--- commands/custom/bwinfo.cpscmd/bwinfo.js
+++ commands/custom/bwinfo.cpscmd/bwinfo.js
@@ -14,6 +14,7 @@ const img_bwroles = 'https://cdn.discordapp.com/attachments/385987386804404224/4
   title_bwtwitter = "This Server's Twitter!",
   title_bwsteam = "This Server's Steam!",
   title_bwinvite = 'Invite your friends!',
+  title_bwselfroles = 'Selfroles',
 
   desc_bwinfo = `¤This is an anime themed community mixed with something more!
 ¤ Has a lot of channels, active voice chats, and selfroles for you to choose!
@@ -22,15 +23,23 @@ const img_bwroles = 'https://cdn.discordapp.com/attachments/385987386804404224/4
   desc_bwstaff = `Anyone with the |BW| Staff role!
 These people are here to make your experience here better! Look on the right member sidebar for anyone with Moderator and up and mention them or send a direct message if you need anything!`,
   desc_bwroles = `We have hundreds of roles available! Staff roles are Moderator +. Level roles are gained simply by chatting. Do !levels to learn more.
-For information regarding self assigned roles, read <#417400871899365396>. Several color roles exist`,
+Several color roles exist, do \-roles\` in a bot channel and flip through the pages to choose one to ask staff for!`,
   desc_bwsupport = 'You can approach anyone with the `ONLINE SUPPORT` role and know your feelings will be respected if you need any emotional support or just want to talk about something!',
   desc_bwinvite = '** https://discord.gg/8ZtDDhK\nhttps://guilds.chipsbot.me/baka_world **',
+  desc_bwselfroles = `Selfroles can be assigned by using @Nadeko#6685 's .iam and .iamnot commands! To give yourself a selfrole just type \`.iam [Selfrole name]\` (without the []), to remove it do \`.aimnot\`
+  Examples:
+  \`.iam Female\`
+A list of selfroles can be fetched by going to the bot channels and doing \`.lsar\`
+To gain access to the NSFW channels use the \`-lewdme\` command! To remove yourself use \`-unlewdme\``,
 
   embed_bwinvite = new Discord.MessageEmbed()
     .setTitle(title_bwinvite)
     .setDescription(desc_bwinvite)
     .setColor('010000'),
-
+  embed_selfroles = new Discord.MessageEmbed()
+    .setTitle(title_bwselfroles)
+    .setDescription(desc_bwselfroles)
+    .setColor('B3Bf89'),
   embed_info = new Discord.MessageEmbed()
     .attachFiles([new Discord.MessageAttachment(img_bwinfo, 'bw_info.png')])
     // .setImage('attachment://img_bwinfo.png')
@@ -62,6 +71,7 @@ exports.name = '_sendinv';
 exports.func = async(msg, { send }) => {
   await send(embed_info);
   await send(embed_roles);
+  await send(embed_selfroles);
   await send(embed_staff);
   await send(embed_online_support);
   await send(embed_bwinvite);
