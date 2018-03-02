8043e835560c78163ebf3ab348a6ed23bfecbc5d
lucaslsg <lucaslucaso2002@gmail.com>
2018-03-02 20:10:04 +0100

    fixes

--- commands/utility/setchannelname.cpscmd/setchannelname.js
+++ commands/utility/setchannelname.cpscmd/setchannelname.js
@@ -3,13 +3,22 @@ const _ = require('lodash');
 module.exports = {
   name: 'setchannelname',
   async func(msg, { send, guild, member, args, channel, suffix, Discord }) {
-    if (!guild) return send('This must be used in a server');
-    if (!suffix) return send('Nothing provided to set as channel name');
+    if (!guild) {
+      return send('This must be used in a server');
+    }
+    if (!suffix) {
+      return send('Nothing provided to set as channel name');
+    }
     if (!suffix.substring(suffix.indexOf(args[0])).match(/^[0-9a-z\-_]+$/i)) {
       return send('Text channel name must be alphanumeric with dashes or underscores.');
     }
-    if (suffix.length > 100) return send('The channel name can only be a maximum of 100 characters in length!');
-    if (suffix.length < 2) return send('The channel name can only be a minumum of 2 characters in length!');
+
+    if (suffix.length > 100) {
+      return send('The channel name can only be a maximum of 100 characters in length!');
+    }
+    if (suffix.length < 2) { 
+      return send('The channel name can only be a minumum of 2 characters in length!'); 
+    }
 
     const cc = await channel.setName(suffix.substring(suffix.indexOf(args[0])));
 
--- commands/utility/setchanneltopic.cpscmd/setchanneltopic.js
+++ commands/utility/setchanneltopic.cpscmd/setchanneltopic.js
@@ -2,9 +2,15 @@ const _ = require('lodash');
 module.exports = {
   name: 'setchanneltopic',
   async func(msg, { send, guild, member, args, channel, suffix, Discord }) {
-    if (!guild) return send('This must be used in a server');
-    if (!suffix) return send('Nothing provided to set as channel topic');
-    if (suffix.length > 1024) return send('The channel topic can only be a maximum of 1024 characters in length!');
+    if (!guild) {
+      return send('This must be used in a server');
+    }
+    if (!suffix) {
+      return send('Nothing provided to set as channel topic');
+    }
+    if (suffix.length > 1024) {
+      return send('The channel topic can only be a maximum of 1024 characters in length!');
+    }
 
     const cc = await channel.setTopic(suffix.substring(suffix.indexOf(args[0])));
 
