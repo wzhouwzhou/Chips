a274629cc752a3564f4b7590b0526753b73cabc0
lucaslsg <lucaslucaso2002@gmail.com>
2018-03-02 21:32:14 +0100

    fix

--- commands/utility/emoji.cpscmd/addemoji.js
+++ commands/utility/emoji.cpscmd/addemoji.js
@@ -3,17 +3,25 @@ const reg = /^(https?:\/\/[^.]+\.[^]+)$/;
 module.exports = {
   name: 'addemoji',
   async func(msg, { send, args, guild }) {
-    if (guild.emojis.size >= 100) return send(`Maximum number of emojis reached (${guild.emojis.size})`);
+    if (guild.emojis.size >= 100) {
+      return send(`Maximum number of emojis reached (${guild.emojis.size})`);
+    }
 
-    if (!args[0]) return send('No emoji name given :(');
+    if (!args[0]) {
+      return send('No emoji name given :(');
+    }
 
     let name = args[0];
 
-    if (!args[1]) return send('No url given :(');
+    if (!args[1]) {
+      return send('No url given :(');
+    }
 
     const emojiurl = (args[1].match(reg) || [0, null])[1];
 
-    if (!args[1].match(reg)) return send("Ensure you've given a url!");
+    if (!args[1].match(reg)) {
+      return send("Ensure you've given a url!");
+    }
 
     let emoji;
     try {
