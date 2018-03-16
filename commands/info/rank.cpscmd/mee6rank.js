c39e79d4b72536a9aa16329cb63b0028499966ab
Ethan <elawrence152@gmail.com>
2018-03-16 18:19:50 -0400

    Update mee6rank.js

--- commands/info/rank.cpscmd/mee6rank.js
+++ commands/info/rank.cpscmd/mee6rank.js
@@ -126,7 +126,7 @@ const userData = ({ member, infobad, Constants }) => new Promise(async res => {
     infobad.setDescription('User is not ranked!');
   } else {
     infobad.addField(`Ranked ${data.rank}`, `Level ${data.lvl} with ${data.total_xp} total xp!`);
-    infobad.addField(`Level xp: ${data.curr_xp}/${data.lvl_xp}/${member.guild.members.size} members/ (${data.xp_percent}%)`,
+    infobad.addField(`Level xp: ${data.curr_xp}/${data.lvl_xp} (${data.xp_percent}%) / ${member.guild.members.size} members`,
       `About ${data.estimated_msgs} msg(s) (${data.remaining_xp} xp) there to level ${data.lvl + 1}!`);
   }
   infobad.setColor(member.displayColor)
