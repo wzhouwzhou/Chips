f401fa12e37a91fe509e43824e573554f9c522c0
Ethan <elawrence152@gmail.com>
2018-03-29 15:52:43 -0400

    Update index.js

--- commands/custom/chipsSupport.cpscmd/index.js
+++ commands/custom/chipsSupport.cpscmd/index.js
@@ -1,6 +1,17 @@
 const iWantUpdates = require('./iWantUpdates');
 const iHateUpdates = require('./iHateUpdates');
+let staffapp = require('./applyForStaff');
+
+staffapp.metadata = {
+  category: require('../').category,
+  description: 'This command guides you through applying for a staff role in the support server!',
+  usage: 'applyforstaff',
+  example: 'applyforstaff',
+  perm: [['global.utility.applyforstaff.apply']],
+};
+
 module.exports = [
   [iWantUpdates.name, iWantUpdates],
   [iHateUpdates.name, iHateUpdates],
+  [staffapp.name, staffapp],
 ];
