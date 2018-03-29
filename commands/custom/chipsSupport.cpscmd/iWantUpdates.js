21fabae47eadd3b2d0daaf015136261146570c25
Ethan <elawrence152@gmail.com>
2018-03-29 15:44:09 -0400

    Update iWantUpdates.js

--- commands/custom/chipsSupport.cpscmd/iWantUpdates.js
+++ commands/custom/chipsSupport.cpscmd/iWantUpdates.js
@@ -1,13 +1,13 @@
 exports.name = 'iwantupdates';
 exports.func = async(msg, ctx) => {
-  const { prefix, send, reply, guild, member, author, channel, Discord, Constants } = ctx;
+  const { prefix, send, guild, member } = ctx;
   if (!guild || guild.id !== '307623291479130132') return true;
   const role = guild.roles.get('413803807248613380') || guild.roles.find('name', 'Update-Notify');
   if (member.roles.has(role.id)) {
     return send(`You already get pinged when there are updates! Remove the Updates role with ${prefix}idontwantupdates`);
   } else {
     await member.addRole(role);
-      return send('You will now be pinged whenever there is an update!');
+    return send('You will now be pinged whenever there is an update!');
   }
 };
 
