f7ecf583cb2957cc02b8d10a2c46878654c367f3
Ethan <elawrence152@gmail.com>
2018-03-30 01:15:41 -0400

    Create fox.js

---
+++ commands/fun/animals.cpscmd/fox.js
@@ -0,0 +1,16 @@
+const snekfetch = require('snekfetch');
+
+module.exports = {
+  name: 'fox',
+  async func(msg, { send, channel, Discord }) {
+    channel.startTyping();
+    try {
+      snekfetch.get('https://randomfox.ca/floof/').then(r => { send(new Discord.MessageAttachment(r.body.image, 'hi.pmg'));});
+      return channel.stopTyping();
+    } catch (err) {
+      channel.stopTyping();
+      await send('The fox photographer went missing!');
+      throw err;
+    }
+  },
+};
