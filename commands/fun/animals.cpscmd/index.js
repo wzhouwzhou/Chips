d25b6ec29268f930a82364e1a4c3ec19c07ab25a
Ethan <elawrence152@gmail.com>
2018-03-30 01:15:23 -0400

    Update index.js

--- commands/fun/animals.cpscmd/index.js
+++ commands/fun/animals.cpscmd/index.js
@@ -2,9 +2,10 @@
 let cat = require('./cat');
 let dog = require('./dog');
 let lizard = require('./lizard');
+let fox = require('./fox');
 
 console.log('[CPSCMD][FUN][animals] Building objects...');
-let cmds = [cat, dog, lizard];
+let cmds = [cat, dog, lizard, fox];
 
 cmds.forEach(cmd => {
   cmd.metadata = {};
@@ -14,18 +15,22 @@ cmds.forEach(cmd => {
 cat.metadata.description = 'This command sends cat images!';
 dog.metadata.description = 'This command sends dog images!';
 lizard.metadata.description = 'This command sends lizard images!';
+fox.metadata.description = 'This command sends fox images!';
 
 cat.metadata.usage = 'cat';
 dog.metadata.usage = 'dog';
 lizard.metadata.usage = 'lizard';
+fox.metadata.usage = 'fox';
 
 cat.metadata.example = 'cat';
 dog.metadata.example = 'dog';
 lizard.metadata.example = 'lizard';
+fox.metadata.example = 'fox';
 
 cat.metadata.perm = [['global.fun.animals.cat']];
 dog.metadata.perm = [['global.fun.animals.dog']];
 lizard.metadata.perm = [['global.fun.animals.*']];
+fox.metadata.perm = [['global.fun.animals.*']];
 
 console.log('[CPSCMD][FUN][animals] Build objects complete!');
 
@@ -33,4 +38,5 @@ module.exports = [
   [cat.name, cat],
   [dog.name, dog],
   [lizard.name, lizard],
+  [fox.name, fox],
 ];
