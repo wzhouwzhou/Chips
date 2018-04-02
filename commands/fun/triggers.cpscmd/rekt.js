94de33b2aea0a83ae95e42d2601682db16ddcf87
Ethan <elawrence152@gmail.com>
2018-04-01 23:09:31 -0400

    Update rekt.js

--- commands/fun/triggers.cpscmd/rekt.js
+++ commands/fun/triggers.cpscmd/rekt.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: 'rekt',
   func(msg, { send }) {
-    return send('Slain! https://giphy.com/gifs/TEcDhtKS2QPqE');
+    //return send('Slain! https://giphy.com/gifs/TEcDhtKS2QPqE');
+    return send('Slain!', new Discord.MessageAttachment('https://media.giphy.com/media/f9Sij046DveTptk2Dl/giphy.gif', 'hi.gif'));
   },
 };
