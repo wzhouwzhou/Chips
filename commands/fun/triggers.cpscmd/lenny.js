a20867eeedb20bb1e1a7a23ddd60935167dc1462
Ethan <elawrence152@gmail.com>
2018-04-02 00:14:28 -0400

    Update lenny.js

--- commands/fun/triggers.cpscmd/lenny.js
+++ commands/fun/triggers.cpscmd/lenny.js
@@ -8,9 +8,9 @@ const lennyTwo = [
   'https://i.imgur.com/svNztwx.gif',
   'https://i.imgur.com/0VgpGVM.gif',
   'https://i.imgur.com/0daFo5U.gif',
-  'https://www.tenor.co/uhvl.gif',
-  'https://www.tenor.co/IEEk.gif',
-  'https://www.tenor.co/vb3T.gif',
+  'https://media.giphy.com/media/C3CUJPTRqNk88/200.gif', //'https://www.tenor.co/uhvl.gif',
+  //'https://www.tenor.co/IEEk.gif',
+  //'https://www.tenor.co/vb3T.gif',
   'https://media.giphy.com/media/YcTRRbkZLELny/giphy.gif',
 ];
 
