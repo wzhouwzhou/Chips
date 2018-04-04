56310a1eb6073b7d313fe644d7616320b1623852
lucaslsg <lucaslucaso2002@gmail.com>
2018-04-04 14:52:23 +0200

    oooof

--- commands/moderation/bans.cpscmd/softban.js
+++ commands/moderation/bans.cpscmd/softban.js
@@ -68,17 +68,13 @@ module.exports = {
             .then(u => { u.ssend('Uh oh!', { embed: emb }); })
             .then(() => {
               m.reply('Softbanning!');
-              memberToUse
-                .ban({ reason: `[SOFTBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }, { days: 7 })
-                .then(guild.unban(memberToUse.toString(), { reason: `[UNBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }))
-                .catch(err => m.channel.send(`Something went wrong…\n${err}`));
+              memberToUse.ban({ reason: `[SOFTBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }, { days: 7 })
+              guild.unban(memberToUse.toString(), { reason: `[UNBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }),
             })
             .catch(() => {
               m.reply('Could not dm the user, but softbanning anyway!');
-              memberToUse
-                .ban({ reason: `[SOFTBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }, { days: 7 })
-                .then(guild.unban(memberToUse.toString(), { reason: `[UNBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }))
-                .catch(err => m.channel.send(`Something went wrong…\n${err}`));
+              memberToUse.ban({ reason: `[SOFTBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }, { days: 7 })
+              guild.unban(memberToUse.toString(), { reason: `[UNBAN]: [Author]: ${m.author.tag} [Reason]: ${reason}` }),
             });
         } else {
           console.log('[Softban] cancelled');
