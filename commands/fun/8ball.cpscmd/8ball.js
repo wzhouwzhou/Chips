9d79462bc1bd7adf19600c74255007ebb893f306
lucaslsg <lucaslucaso2002@gmail.com>
2018-05-05 09:24:55 +0200

    update 8ball

--- commands/fun/8ball.cpscmd/8ball.js
+++ commands/fun/8ball.cpscmd/8ball.js
@@ -1,5 +1,4 @@
-const answer = [
-  'Ok!',
+const normalans = [
   'No.',
   'Yes!',
   'Maybe.',
@@ -26,19 +25,74 @@ const answer = [
   'Indeed',
 ];
 
+const posans = [
+  'Yes',
+  '{shardusercount} people would agree',
+  'I think so',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes, definently',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Signs point to yes',
+  'Indeed',
+];
+
+const negans = [
+  'No',
+  'Maybe',
+  'I doubt it',
+  'You may rely on it',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  "Don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+
+];
+
 module.exports = {
   name: '8ball',
   func(msg, { send, args, suffix, Discord, member, client }) {
     if (!args[0]) return send('Do you have a question?');
     if (suffix.split('').reverse()[0] !== '?') return send('Question mark?');
 
+    if (~suffix.indexOf('love', 'life', 'friend', 'like')) {
     const embed = (new Discord.MessageEmbed)
       .setDescription(`🎱 **| ${
-        answer[~~(answer.length * Math.random())]
+        posans[~~(posans.length * Math.random())]
           .replace(/\{shardusercount\}/g, client.users.size)
       }**`)
       .setColor(member ? member.displayColor : 34203);
 
     return send(embed);
+    } 
+    
+    if (~suffix.indexOf('hate', 'run', 'roast', 'dislike', 'like')) {
+      const embed = (new Discord.MessageEmbed)
+        .setDescription(`🎱 **| ${
+          negans[~~(negans.length * Math.random())]
+            .replace(/\{shardusercount\}/g, client.users.size)
+        }**`)
+        .setColor(member ? member.displayColor : 34203);
+  
+      return send(embed);
+    } 
+    
+    else {
+      const embed = (new Discord.MessageEmbed)
+        .setDescription(`🎱 **| ${
+          normalans[~~(normalans.length * Math.random())]
+            .replace(/\{shardusercount\}/g, client.users.size)
+        }**`)
+        .setColor(member ? member.displayColor : 34203);
+
+      return send(embed);
+    }
   },
 };
