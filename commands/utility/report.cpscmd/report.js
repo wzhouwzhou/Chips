07bf3fcf5c4a04fa9c62a32c6be1dc76536ebb1f
lucaslsg <lucaslucaso2002@gmail.com>
2018-05-05 18:56:36 +0200

    fix

--- commands/utility/report.cpscmd/report.js
+++ commands/utility/report.cpscmd/report.js
@@ -47,7 +47,7 @@ module.exports = {
   async func(msg, { guild, send, args, member, Discord, content, author, suffix, channel, collector, reply }) {
     
     if(!member.roles.has(guild.roles.find('name', 'Server Member'))) {
-      reply(`You are not allowed to use this command!`);
+      return reply(`You are not allowed to use this command!`);
     
     }
     
