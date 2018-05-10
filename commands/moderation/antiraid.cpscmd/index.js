5407210355afca7d0445c7b40ff11ecab090a453
lucaslsg <lucaslucaso2002@gmail.com>
2018-05-10 18:41:03 +0200

    Revert "unverified"
    This reverts commit b7ef9973215c70ac3fa5ff67aa3e6c1529b92389.

--- commands/moderation/antiraid.cpscmd/index.js
+++ commands/moderation/antiraid.cpscmd/index.js
@@ -1,5 +1,4 @@
 let vs = require('./-vs');
-let uv = require('./uv');
 
 console.log('[CPSCMD][MODERATION][mutes] Building objects...');
 vs.metadata = {
@@ -11,20 +10,9 @@ vs.metadata = {
   customperm: ['BAN_MEMBERS'],
 };
 
-uv.metadata = {
-  category: require('../').category,
-  description: 'This command lets you unverify server members, however, a verification system must be setup beforehand!',
-  usage: '-uv <user>',
-  example: '-vs <user>',
-  perm: [['global.moderation.antiraid.-vs']],
-  customperm: ['MANAGE_ROLES'],
-};
-
 console.log('[CPSCMD][MODERATION][mutes] Build objects complete!');
 module.exports = [
   [vs.name, vs],
   ['verify', vs],
   ['verification', vs],
-  [uv.name, uv],
-  ['unverify', uv],
 ];
