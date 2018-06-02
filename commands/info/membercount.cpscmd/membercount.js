20265d3aefbc9289da2c68b2b7f2bf334a603c37
William <wzhouwzhou@gmail.com>
2018-06-02 11:08:51 -0400

    bot membercount

--- commands/info/membercount.cpscmd/membercount.js
+++ commands/info/membercount.cpscmd/membercount.js
@@ -3,7 +3,7 @@ module.exports = {
   async func(msg, { send, guild }) {
     if (guild) {
       let trueMemC = guild.members.filter(member => member.user.bot);
-      return send(`**${guild.members.size - trueMemC.size}** humans are in **${guild.name}**!`);
+      return send(`**${guild.members.size - trueMemC.size}** humans and **${trueMemC.size} bots** are in **${guild.name}**!`, { mentionEveryone: false });
     } else {
       return send('You must be in a server to use this!');
     }
