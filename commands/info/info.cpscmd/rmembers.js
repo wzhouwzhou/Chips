37b8e23dfda640660f8d628d386ec9a03c2fb135
William <wzhouwzhou@gmail.com>
2018-06-02 11:16:23 -0400

    Potential fix

--- commands/info/info.cpscmd/rmembers.js
+++ commands/info/info.cpscmd/rmembers.js
@@ -1,40 +1,41 @@
-const Searcher = require(path.join(__dirname, '../../../handlers/Searcher')).default
+const path = require('path');
+const Searcher = require(path.join(__dirname, '../../../handlers/Searcher')).default;
 
 module.exports = {
-    name: 'rmembers',
-    func(msg, { send, channel, guild, args,  }) {
+  name: 'rmembers',
+  async func(msg, { send, suffix, member, guild, args, Discord }) {
     if (guild) {
-        let options = { guild: guild };
-        global.searchers[guild.id] = new Searcher(options.guild);
+      let options = { guild: guild };
+      global.searchers[guild.id] = new Searcher(options.guild);
     }
-    if (!args[0]) { return send('No role given :<'); 
-} else {  
-    let role;
-    try {
-      role = args[0].substring(3, args[0].length - 1);
-      console.log(`Trying to find role from mention ${role}`);
-      role = guild.roles.get(role);
-      if (!role) throw 'NotRoleId';
-    } catch (err) { // Failed to find by id
-      role = content.substring(`${prefix}info ${action} `.length);
-      let list = searchers[guild.id].searchRole(role);
-      if (list.length > 1) {
-        await send('Multiple matches found, using first one..');
-      } else if (list.length < 1) {
-        return send(new Discord.MessageEmbed().setColor(member.displayColor).setDescription(`Role [${role}] not found!`));
+    if (!args[0]) {
+      return send('No role given :<');
+    } else {
+      let role;
+      try {
+        role = args[0];
+        role = guild.roles.get(role);
+        if (!role) throw new Error('NotRoleId');
+      } catch (err) {
+        role = suffix;
+        let list = global.searchers[guild.id].searchRole(role);
+        if (list.length > 1) {
+          await send('Multiple matches found, using first one..');
+        } else if (list.length < 1) {
+          return send(new Discord.MessageEmbed().setColor(member.displayColor).setDescription(`Role [${role}] not found!`));
+        }
+        role = list[0];
       }
-      role = list[0];
-    }
 
-    let memList = '';
-    for (mem of role.members.array()) {
-      memList += `[<@${mem.id}>] `;
-      if (memList.length > 1000) {
-        memList = 'Member list is too long!';
-        break;
+      let memList = '';
+      for (const mem of role.members.array()) {
+        memList += `${mem.user.tag} `;
+        if (memList.trim().length > 1950) {
+          send('Member list is too long! Truncating results...');
+          break;
+        }
       }
+      return send(`${memList ? memList.trim() : 'Nobody has this role!'}`);
     }
-    send(`${memList ? memList : 'Nobody has this role!'}`)
-      }
-    },
+  },
 };
