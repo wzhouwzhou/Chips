2ed6d692e6d7bffae269e5b9428ace599e9a7ebb
LegoDev <legoplaysmcmail@gmail.com>
2018-06-03 15:21:35 +0100

    fixed python eval.
    fixed a bug where using python eval without any code to evaluate would just return 'py'

--- commands/utility/eval.cpscmd/python.js
+++ commands/utility/eval.cpscmd/python.js
@@ -2,7 +2,8 @@ const childProcess = require('child_process');
 const cb = '```';
 module.exports = {
   name: 'python',
-  async func(msg, { reply, content, prefix }) {
+  async func(msg, { reply, content, prefix, args}) {
+    if (args.length < 1) return send('Nothing to eval in python.');
     let output = await new Promise(res =>
       childProcess.exec(`python3 -c "${content.substring(`${prefix}python `.length).replace(/\\n/, '\\\\n').replace(/"/g, '\\"')}"`, (err, sout, serr) => err || serr ? res(err || serr) : res(sout))
     );
