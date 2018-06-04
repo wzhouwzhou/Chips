11dbed927aeba90f83697199bf328b725fbb919c
William <wzhouwzhou@gmail.com>
2018-06-04 16:57:50 -0400

    Fix avatar

--- commands/info/avatar.cpscmd/avatar.js
+++ commands/info/avatar.cpscmd/avatar.js
@@ -4,19 +4,19 @@ const Paginator = require('../../../rewrite-all/src/struct/client/Paginator').Pa
 
 module.exports = {
   name: 'avatar',
-  async func(msg, { send, Discord, author, guild, reply }) {
+  async func(msg, { send, Discord, author, member, guild, reply }) {
     console.log(chalk.bold.bgBlue.green(`${author.tag} `) +
       chalk.bgWhite.red(`${author.id} `) +
       chalk.black.bgWhite(guild.id) +
       chalk.cyan(' [Avatar] '));
-    let targetmember = msg.mentions.members;
-    if (!targetmember || targetmember.size < 1) {
+    let targets = msg.mentions.users;
+    if (!targets || targets.size < 1) {
       let _ava = author.displayAvatarURL({ size: 2048 });
-      const ava = _ava.replace(/\.webp$/, '.png');
+      const ava = _ava.replace(/\.webp/, '.png');
 
       const selftar = new Discord.MessageEmbed()
         .setTitle(`Avatar Image of ${author.tag} `, ``, true)
-        .setColor(guild ? msg.member.displayColor : 71340)
+        .setColor(guild ? member.displayColor : 71340)
         .addField('Avatar Link: ', `[Click Here](${ava})`)
         .setImage(ava);
 
@@ -24,32 +24,32 @@ module.exports = {
 
       return send({ embed: selftar });
     } else {
-      const memberList = new Map();
-      targetmember.forEach(member => {
-        let _ava = member.user.displayAvatarURL({ size: 2048 });
-        const ava = _ava.replace(/\.webp$/, '.png');
-        memberList.set(member.id, {
-          title: `Avatar Image of ${member.user.tag} `,
+      const users = new Map();
+      targets.forEach(user => {
+        let _ava = user.displayAvatarURL({ size: 2048 });
+        const ava = _ava.replace(/\.webp/, '.png');
+        users.set(user.id, {
+          title: `Avatar Image of ${user.tag} `,
           page: ['Avatar Link: ', `[Click Here](${ava})`],
           image: ava,
         });
       });
       const pages = [], title = [], image = [];
-      memberList.forEach(e => {
+      users.forEach(e => {
         pages.push([e.page]);
         title.push(e.title);
         image.push(e.image);
       });
 
       if (pages.length === 1) {
-        const avator = new Discord.MessageEmbed()
+        const embed = new Discord.MessageEmbed()
           .setTitle(title[0])
-          .setColor(guild ? msg.member.displayColor : 71340)
+          .setColor(guild ? member.displayColor : 71340)
           .addField(...pages[0][0])
           .setImage(image[0]);
 
-        avator.setFooter(`Requested by ${author.tag}`);
-        return send('', { embed: avator });
+        embed.setFooter(`Requested by ${author.tag}`);
+        return send(embed);
       }
 
       const p = new Paginator(msg, {
