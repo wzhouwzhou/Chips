e02e7469190a50bc770832971a3dcec50495ad0f
lucaslsg <lucaslucaso2002@gmail.com>
2018-06-06 16:59:17 +0200

    fix?

--- commands/custom/selfrole.cpscmd/selfrole.js
+++ commands/custom/selfrole.cpscmd/selfrole.js
@@ -7,14 +7,14 @@ module.exports = {
     if (!guild.id === '274260111415836675') {
       return send();
     }
+    
     if (member.roles.some(r => ['GiveawayNotify', 'EventNotify', 'XP Suspended'].includes(r.name))) {
-      send(`Succesfully removed ${targetR} from <@!${author.id}>!`)µ
+      send(`Succesfully removed ${targetR} from <@!${author.id}>!`);
       return member.removeRole(targetRole.id);
-    }
-
-    if (~'GiveawayNotify EventNotify XP Suspended'.split(/\s+/).indexOf(content.substring(content.indexOf(args[0])))) {
+    } else { (~'GiveawayNotify EventNotify XP Suspended'.split(/\s+/).indexOf(content.substring(content.indexOf(args[0])))) {
       send(`Succesfully gave <@!${author.id}> ${targetR}!`);
       return member.addRole(targetRole.id);
+      }
     }
   },
 };
