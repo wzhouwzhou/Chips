331a2e18e10272c9496c331d793c2e05a7cff064
William <wzhouwzhou@gmail.com>
2018-07-03 13:43:59 -0400

    Update non guild lookup

--- commands/info/info.cpscmd/userinfo.js
+++ commands/info/info.cpscmd/userinfo.js
@@ -1,3 +1,4 @@
+/* eslint capitalized-comments: 'off' */
 const path = require('path');
 const Searcher = require(path.join(__dirname, '../../../handlers/Searcher')).default;
 const snekfetch = require('snekfetch');
@@ -32,22 +33,66 @@ exports.func = async(msg, ctx) => {
   return handle_user(Object.assign({ target, multiple }, ctx));
 };
 
-const handle_member = async({ client, send, target, multiple, Constants, Discord, convertTime, times }) => {
-  const avatarURL = target.user.displayAvatarURL({ format: 'png', size: 1024 });
-  const status = (() => {
-    switch (target.presence.status) {
-      case 'online': return 'online';
-      case 'idle': return 'idle';
-      case 'dnd': return 'dnd';
-      default: return 'invis';
-    }
-  })();
+const get_status = status => {
+  switch (status) {
+    case 'online': return 'online';
+    case 'idle': return 'idle';
+    case 'dnd': return 'dnd';
+    default: return 'invis';
+  }
+};
+
+const handle_user = async({ multiple, target, send, client, guild, Constants, Discord, convertTime, times }) => {
+  const avatarURL = target.displayAvatarURL({ format: 'png', size: 1024 });
+  const status = get_status(target.presence.status);
 
   const rp = snekfetch.get(`${Constants.APIURL}avaround`)
     .set('Authorization', process.env.RETHINKPSWD)
     .set('Status', status)
     .set('X-Data-Src', avatarURL);
 
+  let diff3, highest = 'years';
+  diff3 = convertTime(target.createdAt, times.indexOf(highest));
+  diff3 = `${diff3[0]} ${times[diff3[1]]}`;
+  const embed = new Discord.MessageEmbed;
+  embed.addField(`${target.tag}: `,
+    `${target.presence.activity && target.presence.activity.type ?
+      target.presence.activity.type
+        .replace(/(\w)(\w+)/,
+          (a, b, c) => b.toUpperCase() + c.toLowerCase()) :
+      'Playing'
+    } ${
+      target.presence.activity ?
+        target.presence.activity.name :
+        'nothing.'
+    }`, true
+  );
+  embed.addField('User id:', `${target.id}`, true);
+  embed.addField(`Joined Discord on ${target.createdAt.toUTCString()}`, `That's about ${diff3} ago!`);
+  embed.setColor(guild ? guild.me.displayColor : 'RANDOM');
+  embed.setThumbnail(Constants.loading[2]);
+  const shared = (await client.shard.broadcastEval(`client.guilds.filter(g => g.members.has('${target.id}')).size`))
+    .reduce((a, b) => a + b, 0);
+  embed.addField(`Bot Account: ${target.bot}`, `Shared servers with chips: ${shared}`);
+  let sentmsg;
+  rp.then(async r => {
+    const mm = await (await client.users.fetch('302252773427249163')).send(new Discord.MessageAttachment(r.body, 'image.png'));
+    embed.setThumbnail(mm.attachments.first().url);
+    if (sentmsg) await sentmsg.edit(multiple ? '(multiple users were found, using the first one)' : '\u200B', { embed });
+    else sentmsg = await send(multiple ? '(multiple users were found, using the first one)' : '\u200B', { embed });
+    return sentmsg;
+  });
+  sentmsg = await send(multiple ? '(multiple users were found, using the first one)' : '\u200B', { embed });
+};
+
+const handle_member = async({ client, send, target, multiple, Constants, Discord, convertTime, times }) => {
+  const avatarURL = target.user.displayAvatarURL({ format: 'png', size: 1024 });
+  const status = get_status(target.presence.status);
+
+  const rp = snekfetch.get(`${Constants.APIURL}avaround`)
+    .set('Authorization', process.env.RETHINKPSWD)
+    .set('Status', status)
+    .set('X-Data-Src', avatarURL);
 
   const membername = target.displayName.replace('@', '(at)');
   let highest = 'years';
@@ -55,7 +100,6 @@ const handle_member = async({ client, send, target, multiple, Constants, Discord
   diff = `${diff[0]} ${times[diff[1]]}`;
 
   let diff3;
-  highest = 'years';
   diff3 = convertTime(target.user.createdAt, times.indexOf(highest));
   diff3 = `${diff3[0]} ${times[diff3[1]]}`;
   const embed = new Discord.MessageEmbed;
@@ -75,14 +119,8 @@ const handle_member = async({ client, send, target, multiple, Constants, Discord
     .addField(`Nickname: ${membername}`, `Colour: ${target.displayHexColor}`, true);
   embed.addField(`Joined Discord on ${target.user.createdAt.toUTCString()}`, `That's about ${diff3} ago!`);
   embed.addField(`Joined ${target.guild.name}`, `About ${diff} ago on ${target.joinedAt.toUTCString()}`);
-  // Infobad.addField(`${member.lastMessage?"Last seen here at: "+member.lastMessage.createdAt.toUTCString():
-  //  "Last seen here: Unknown"}`,`${diff2!="NAN"?"That's about "+diff2+" ago!":"Time ago: Unknown"}`);
-  // infobad.addField(`Colour: `,`${member.displayHexColor}`,true)
   embed.addField(`Highest Role: ${target.highestRole.name}`, `Total number of roles: ${target.roles.size}`, true);
-  // Infobad.addField(`Status:`,`    ${member.presence.status}`, true);
   embed.addField(`Permissions number:`, target.permissions.bitfield);
-  // Infobad.addField(`Avatar URL`, `[Click Here](${member.user.avatarURL})`);
-  // infobad.setThumbnail(member.user.avatarURL);
   embed.setColor(target.displayColor || 'RANDOM');
   embed.setThumbnail(Constants.loading[2]);
   let sentmsg;
@@ -96,21 +134,17 @@ const handle_member = async({ client, send, target, multiple, Constants, Discord
   sentmsg = await send(multiple ? '(multiple users were found, using the first one)' : '\u200B', { embed });
 };
 
-const handle_user = ({ send, Discord }) => {
-  const embed = new Discord.MessageEmbed;
-  return send(embed);
-};
-
-const get_id = (args$n, clientorguild = {}) => {
+const get_id = (args$n, clientorguild = {}) => new Promise(res => {
   if (/^\d+$/.test(args$n)) {
     if ('members' in clientorguild) {
-      return clientorguild.members.fetch(args$n) || clientorguild.client.users.fetch(args$n) || null;
+      const [mp, up] = [clientorguild.members.fetch(args$n), clientorguild.client.users.fetch(args$n)];
+      mp.then(res).catch(() => up.then(res).catch(() => res(null)));
     } else {
       return clientorguild.users.fetch(args$n) || null;
     }
   }
   return null;
-};
+});
 
 const get_mention = (args$n, match, clientorguild) => {
   if (match.test(args$n)) {
