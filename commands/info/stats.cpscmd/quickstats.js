6b50d9f5d57267c77f1535420c4a37f94409cacf
William <wzhouwzhou@gmail.com>
2018-07-03 02:02:28 -0400

    Fix spacing

--- commands/info/stats.cpscmd/quickstats.js
+++ commands/info/stats.cpscmd/quickstats.js
@@ -4,6 +4,6 @@ module.exports = {
     const [members, guilds] = await client.shard.broadcastEval(
       '[client.guilds.array().map(g => g.members.size).reduce((a, b) => a + b, 0), client.guilds.size]')
       .then(r => [r.reduce((a, b) => a + b[0], 0), r.reduce((a, b) => a + b[1], 0)]);
-    return send(`**${members}** members in **${guilds}**guilds\nAverage Server Size: **${members / guilds}**`);
+    return send(`**${members}** members in **${guilds}** guilds\nAverage Server Size: **${members / guilds}**`);
   },
 };
